cf46bc1387e5e1894d2a04704cd34a624bb8ba11
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2021-05-14 15:42:34 +0100

    Should work but doesnt

--- api/src/main.py
+++ api/src/main.py
@@ -1,6 +1,7 @@
 from ariadne import QueryType, make_executable_schema
 from ariadne.asgi import GraphQL
 from starlette.applications import Starlette
+import uvicorn
 
 type_defs = """
     type Query {
@@ -21,3 +22,8 @@ schema = make_executable_schema(type_defs, query)
 
 app = Starlette(debug=True)
 app.mount("/graphql", GraphQL(schema, debug=True))
+
+if __name__ == "__main__":
+    print("======================================")
+    print("Launching the server")
+    uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info")
