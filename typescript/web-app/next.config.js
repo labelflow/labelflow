f42660b0236f69b72f98cac002a57aeea7c9667f
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2021-05-14 20:51:55 +0100

    install

---
+++ typescript/web-app/next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    images: {
+        deviceSizes: [320, 480, 640, 750, 828, 960, 1080, 1200, 1440, 1920, 2048, 2560, 3840],
+    },
+    future: {
+        //     webpack5: true,
+    },
+}
