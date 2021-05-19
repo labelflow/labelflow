3acffb9f29ff1cef45dd6cadc09d510422539430
Basile <basile-d@users.noreply.github.com>
2021-05-19 10:36:25 +0200

    Adding basic import modal and its tests

--- typescript/web-app/components/import-modal/__tests__/import-modal.tsx
+++ typescript/web-app/components/import-modal/__tests__/import-modal.tsx
@@ -1,8 +1,16 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import { ImportModal } from "../import-modal";
+import userEvent from "@testing-library/user-event";
 
-test("Should instanciate component", () => {
+test("Should be renamed", async () => {
+  const file = new File(["Hello"], "hello.png", { type: "image/png" });
+
   render(<ImportModal />);
 
-  expect(screen.getByText("Hello")).toBeDefined();
+  const input = screen.getByLabelText(/drop folders or images/i);
+
+  await waitFor(() => userEvent.upload(input, file));
+
+  // @ts-ignore
+  expect(input.files).toHaveLength(1);
 });
--- typescript/web-app/components/import-modal/import-modal.tsx
+++ typescript/web-app/components/import-modal/import-modal.tsx
@@ -1,3 +1,16 @@
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
 export const ImportModal = () => {
-  return <div>Hello</div>;
+  const onDrop = useCallback(() => {
+    // Do something with the files
+  }, []);
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()}>
+      <label htmlFor="file-uploader">Drop folders or images</label>
+      <input {...getInputProps()} id="file-uploader" />
+    </div>
+  );
 };
