e318b3b4aa94fa31a2a060ad1ccf6823bfe0cc8c
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-05-19 12:40:15 +0100

    Change name of generated schema

--- typescript/web-app/src/connectors/apollo-client/index.ts
+++ typescript/web-app/src/connectors/apollo-client/index.ts
@@ -1,7 +1,7 @@
 import { makeExecutableSchema } from "@apollo-model/graphql-tools";
 import { SchemaLink } from "@apollo/client/link/schema";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import typeDefs from "../../../../../data/__generated.graphql";
+import typeDefs from "../../../../../data/generated-schema.graphql";
 import { resolvers } from "./resolvers";
 
 const schema = makeExecutableSchema({
