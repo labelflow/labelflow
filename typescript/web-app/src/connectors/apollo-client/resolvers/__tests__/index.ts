a2f97f851f1a0b3610c9be93f224818d4fc7ca7b
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-05-19 12:43:59 +0100

    Fix tests

--- typescript/web-app/src/connectors/apollo-client/resolvers/__tests__/index.ts
+++ typescript/web-app/src/connectors/apollo-client/resolvers/__tests__/index.ts
@@ -7,19 +7,27 @@ describe("Example resolver test suite", () => {
     expect(queryResult.length).toBe(0);
   });
   test("Create example", async () => {
-    const createResult = await createExample(undefined, { name: "test" });
+    const createResult = await createExample(undefined, {
+      data: { name: "test" },
+    });
     expect(createResult?.name).toBe("test");
     expect(await localforage.getItem(`Example:${createResult.id}`)).toBe(
       createResult
     );
   });
   test("Query example", async () => {
-    const createResult = await createExample(undefined, { name: "test" });
-    const queryResult = await example(undefined, { id: createResult.id });
+    const createResult = await createExample(undefined, {
+      data: { name: "test" },
+    });
+    const queryResult = await example(undefined, {
+      where: { id: createResult.id },
+    });
     expect(queryResult).toBe(createResult);
   });
   test("Query examples", async () => {
-    const createResult = await createExample(undefined, { name: "test" });
+    const createResult = await createExample(undefined, {
+      data: { name: "test" },
+    });
     const queryResult = await examples();
     expect(queryResult.length).toBe(3);
     expect(queryResult[queryResult.length - 1]).toBe(createResult);
