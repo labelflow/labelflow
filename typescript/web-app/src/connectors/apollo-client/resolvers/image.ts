f0d6c99357f776de07013f30dccf9a881fcc10e4
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-05-20 18:26:22 +0100

    Replace common code by utils

--- typescript/web-app/src/connectors/apollo-client/resolvers/image.ts
+++ typescript/web-app/src/connectors/apollo-client/resolvers/image.ts
@@ -1,11 +1,11 @@
 import localforage from "localforage";
 import { v4 as uuidv4 } from "uuid";
-import { isEmpty } from "lodash/fp";
 import {
   MutationCreateImageArgs,
   QueryImageArgs,
   QueryImagesArgs,
 } from "../../../types.generated";
+import { appendToListInStorage, getListFromStorage } from "./utils";
 
 const typeName = "Image";
 const typeNamePlural = "Image:list";
@@ -15,13 +15,7 @@ export const image = (_: any, args: QueryImageArgs) => {
   return localforage.getItem(`${typeName}:${args?.where?.id}`);
 };
 export const images = async (_: any, args: QueryImagesArgs) => {
-  const entityKeysList = await localforage.getItem(typeNamePlural);
-  if (isEmpty(entityKeysList)) {
-    return [];
-  }
-  const entities = await Promise.all(
-    (entityKeysList as []).map((key: string) => localforage.getItem(key))
-  );
+  const entities = await getListFromStorage(typeNamePlural);
   const first = args?.first ?? entities.length;
   const skip = args?.skip ?? 0;
   return entities.slice(skip, first + skip);
@@ -41,18 +35,10 @@ export const createImage = async (_: any, args: MutationCreateImageArgs) => {
 
   // Set entity in db
   const newEntityKey = `${typeName}:${newEntity.id}`;
-
   await localforage.setItem(newEntityKey, newEntity);
 
   // Add entity to entity list
-  const oldEntityKeysList = await localforage.getItem(typeNamePlural);
-
-  const newEntitiesList =
-    oldEntityKeysList == null
-      ? [newEntityKey]
-      : [...(oldEntityKeysList as []), newEntityKey];
-
-  await localforage.setItem(typeNamePlural, newEntitiesList);
+  await appendToListInStorage(typeNamePlural, newEntityKey);
 
   return newEntity;
 };
