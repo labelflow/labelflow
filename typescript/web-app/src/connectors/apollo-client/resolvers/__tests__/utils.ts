6a1254ad2819833a82bce9a9f631a3fa72146904
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-05-20 18:12:11 +0100

    Add function to append element to list in storage

--- typescript/web-app/src/connectors/apollo-client/resolvers/__tests__/utils.ts
+++ typescript/web-app/src/connectors/apollo-client/resolvers/__tests__/utils.ts
@@ -1,5 +1,5 @@
 import localforage from "localforage";
-import { getListFromStorage } from "../utils";
+import { getListFromStorage, appendToListInStorage } from "../utils";
 
 jest.mock("localforage");
 
@@ -35,5 +35,34 @@ describe("Resolver utils tests", () => {
     expect(mockedLocalForage.getItem.mock.calls[2][0]).toEqual("Entity:2");
     expect(mockedLocalForage.getItem.mock.calls[3][0]).toEqual("Entity:3");
     expect(mockedLocalForage.getItem.mock.calls[4]).toBeUndefined();
+  });
+
+  test("Get list from storage when list is empty", async () => {
+    mockedLocalForage.setItem = jest.fn(
+      async (_: string, elements: any) => elements
+    );
+    mockedLocalForage.getItem = jest.fn(async () => null);
+
+    await appendToListInStorage("Entity:list", "Entity:1");
+
+    expect(mockedLocalForage.getItem.mock.calls[0][0]).toEqual("Entity:list");
+    expect(mockedLocalForage.setItem.mock.calls[0][0]).toEqual("Entity:list");
+    expect(mockedLocalForage.setItem.mock.calls[0][1]).toEqual(["Entity:1"]);
+  });
+
+  test("Get list from storage", async () => {
+    mockedLocalForage.setItem = jest.fn(
+      async (_: string, elements: any) => elements
+    );
+    mockedLocalForage.getItem = jest.fn(async () => ["Entity:1"]);
+
+    await appendToListInStorage("Entity:list", "Entity:2");
+
+    expect(mockedLocalForage.getItem.mock.calls[0][0]).toEqual("Entity:list");
+    expect(mockedLocalForage.setItem.mock.calls[0][0]).toEqual("Entity:list");
+    expect(mockedLocalForage.setItem.mock.calls[0][1]).toEqual([
+      "Entity:1",
+      "Entity:2",
+    ]);
   });
 });
--- typescript/web-app/src/connectors/apollo-client/resolvers/utils.ts
+++ typescript/web-app/src/connectors/apollo-client/resolvers/utils.ts
@@ -1,8 +1,8 @@
 import localforage from "localforage";
 import { isEmpty } from "lodash/fp";
 
-export const getListFromStorage = async (key: string) => {
-  const entityKeysList = await localforage.getItem(key);
+export const getListFromStorage = async (listKey: string) => {
+  const entityKeysList = await localforage.getItem(listKey);
   if (isEmpty(entityKeysList)) {
     return [];
   }
@@ -11,4 +11,18 @@ export const getListFromStorage = async (key: string) => {
       localforage.getItem(entityKey)
     )
   );
+};
+
+export const appendToListInStorage = async (
+  listKey: string,
+  element: string
+): Promise<any[]> => {
+  const oldEntityKeysList = await (<Promise<any[] | null>>(
+    localforage.getItem(listKey)
+  ));
+
+  const newEntitiesList =
+    oldEntityKeysList == null ? [element] : [...oldEntityKeysList, element];
+
+  return localforage.setItem(listKey, newEntitiesList);
 };
