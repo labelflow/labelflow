edba78af71c0df9ec2cbd9300b8b9f52e0c7e462
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-05-21 13:53:48 +0100

    Fix tests in example resolver

--- typescript/web-app/src/connectors/apollo-client/resolvers/__tests__/example.ts
+++ typescript/web-app/src/connectors/apollo-client/resolvers/__tests__/example.ts
@@ -1,35 +1,70 @@
 import localforage from "localforage";
 import { createExample, example, examples } from "../example";
 
+jest.mock("localforage", () => ({
+  setItem: jest.fn(async () => {}),
+  getItem: jest.fn(async () => {}),
+}));
+
+const mockedLocalForage = <
+  {
+    getItem: jest.Mock<Promise<any>>;
+    setItem: jest.Mock<Promise<any>>;
+  }
+>(localforage as unknown);
+
 describe("Example resolver test suite", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
   test("Query example when db is empty", async () => {
     const queryResult = await examples();
+
     expect(queryResult.length).toBe(0);
   });
+
   test("Create example", async () => {
     const createResult = await createExample(undefined, {
       data: { name: "test" },
     });
+
     expect(createResult?.name).toBe("test");
-    expect(await localforage.getItem(`Example:${createResult.id}`)).toEqual(
-      createResult
+    expect(mockedLocalForage.setItem.mock.calls[0][0]).toEqual(
+      `Example:${createResult.id}`
     );
   });
+
   test("Query example", async () => {
-    const createResult = await createExample(undefined, {
-      data: { name: "test" },
+    await example(undefined, {
+      where: { id: "testId" },
     });
-    const queryResult = await example(undefined, {
-      where: { id: createResult.id },
-    });
-    expect(queryResult).toEqual(createResult);
+
+    expect(mockedLocalForage.getItem.mock.calls[0][0]).toEqual(
+      "Example:testId"
+    );
   });
+
   test("Query examples", async () => {
-    const createResult = await createExample(undefined, {
-      data: { name: "test" },
-    });
+    mockedLocalForage.getItem.mockReturnValue(
+      Promise.resolve([
+        "Example:1",
+        "Example:2",
+        "Example:3",
+        "Example:4",
+        "Example:5",
+      ])
+    );
+
     const queryResult = await examples();
-    expect(queryResult.length).toBe(3);
-    expect(queryResult[queryResult.length - 1]).toEqual(createResult);
+
+    expect(queryResult.length).toEqual(5);
+    expect(mockedLocalForage.getItem.mock.calls[0][0]).toEqual(`Example:list`);
+    expect(mockedLocalForage.getItem.mock.calls[1][0]).toEqual(`Example:1`);
+    expect(mockedLocalForage.getItem.mock.calls[2][0]).toEqual(`Example:2`);
+    expect(mockedLocalForage.getItem.mock.calls[3][0]).toEqual(`Example:3`);
+    expect(mockedLocalForage.getItem.mock.calls[4][0]).toEqual(`Example:4`);
+    expect(mockedLocalForage.getItem.mock.calls[5][0]).toEqual(`Example:5`);
+    expect(mockedLocalForage.getItem.mock.calls[6]).toBeUndefined();
   });
 });
