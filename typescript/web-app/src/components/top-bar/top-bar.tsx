4a44ae2e90650af23d109b539618cd36837c3437
Hugo Raynal <raynalhugo@gmail.com>
2021-05-25 19:38:52 +0200

    (dumb) implementation of the button in the top bar

--- typescript/web-app/src/components/top-bar/top-bar.tsx
+++ typescript/web-app/src/components/top-bar/top-bar.tsx
@@ -1,10 +1,13 @@
 import { Flex } from "@chakra-ui/react";
 import { Logo } from "../logo";
 
+import { ImportButton } from "./import-button";
+
 export const TopBar = () => {
   return (
     <Flex as="header" alignItems="center" bg="white" p="4" h="64px">
       <Logo h="26px" />
+      <ImportButton />
     </Flex>
   );
 };
