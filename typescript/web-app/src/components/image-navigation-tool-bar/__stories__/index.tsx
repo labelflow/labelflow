7a78402c55104b338e6261c033d2dfa0b222c6fa
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2021-05-27 16:23:39 +0100

    Fix

--- typescript/web-app/src/components/image-navigation-tool-bar/__stories__/index.tsx
+++ typescript/web-app/src/components/image-navigation-tool-bar/__stories__/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-
 import { addDecorator } from "@storybook/react";
+import { NextRouter } from "next/router";
 
 import { ImageNav } from "../index";
 import { chakraDecorator } from "../../../utils/chakra-decorator";
@@ -10,25 +10,39 @@ export default {
   title: "web-app/Image Navigation Toolbar",
 };
 
+// @ts-ignore
 export const NoInput = () => <ImageNav />;
 
-export const NoImage = () => <ImageNav imageId="a" images={[]} router={{}} />;
+export const NoImage = () => (
+  <ImageNav imageId="a" images={[]} router={{} as unknown as NextRouter} />
+);
 
-export const NoImageNoId = () => <ImageNav images={[]} router={{}} />;
+export const NoImageNoId = () => (
+  // @ts-ignore
+  <ImageNav images={[]} router={{} as unknown as NextRouter} />
+);
 
 export const OneImage = () => (
-  <ImageNav imageId="a" images={[{ id: "a" }]} router={{}} />
+  <ImageNav
+    imageId="a"
+    images={[{ id: "a" }]}
+    router={{} as unknown as NextRouter}
+  />
 );
 
 export const OneWrongImage = () => (
-  <ImageNav imageId="a" images={[{ id: "b" }]} router={{}} />
+  <ImageNav
+    imageId="a"
+    images={[{ id: "b" }]}
+    router={{} as unknown as NextRouter}
+  />
 );
 
 export const Basic1 = () => (
   <ImageNav
     imageId="a"
     images={[{ id: "a" }, { id: "b" }, { id: "c" }]}
-    router={{}}
+    router={{} as unknown as NextRouter}
   />
 );
 
@@ -36,7 +50,7 @@ export const Basic2 = () => (
   <ImageNav
     imageId="b"
     images={[{ id: "a" }, { id: "b" }, { id: "c" }]}
-    router={{}}
+    router={{} as unknown as NextRouter}
   />
 );
 
@@ -44,7 +58,7 @@ export const Basic3 = () => (
   <ImageNav
     imageId="c"
     images={[{ id: "a" }, { id: "b" }, { id: "c" }]}
-    router={{}}
+    router={{} as unknown as NextRouter}
   />
 );
 
@@ -52,6 +66,6 @@ export const BasicWrongImage = () => (
   <ImageNav
     imageId="d"
     images={[{ id: "a" }, { id: "b" }, { id: "c" }]}
-    router={{}}
+    router={{} as unknown as NextRouter}
   />
 );
