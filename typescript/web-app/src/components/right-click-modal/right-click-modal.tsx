27285d1307d380ecc8b376b6fbc8648f10cb7c76
Etienne Dupont <etienne.dupont@sterblue.com>
2021-05-28 18:12:29 +0100

    Play around

--- typescript/web-app/src/components/right-click-modal/right-click-modal.tsx
+++ typescript/web-app/src/components/right-click-modal/right-click-modal.tsx
@@ -4,6 +4,8 @@ import {
   Box,
   Stack,
   Heading,
+  IconButton,
+  Editable,
   Modal,
   ModalOverlay,
   ModalContent,
@@ -16,8 +18,68 @@ import {
   Tr,
   Td,
   Text,
+  EditableInput,
+  EditablePreview,
+  SimpleGrid
 } from "@chakra-ui/react";
-import Downshift from "downshift";
+import { BsChevronDown } from "react-icons/bs";
+import { useCombobox } from "downshift";
+
+const items = ["person", "dog", "car", "cycle", "plane"];
+
+export const DropdownCombobox = () => {
+  const [inputItems, setInputItems] = useState(items);
+  const {
+    isOpen,
+    getToggleButtonProps,
+    getLabelProps,
+    getMenuProps,
+    getInputProps,
+    getComboboxProps,
+    highlightedIndex,
+    getItemProps,
+  } = useCombobox({
+    items: inputItems,
+    onInputValueChange: ({ inputValue }) => {
+      setInputItems(
+        items.filter((item) =>
+          item.toLowerCase().startsWith(inputValue.toLowerCase())
+        )
+      );
+    },
+  });
+  return (
+    <Box>
+      <SimpleGrid {...getComboboxProps()} columns={2}>
+        <Editable defaultValue="abcd" width="50%">
+          <EditablePreview />
+          <EditableInput {...getInputProps()} />
+        </Editable>
+        <IconButton
+          type="button"
+          {...getToggleButtonProps()}
+          aria-label="toggle menu"
+          icon={<BsChevronDown />}
+          background="white"
+        />
+      </SimpleGrid>
+      <ul {...getMenuProps()}>
+        {isOpen &&
+          inputItems.map((item, index) => (
+            <li
+              style={
+                highlightedIndex === index ? { backgroundColor: "#bde4ff" } : {}
+              }
+              key={`${item}${index}`}
+              {...getItemProps({ item, index })}
+            >
+              {item}
+            </li>
+          ))}
+      </ul>
+    </Box>
+  );
+};
 
 export const RightClickModal = ({
   isOpen = false,
@@ -35,15 +97,6 @@ export const RightClickModal = ({
     >
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader textAlign="center" padding="6">
-          <Heading as="h2" size="lg" pb="2">
-            TODO
-          </Heading>
-          <Text fontSize="lg" fontWeight="medium">
-            Implement combo box with downshift JS
-          </Text>
-        </ModalHeader>
-        <ModalCloseButton />
         <ModalBody
           display="flex"
           pt="0"
@@ -52,7 +105,12 @@ export const RightClickModal = ({
           pl="6"
           overflowY="hidden"
           flexDirection="column"
-        ></ModalBody>
+        >
+          <Text fontSize="lg" fontWeight="medium">
+            Implement combo box with downshift JS
+          </Text>
+          <DropdownCombobox />
+        </ModalBody>
       </ModalContent>
     </Modal>
   );
