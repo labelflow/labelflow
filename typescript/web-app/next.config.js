920f94b67213bda29015d67fc50d1c003013c9f2
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2021-05-31 12:58:30 +0100

    Docs

--- typescript/web-app/next.config.js
+++ typescript/web-app/next.config.js
@@ -21,8 +21,11 @@ module.exports = {
     });
 
     // Transpile other packages of the monorepo
+    // E.g.: `@labelflow/react-openlayers-fiber`
     // See https://github.com/vercel/next.js/blob/0af3b526408bae26d6b3f8cab75c4229998bf7cb/test/integration/typescript-workspaces-paths/packages/www/next.config.js
+    // The root folder of the monorepo:
     const resolvedBaseUrl = path.resolve(config.context, '../../')
+    // We add a rule to transpile files in this folder, except files in `node_modules`
     config.module.rules = [
       ...config.module.rules,
       {
@@ -37,16 +40,17 @@ module.exports = {
     ];
 
     // Transpile node-modules packages that provides no nodejs compatible files
-    // E.g.: ol
+    // E.g.: `ol`
     // See https://github.com/vercel/next.js/issues/9890
     // See https://github.com/openlayers/openlayers/issues/10470
     // See https://github.com/vercel/next.js/blob/bd589349d2a90c41e7fc9549ea2438febfc9a510/packages/next/build/webpack-config.ts#L637
+    // To do this we modify the `externals` option of webpack:
     config.externals = config.externals.map(external => {
       if (!(typeof external === 'function')) {
-        // `externals` options that are hardcoded strings and arrays are used diretly
+        // `externals` options that are hardcoded strings and arrays are used directly
         return external
       } else {
-        // Override `externals` options that are functions
+        // `externals` options that are functions are overridden, to force externalize of the packages we want
         const isWebpack5 = nextConfig.future.webpack5;
         if (isWebpack5) {
           throw new Error("Webpack 5 not yet supported, check next.config.js")
@@ -54,10 +58,10 @@ module.exports = {
           // Return a webpack4-like `externals` option function
           return (context, request, callback) => {
             if (/^ol/.test(request)) {
-              // Make an exception for ol, never externalize this import, it must be transpiled and bundled
+              // Make an exception for `ol`, never externalize this import, it must be transpiled and bundled
               callback();
             } else {
-              // Use the normal NextJS externals function
+              // Use the standard NextJS `externals` function
               external(context, request, callback)
             }
           }
