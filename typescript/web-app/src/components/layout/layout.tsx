8aede473bb5f4119e68bb177ff46c3403d8d61d2
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2021-05-31 15:17:53 +0100

    Comment

--- typescript/web-app/src/components/layout/layout.tsx
+++ typescript/web-app/src/components/layout/layout.tsx
@@ -14,7 +14,15 @@ export const Layout = ({ children, topBarLeftContent }: Props) => {
     <ChakraProvider theme={theme} resetCSS>
       <Flex direction="column" h="100vh">
         <TopBar leftContent={topBarLeftContent} />
-        <Box as="main" bg="gray.100" flex="1" position="relative">
+        <Box
+          as="main"
+          bg="gray.100"
+          flex="1"
+          // Position relative to allow children to use `height: 100%` if they want to take the whole height
+          // E.g. Openlayers uses `height: 100%` to have the right size. without this, openlayers would have
+          // the same height as the screen, and be too big, this would allow the user to scroll down weirdly.
+          position="relative"
+        >
           {children}
         </Box>
       </Flex>
