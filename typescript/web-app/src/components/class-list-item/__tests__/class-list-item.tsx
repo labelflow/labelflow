6bf7ea26178e82a8c72c1b664e34f674a27f01c0
Etienne Dupont <etienne.dupont@sterblue.com>
2021-06-02 12:07:50 +0100

    Add tests on class-list-item

---
+++ typescript/web-app/src/components/class-list-item/__tests__/class-list-item.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom/extend-expect";
+
+import { ClassListItem } from "../class-list-item";
+
+const classDefault = { color: "#00ff00", name: "someClass", shortCut: "1" };
+const classCreate = { type: "CreateClassItem", name: "someClass" };
+
+test("should display class name", () => {
+  render(
+    <ClassListItem
+      highlight={false}
+      index={0}
+      item={classDefault}
+      itemProps={{}}
+    />
+  );
+  expect(screen.queryByText(/someClass/i)).toBeInTheDocument();
+  expect(screen.queryByText(/Create class/i)).not.toBeInTheDocument();
+});
+
+test("should propose to create class", () => {
+  render(
+    <ClassListItem
+      highlight={false}
+      index={0}
+      item={classCreate}
+      itemProps={{}}
+    />
+  );
+  expect(screen.queryByText(/someClass/i)).toBeInTheDocument();
+  expect(screen.queryByText(/Create class/i)).toBeInTheDocument();
+});
