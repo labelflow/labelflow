f3330cb8c4230d427db716014f7b30dcc522805d
Etienne Dupont <etienne.dupont@sterblue.com>
2021-06-02 18:31:25 +0100

    Test refinement

--- python/iog/eval_refinement_on_image.py
+++ python/iog/eval_refinement_on_image.py
@@ -29,6 +29,44 @@ import cv2
 import argparse
 
 image_name = "data/VOCdevkit/VOC2012/JPEGImages/2009_000366.jpg"
+roi = (136, 147, 200, 193)
+# refinement_points_outside = [(233, 309), (292, 288)]
+refinement_points_outside = [(0, 0)]
+
+
+def print_mask(outputs, name, gt, image, im_rgb):
+    pred = np.transpose(outputs.data.numpy()[0, :, :, :], (1, 2, 0))
+    pred = 1 / (1 + np.exp(-pred))
+    pred = np.squeeze(pred)
+    gt = tens2image(gt)
+    bbox = get_bbox(gt, pad=30, zero_pad=True)
+    result = crop2fullmask(pred, bbox, gt, zero_pad=True, relax=0, mask_relax=False)
+
+    light = np.zeros_like(image)
+    light[:, :, 2] = 255.0
+
+    alpha = 0.5
+
+    blending = (alpha * light + (1 - alpha) * image) * result[..., None] + (
+        1 - result[..., None]
+    ) * image
+
+    blending[blending > 255.0] = 255
+
+    # find contours
+    # print("result shape: ", result.shape)
+    # im_mask = cv2.cvtColor(result.astype(np.uint8), cv2.COLOR_BGR2GRAY)
+    im_mask = (result * 255).astype(np.uint8)
+    ret, thresh = cv2.threshold(im_mask, 127, 255, 0)
+    # kernel = np.ones((5, 5), np.uint8)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 1))
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=1)
+    contours, hierarchy = cv2.findContours(
+        opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+    )
+    # print("contours", contours)
+    cv2.drawContours(im_rgb, contours, -1, (0, 255, 0), 3)
+    cv2.imwrite(f"{name}.png", im_rgb)
 
 
 def process():
@@ -65,8 +103,16 @@ def process():
 
     image = np.array(Image.open(image_name).convert("RGB"))
     im_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    roi = (136, 147, 200, 193)
+
     image = image.astype(np.float32)
+    len_x, len_y, _ = image.shape
+    refinement_points_outside_scaled = list(
+        map(
+            lambda tup: (tup[0] * 512 / len_x, tup[1] * 512 / len_y),
+            refinement_points_outside,
+        )
+    )
+    print("refinement_points_outside_scaled = ", refinement_points_outside_scaled)
 
     bbox = np.zeros_like(image[..., 0])
     # bbox[0: 130, 220: 320] = 1 # for ponny
@@ -104,43 +150,43 @@ def process():
     tr_sample = trns(sample)
 
     inputs = tr_sample["concat"][None]
+    IOG_points = tr_sample["IOG_points"].unsqueeze(0)
     # inputs = inputs.to(device)
-    outputs = net.inference(inputs, tr_sample["IOG_points"].unsqueeze(0))[
-        -1
-    ]  # We just keep the fine net output
+    res = net.inference(inputs, IOG_points)
+    outputs = res[-1]  # fine net output
+    backbone_features = res[0]  # output of resnet
     # outputs = fine_out.to(torch.device('cpu'))
-    pred = np.transpose(outputs.data.numpy()[0, :, :, :], (1, 2, 0))
-    pred = 1 / (1 + np.exp(-pred))
-    pred = np.squeeze(pred)
-    gt = tens2image(tr_sample["gt"])
-    bbox = get_bbox(gt, pad=30, zero_pad=True)
-    result = crop2fullmask(pred, bbox, gt, zero_pad=True, relax=0, mask_relax=False)
 
-    light = np.zeros_like(image)
-    light[:, :, 2] = 255.0
-
-    alpha = 0.5
-
-    blending = (alpha * light + (1 - alpha) * image) * result[..., None] + (
-        1 - result[..., None]
-    ) * image
-
-    blending[blending > 255.0] = 255
-
-    # find contours
-    # print("result shape: ", result.shape)
-    # im_mask = cv2.cvtColor(result.astype(np.uint8), cv2.COLOR_BGR2GRAY)
-    im_mask = (result * 255).astype(np.uint8)
-    ret, thresh = cv2.threshold(im_mask, 127, 255, 0)
-    # kernel = np.ones((5, 5), np.uint8)
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 1))
-    opening = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=1)
-    contours, hierarchy = cv2.findContours(
-        opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+    # Save result without refinements
+    print_mask(
+        outputs,
+        "result_without_refinement",
+        tr_sample["gt"],
+        image,
+        im_rgb,
     )
-    # print("contours", contours)
-    cv2.drawContours(im_rgb, contours, -1, (0, 255, 0), 3)
-    cv2.imwrite("result.png", im_rgb)
+    # Generate results with refinement
+    for index, point in enumerate(refinement_points_outside_scaled):
+        # one result
+        print("IOG_points shape: ", IOG_points.shape)
+        points_center = IOG_points[0, 0:1, :, :]
+        points_bg = IOG_points[0, 1:2, :, :]
+        points_bg = 255 * np.maximum(
+            points_bg / 255,
+            make_gaussian(
+                (IOG_points.shape[2], IOG_points.shape[3]), center=point, sigma=10
+            ),
+        )
+        IOG_points[0, 1:2, :, :] = points_bg
+        outputs = net.refine(backbone_features, IOG_points)
+        # Save result without refinements
+        print_mask(
+            outputs,
+            f"result_with_refinement_{index}",
+            tr_sample["gt"],
+            image,
+            im_rgb,
+        )
     # cv2.imwrite('resulting segmentation', cv2.cvtColor(blending.astype(np.uint8), cv2.COLOR_RGB2BGR))
     cv2.waitKey(0)
     cv2.destroyAllWindows()
