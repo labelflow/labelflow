3aaea086d1cba514a418c691033c30f40181b4e9
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2021-06-02 09:54:59 +0100

    Remove keymap

--- typescript/web-app/src/keymap.ts
+++ typescript/web-app/src/keymap.ts
@@ -17,19 +17,4 @@ export const keymap = {
     description: "Navigate to the next image",
     category: "Navigation",
   },
-  esc: {
-    key: "esc",
-    description: "Cancel current action",
-    category: "General",
-  },
-  undo: {
-    key: "command+z,ctrl+z",
-    description: "Undo",
-    category: "General",
-  },
-  redo: {
-    key: "command+y,ctrl+y,command+shift+z,ctrl+shift+z",
-    description: "Redo",
-    category: "General",
-  },
 };
