8b5e1491e426c71f37a29ee6188ff671a303aea1
ShauniArima <anderfriou@protonmail.com>
2021-06-03 16:18:11 +0200

    feature: Start export label and label classes

--- typescript/web-app/src/connectors/apollo-client/resolvers/__tests__/export-to-coco.ts
+++ typescript/web-app/src/connectors/apollo-client/resolvers/__tests__/export-to-coco.ts
@@ -1,6 +1,15 @@
 import "fake-indexeddb/auto";
 import gql from "graphql-tag";
+import { v4 as uuidv4 } from "uuid";
 import { client } from "../../index";
+import {
+  exportLabelClassToCoco,
+  CocoCategory,
+  exportLabelClassesToCoco,
+  CocoAnnotation,
+  exportLabelToCoco,
+} from "../export-to-coco";
+import type { Label, LabelClass } from "../../../../types.generated";
 
 /**
  * We bypass the structured clone algorithm as its current js implementation
@@ -14,6 +23,84 @@ jest.mock("fake-indexeddb/build/lib/structuredClone", () => ({
 
 beforeAll(() => {
   global.URL.createObjectURL = jest.fn(() => "mockedUrl");
+});
+
+describe("to found", () => {
+  const date = new Date("1995-12-17T03:24:00");
+
+  const createLabelClass = (name: string): LabelClass => ({
+    id: uuidv4(),
+    createdAt: date,
+    updatedAt: date,
+    name,
+    color: "#000000",
+    labels: [],
+  });
+
+  test("Should create coco json category from a label class", () => {
+    const myLabelClass = createLabelClass("My Label Class");
+
+    const cocoCategory = exportLabelClassToCoco(myLabelClass, 1);
+
+    const expectedCocoCategory: CocoCategory = {
+      id: 1,
+      name: "My Label Class",
+      supercategory: "",
+    };
+
+    expect(cocoCategory).toEqual(expectedCocoCategory);
+  });
+
+  test("Should create coco json categories from label classes", () => {
+    const labelClassList = [
+      createLabelClass("Class 1"),
+      createLabelClass("Class 2"),
+    ];
+
+    const cocoCategories = exportLabelClassesToCoco(labelClassList);
+
+    const expectedCocoCategories: CocoCategory[] = [
+      {
+        id: 1,
+        name: "Class 1",
+        supercategory: "",
+      },
+      {
+        id: 2,
+        name: "Class 2",
+        supercategory: "",
+      },
+    ];
+
+    expect(cocoCategories).toEqual(expectedCocoCategories);
+  });
+
+  test("Should create coco json annotation from a label without label class", () => {
+    const label: Label = {
+      id: "test",
+      createdAt: date,
+      updatedAt: date,
+      height: 4,
+      imageId: "image-1",
+      width: 2,
+      x: 0,
+      y: 1,
+    };
+
+    const cocoAnnotation = exportLabelToCoco(label, 1, 42, 0);
+
+    const expectedAnnotation: CocoAnnotation = {
+      id: 1,
+      image_id: 42,
+      category_id: 0,
+      segmentation: [],
+      area: 8,
+      bbox: [0, 1, 2, 4],
+      iscrowd: 0,
+    };
+
+    expect(cocoAnnotation).toEqual(expectedAnnotation);
+  });
 });
 
 describe("Exporting a dataset to coco format", () => {
--- typescript/web-app/src/connectors/apollo-client/resolvers/export-to-coco.ts
+++ typescript/web-app/src/connectors/apollo-client/resolvers/export-to-coco.ts
@@ -1,3 +1,64 @@
+import { Label, LabelClass } from "../../../types.generated";
+
+/**
+ * Query tout les labelclass -> categories (cache: Map<uuid, id>);
+ * Query toutes les images avec labels -> annotations + images (reducer (etatCourantDataset, labelClass ou image) -> nouveauDataset);
+ */
+
+export type CocoCategory = {
+  id: number;
+  name: string;
+  supercategory: string;
+};
+
+type Polygon = number[][];
+
+export type CocoAnnotation = {
+  id: number;
+  image_id: number;
+  category_id: number;
+  segmentation: string | Polygon;
+  area: number;
+  bbox: [x: number, y: number, width: number, height: number];
+  iscrowd: 0 | 1;
+};
+
+export const exportLabelClassToCoco = (
+  labelClass: LabelClass,
+  id: number
+): CocoCategory => {
+  return {
+    id,
+    name: labelClass.name,
+    supercategory: "",
+  };
+};
+
+export const exportLabelClassesToCoco = (
+  labelClasses: LabelClass[]
+): CocoCategory[] => {
+  return labelClasses.map((value, index) =>
+    exportLabelClassToCoco(value, index + 1)
+  );
+};
+
+export const exportLabelToCoco = (
+  { x, y, width, height }: Label,
+  id: number,
+  imageId: number,
+  categoryId: number
+): CocoAnnotation => {
+  return {
+    id,
+    image_id: imageId,
+    category_id: categoryId,
+    segmentation: [],
+    area: width * height,
+    bbox: [x, y, width, height],
+    iscrowd: 0,
+  };
+};
+
 const exportToCoco = async (_: any): Promise<String | undefined> => {
   return "{}";
 };
