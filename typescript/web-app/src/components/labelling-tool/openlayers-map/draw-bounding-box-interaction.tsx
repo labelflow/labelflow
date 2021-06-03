96d3ef325c26557560e03655c3ff87dbd8626c07
Hugo Raynal <raynalhugo@gmail.com>
2021-06-03 15:52:09 +0200

    WIP: Beginning of the createLabelEffect implementation

--- typescript/web-app/src/components/labelling-tool/openlayers-map/draw-bounding-box-interaction.tsx
+++ typescript/web-app/src/components/labelling-tool/openlayers-map/draw-bounding-box-interaction.tsx
@@ -1,11 +1,55 @@
-import { createBox } from "ol/interaction/Draw";
+import { createBox, DrawEvent } from "ol/interaction/Draw";
 import GeometryType from "ol/geom/GeometryType";
+import gql from "graphql-tag";
 import { useLabellingStore, Tools } from "../../../connectors/labelling-state";
+import { useUndoStore, Effect } from "../../../connectors/undo-store";
+import { client } from "../../../connectors/apollo-client";
+
+type Props = { imageId: string };
+
+const createLabelMutation = gql`
+  mutation createLabel(
+    $imageId: ID!
+    $x: Float!
+    $y: Float!
+    $width: Int!
+    $height: Int!
+  ) {
+    createLabel(
+      data: { imageId: $imageId, x: $x, y: $y, width: $width, height: $height }
+    ) {
+      id
+    }
+  }
+`;
+
+const createLabelEffect = ({
+  imageId,
+  x,
+  y,
+  width,
+  height,
+}: {
+  imageId: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}): Effect => ({
+  do: () => {
+    client.mutate({
+      mutation: createLabelMutation,
+      variables: { imageId, x, y, width, height },
+    });
+  },
+  undo: () => console.log(imageId),
+});
 
 const geometryFunction = createBox();
 
-export const DrawBoundingBoxInteraction = () => {
+export const DrawBoundingBoxInteraction = ({ imageId }: Props) => {
   const selectedTool = useLabellingStore((state) => state.selectedTool);
+  const { perform } = useUndoStore();
 
   if (selectedTool !== Tools.BOUNDING_BOX) {
     return null;
@@ -17,8 +61,19 @@ export const DrawBoundingBoxInteraction = () => {
         type: GeometryType.CIRCLE,
         geometryFunction,
       }}
-      onDrawend={(drawEvent) => {
-        console.log(drawEvent);
+      onDrawend={(drawEvent: DrawEvent) => {
+        const [x, y, destX, destY] = drawEvent.feature
+          .getGeometry()
+          .getExtent();
+        perform(
+          createLabelEffect({
+            imageId,
+            x,
+            y,
+            width: Math.floor(destX - x),
+            height: Math.floor(destY - y),
+          })
+        );
         return false;
       }}
     />
