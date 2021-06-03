2e5275fe3234bbcaf10f91b0ca23af381908c02d
Yvonnick Frin <frin.yvonnick@gmail.com>
2021-06-03 14:41:47 +0200

    Fix lint

--- typescript/web-app/src/components/labelling-tool/openlayers-map/draw-bounding-box-interaction.tsx
+++ typescript/web-app/src/components/labelling-tool/openlayers-map/draw-bounding-box-interaction.tsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
 import { createBox } from "ol/interaction/Draw";
-import OlSourceVector from "ol/source/Vector";
 import GeometryType from "ol/geom/GeometryType";
 import { useLabellingStore, Tools } from "../../../connectors/labelling-state";
 
-type Props = { imageId: string };
-
 const geometryFunction = createBox();
 
-export const DrawBoundingBoxInteraction = ({}: Props) => {
+export const DrawBoundingBoxInteraction = () => {
   const selectedTool = useLabellingStore((state) => state.selectedTool);
 
   if (selectedTool !== Tools.BOUNDING_BOX) {
