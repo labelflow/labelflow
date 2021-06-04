0edbc77241ce6aa30d9b6f532d0336d8fee7caba
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2021-06-04 14:33:21 +0100

    wtf

--- typescript/web-app/src/graphql-types.generated.ts
+++ typescript/web-app/src/graphql-types.generated.ts
@@ -1,5 +1,4 @@
 import { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';
-
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
