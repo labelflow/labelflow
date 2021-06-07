9c48d38e4d80734b194ebfdd0a0dfeb2542bc035
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2021-06-07 13:33:36 +0100

    Some last fixes

--- typescript/web-app/src/components/import-button/import-images-modal/modal-url-list/url-list.tsx
+++ typescript/web-app/src/components/import-button/import-images-modal/modal-url-list/url-list.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, useState } from "react";
 import { Stack, VStack, Button, Textarea } from "@chakra-ui/react";
 import { isWebUri } from "valid-url";
-
+import { isEmpty } from "lodash/fp";
 import { DroppedUrl } from "../types";
 
 export const UrlList = ({
@@ -9,9 +9,7 @@ export const UrlList = ({
 }: {
   onDropEnd: (images: Array<DroppedUrl>) => void;
 }) => {
-  const [value, setValue] = useState<string>(
-    "https://images.unsplash.com/photo-1593642634443-44adaa06623a?auto=format&fit=crop&w=600&q=80\nhttps://images.unsplash.com/photo-1611095790444-1dfa35e37b52?auto=format&fit=crop&w=600&q=80\nhttps://images.unsplash.com/photo-1622832148332-b436648cef62?auto=format&fit=crop&w=600&q=80"
-  );
+  const [value, setValue] = useState<string>("");
 
   const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const inputValue = e.target.value;
@@ -20,6 +18,16 @@ export const UrlList = ({
 
   return (
     <VStack spacing={4} flex="1" alignItems="stretch">
+      <Button
+        mt={2}
+        onClick={() =>
+          setValue(
+            "https://images.unsplash.com/photo-1593642634443-44adaa06623a?auto=format&fit=crop&w=600&q=80\nhttps://images.unsplash.com/photo-1611095790444-1dfa35e37b52?auto=format&fit=crop&w=600&q=80\nhttps://images.unsplash.com/photo-1622832148332-b436648cef62?auto=format&fit=crop&w=600&q=80"
+          )
+        }
+      >
+        Insert example images
+      </Button>
       <Stack
         as="form"
         border="1px dashed"
@@ -33,7 +41,7 @@ export const UrlList = ({
           border="none"
           value={value}
           onChange={handleInputChange}
-          placeholder="Enter your urls here, one per line\nE.g:\nhttp://"
+          placeholder="Enter your image URLs here, one per line. Example:&#13;&#10;https://example.com/image1.jpeg&#13;&#10;https://example.com/image2.jpeg"
           size="sm"
           h="full"
           resize="none"
@@ -42,12 +50,15 @@ export const UrlList = ({
       <Button
         onClick={() => {
           onDropEnd(
-            value.split("\n").map((url) => {
-              if (!isWebUri(url)) {
-                return { url, errors: [new Error("Invalid URL")] };
-              }
-              return { url, errors: [] };
-            })
+            value
+              .split("\n")
+              .filter((line) => !isEmpty(line))
+              .map((url) => {
+                if (!isWebUri(url)) {
+                  return { url, errors: [new Error("Invalid URL")] };
+                }
+                return { url, errors: [] };
+              })
           );
         }}
       >
