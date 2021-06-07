366749a0d624c35efbd3448543b8549598a1603f
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-06-07 17:03:49 +0100

    Display in select class menu the selected label class in button

--- typescript/web-app/src/components/class-selection-menu/class-selection-menu.tsx
+++ typescript/web-app/src/components/class-selection-menu/class-selection-menu.tsx
@@ -10,11 +10,17 @@ import {
   Flex,
   Button,
   chakra,
+  useDisclosure,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { HiSelector } from "react-icons/hi";
+import { RiCheckboxBlankCircleFill } from "react-icons/ri";
+import { omit } from "lodash/fp";
 import { LabelClass } from "../../graphql-types.generated";
+import { ClassListItem } from "../class-list-item";
 import { ClassSelectionPopover } from "../class-selection-popover";
+
+const CircleIcon = chakra(RiCheckboxBlankCircleFill);
 
 const SelectorIcon = chakra(HiSelector);
 
@@ -22,29 +28,44 @@ export const ClassSelectionMenu = ({
   labelClasses,
   onSelectedClassChange,
   createNewClass,
+  selectedLabelClass,
 }: {
   labelClasses: LabelClass[];
   onSelectedClassChange: (item: LabelClass) => void;
   createNewClass: (name: string) => void;
+  selectedLabelClass: LabelClass | null;
 }) => {
-  const [isOpen, setIsOpen] = useState(false);
-  const toggleOpen = () => {
-    setIsOpen(!isOpen);
-  };
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const selectedLabelClassName = selectedLabelClass?.name;
 
   return (
     <Box pl="3" pr="3">
-      <Button onClick={toggleOpen} pl="5" pr="5">
-        <Flex>
-          <Text>Test</Text>
-          <Spacer />
-          <SelectorIcon fontSize="md" />
+      <Button
+        onClick={onOpen}
+        rightIcon={<SelectorIcon fontSize="md" />}
+        pl="5"
+        pr="5"
+        minW="60"
+        justifyContent="space-between"
+        pointerEvents={isOpen ? "none" : "auto"}
+      >
+        <Flex alignItems="center">
+          <CircleIcon
+            color={selectedLabelClass?.color ?? "gray.300"}
+            fontSize="2xl"
+            mr="2"
+          />
+          <Text>{selectedLabelClassName ?? "None"}</Text>
         </Flex>
       </Button>
       <ClassSelectionPopover
         isOpen={isOpen}
+        onClose={onClose}
         labelClasses={labelClasses}
-        onSelectedClassChange={onSelectedClassChange}
+        onSelectedClassChange={(labelClass: LabelClass) => {
+          onSelectedClassChange(labelClass);
+          onClose();
+        }}
         createNewClass={createNewClass}
       />
     </Box>
