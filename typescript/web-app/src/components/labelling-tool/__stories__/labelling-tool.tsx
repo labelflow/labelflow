3a0c8387444cd6a9733fc5d08a367cd414ed2f21
Hugo Raynal <raynalhugo@gmail.com>
2021-06-07 14:33:43 +0200

    Update stories to properly load 3 images

--- typescript/web-app/src/components/labelling-tool/__stories__/labelling-tool.tsx
+++ typescript/web-app/src/components/labelling-tool/__stories__/labelling-tool.tsx
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import { useEffect, useState } from "react";
 import gql from "graphql-tag";
-import { addDecorator, Story } from "@storybook/react";
+import { DecoratorFn, Story } from "@storybook/react";
 import { NextRouter } from "next/router";
 import { Box } from "@chakra-ui/react";
 
@@ -8,17 +9,41 @@ import { client } from "../../../connectors/apollo-client";
 import { chakraDecorator } from "../../../utils/chakra-decorator";
 import { apolloDecorator } from "../../../utils/apollo-decorator";
 
-import { LabellingTool, Props } from "../labelling-tool";
-
-addDecorator(chakraDecorator);
-addDecorator(apolloDecorator);
+import { LabellingTool } from "../labelling-tool";
+import { Image } from "../../../graphql-types.generated";
 
 export default {
   title: "web-app/Labelling Tool",
   component: LabellingTool,
+  decorators: [
+    chakraDecorator,
+    apolloDecorator,
+    mockImagesDecorator,
+    inGreyBoxDecorator,
+  ],
 };
 
-const createImage = async (name: String, file: Blob) => {
+const fakeRouter = {
+  push: (x: string) => console.log(`Navigate to ${x}`),
+} as unknown as NextRouter;
+
+export const OneImage: Story = (_, { images }) => {
+  return (
+    <LabellingTool
+      images={images.slice(0, 1)}
+      image={images[0]}
+      router={fakeRouter}
+    />
+  );
+};
+
+export const ThreeImages: Story = (_, { images }) => {
+  return (
+    <LabellingTool images={images} image={images[0]} router={fakeRouter} />
+  );
+};
+
+async function createImage(name: String, file: Blob) {
   const mutationResult = await client.mutate({
     mutation: gql`
       mutation createImage($file: Upload!, $name: String!) {
@@ -42,40 +67,50 @@ const createImage = async (name: String, file: Blob) => {
   } = mutationResult;
 
   return image;
-};
+}
 
-addDecorator((StoryComponent) => {
-  const [image, setImage] = useState();
+function mockImagesDecorator(
+  StoryComponent: Parameters<DecoratorFn>[0],
+  StoryContext: Parameters<DecoratorFn>[1]
+): ReturnType<DecoratorFn> {
+  const [images, setImages] = useState<Array<Image>>();
+
   useEffect(() => {
-    fetch(
-      "https://images.unsplash.com/photo-1612564148954-59545876eaa0?auto=format&fit=crop&w=600&q=80"
-    )
-      .then((res) => res.blob())
-      .then((blob) => createImage("Hello puffin", blob))
-      .then(setImage);
+    Promise.all(
+      [
+        {
+          name: "Hello puffin 1",
+          url: "https://images.unsplash.com/photo-1612564148954-59545876eaa0?auto=format&fit=crop&w=600&q=80",
+        },
+        {
+          name: "Hello puffin 2",
+          url: "https://images.unsplash.com/photo-1580629905303-faaa03202631?auto=format&fit=crop&w=600&q=80",
+        },
+        {
+          name: "Hello puffin 3",
+          url: "https://images.unsplash.com/photo-1490718720478-364a07a997cd?auto=format&fit=crop&w=600&q=80",
+        },
+      ].map(({ url, name }) =>
+        fetch(url)
+          .then((res) => res.blob())
+          .then((blob) => createImage(name, blob))
+      )
+    ).then(setImages);
   }, []);
-  return image ? (
-    <StoryComponent image={image} images={[{ id: image.id }]} />
-  ) : null;
-});
 
-const Template: Story<Props> = (args: Props, context) => (
-  <Box background="gray.100" width="640px" height="480px">
-    <LabellingTool {...context} {...args} />
-  </Box>
-);
+  return images ? (
+    <StoryComponent {...StoryContext} images={images} />
+  ) : (
+    <div>Loading mocked images into storybook...</div>
+  );
+}
 
-const router = {
-  push: (x: string) => console.log(`Navigate to ${x}`),
-} as unknown as NextRouter;
-
-export const OneImage = Template.bind({});
-OneImage.args = {
-  router,
-};
-
-export const ThreeImages = Template.bind({});
-ThreeImages.args = {
-  images: [{ id: "titi" }, { id: "toto" }, { id: "tata" }],
-  router,
-};
+function inGreyBoxDecorator(
+  storyFn: Parameters<DecoratorFn>[0]
+): ReturnType<DecoratorFn> {
+  return (
+    <Box background="gray.100" width="640px" height="480px">
+      {storyFn()}
+    </Box>
+  );
+}
