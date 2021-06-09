809c456e1470426df302c4d98bd27da6231d9512
Yvonnick Frin <frin.yvonnick@gmail.com>
2021-06-09 14:42:00 +0200

    Implement undo redo in delete label without tests

--- typescript/web-app/src/components/labelling-tool/openlayers-map/labels.tsx
+++ typescript/web-app/src/components/labelling-tool/openlayers-map/labels.tsx
@@ -8,6 +8,7 @@ import { useHotkeys } from "react-hotkeys-hook";
 import { client } from "../../../connectors/apollo-client";
 import { keymap } from "../../../keymap";
 import { useLabellingStore } from "../../../connectors/labelling-state";
+import { useUndoStore, Effect } from "../../../connectors/undo-store";
 import { Label } from "../../../graphql-types.generated";
 
 const getImageLabelsQuery = gql`
@@ -24,19 +25,69 @@ const getImageLabelsQuery = gql`
   }
 `;
 
-const deleteLabel = gql`
+const deleteLabelMutation = gql`
   mutation deleteLabel($id: ID!) {
     deleteLabel(where: { id: $id }) {
+      id
+      x
+      y
+      width
+      height
+      imageId
+    }
+  }
+`;
+
+const createLabelMutation = gql`
+  mutation createLabel(
+    $imageId: ID!
+    $x: Float!
+    $y: Float!
+    $width: Float!
+    $height: Float!
+  ) {
+    createLabel(
+      data: { imageId: $imageId, x: $x, y: $y, width: $width, height: $height }
+    ) {
       id
     }
   }
 `;
+
+const createDeleteLabelEffect = (
+  { id }: { id: string },
+  {
+    setSelectedLabelId,
+  }: { setSelectedLabelId: (labelId: string | null) => void }
+): Effect => ({
+  do: async (labelId = id) => {
+    const { data } = await client.mutate({
+      mutation: deleteLabelMutation,
+      variables: { id: labelId },
+      refetchQueries: ["getImageLabels"],
+    });
+    setSelectedLabelId(null);
+    return data?.deleteLabel;
+  },
+  undo: async (deletedLabel) => {
+    const { data } = await client.mutate({
+      mutation: createLabelMutation,
+      variables: deletedLabel,
+      refetchQueries: ["getImageLabels"],
+    });
+
+    setSelectedLabelId(data?.createLabel?.id);
+
+    return data?.createLabel?.id;
+  },
+});
 
 export const Labels = () => {
   const selectedLabelId = useLabellingStore((state) => state.selectedLabelId);
   const setSelectedLabelId = useLabellingStore(
     (state) => state.setSelectedLabelId
   );
+  const { perform } = useUndoStore();
   const imageId = useRouter().query?.id;
   const { data } = useQuery(getImageLabelsQuery, {
     skip: typeof imageId !== "string",
@@ -53,16 +104,12 @@ export const Labels = () => {
         return;
       }
 
-      client
-        .mutate({
-          mutation: deleteLabel,
-          variables: { id: selectedLabelId },
-          refetchQueries: ["getImageLabels"],
-        })
-        .then(() => setSelectedLabelId(null));
+      perform(
+        createDeleteLabelEffect({ id: selectedLabelId }, { setSelectedLabelId })
+      );
     },
     {},
-    [selectedLabelId]
+    [selectedLabelId, setSelectedLabelId]
   );
 
   const labels = data?.image?.labels ?? [];
