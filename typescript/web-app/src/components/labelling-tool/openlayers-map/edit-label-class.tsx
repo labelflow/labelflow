d2a120d7fceb1ff1590bdb0fd9b3d11d1ab9b8d9
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-06-09 18:46:37 +0100

    WIP to display label classes from DB and create class from popover

--- typescript/web-app/src/components/labelling-tool/openlayers-map/edit-label-class.tsx
+++ typescript/web-app/src/components/labelling-tool/openlayers-map/edit-label-class.tsx
@@ -2,14 +2,50 @@ import React, { useState } from "react";
 import { MapBrowserEvent, Overlay } from "ol";
 import { SelectEvent } from "ol/interaction/Select";
 import { Coordinate } from "ol/coordinate";
+import { useMutation, useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+
 import { ClassSelectionPopover } from "../../class-selection-popover";
 import { useLabellingStore } from "../../../connectors/labelling-state";
 
+const labelClassesQuery = gql`
+  query getLabelClasses {
+    labelClasses {
+      id
+      name
+      color
+    }
+  }
+`;
+
+const createLabelQuery = gql`
+  mutation createLabelClass($data: LabelClassCreateInput!) {
+    createLabelClass(data: $data) {
+      id
+    }
+  }
+`;
+
 export const EditLabelClass = ({ editClassOverlayRef, isOpen, onClose }) => {
+  const { data } = useQuery(labelClassesQuery);
+  const [createLabelClass] = useMutation(createLabelQuery, {
+    refetchQueries: ["getLabelClasses"],
+  });
+
+  const labelClasses = data?.labelClasses ?? [];
   return (
     <div ref={editClassOverlayRef}>
       {isOpen && (
-        <ClassSelectionPopover isOpen onClose={onClose} labelClasses={[]} />
+        <ClassSelectionPopover
+          isOpen
+          onClose={onClose}
+          labelClasses={labelClasses}
+          createNewClass={(name) =>
+            createLabelClass({
+              variables: { data: { name, color: "#DD3322" } },
+            })
+          }
+        />
       )}
     </div>
   );
