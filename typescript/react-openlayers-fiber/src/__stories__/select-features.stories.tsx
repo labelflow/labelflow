2fbadf6c991568b4e995b119ee9a5f4a9429b450
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-06-09 12:26:26 +0100

    Add story for select features in react-openlayers-fiber

---
+++ typescript/react-openlayers-fiber/src/__stories__/select-features.stories.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import GeoJSON from "ol/format/GeoJSON";
+import { click, pointerMove, altKeyOnly } from "ol/events/condition";
+import { SelectEvent } from "ol/interaction/Select";
+import { MapBrowserEvent } from "ol";
+import { Map } from "../map";
+
+export const SelectFeatures = () => {
+  const [displayText, setDisplayText] = useState("0 selected features");
+  const [selectMethod, setSelectMethod] = useState("singleClick");
+
+  const getSelectCondition = () => {
+    switch (selectMethod) {
+      case "singleClick":
+        return click;
+      case "pointerMove":
+        return pointerMove;
+      case "altClick":
+        return (mapBrowserEvent: MapBrowserEvent) => {
+          return click(mapBrowserEvent) && altKeyOnly(mapBrowserEvent);
+        };
+      default:
+        return undefined;
+    }
+  };
+  return (
+    <>
+      <Map>
+        <olView initialCenter={[0, 0]} initialZoom={2} />
+        <olLayerTile>
+          <olSourceOSM />
+        </olLayerTile>
+        <olLayerVector>
+          <olSourceVector
+            url="https://openlayers.org/en/latest/examples/data/geojson/countries.geojson"
+            format={new GeoJSON()}
+          />
+        </olLayerVector>
+        <olInteractionSelect
+          args={{ condition: getSelectCondition() }}
+          onSelect={(e: SelectEvent) => {
+            setDisplayText(
+              ` ${e.target
+                .getFeatures()
+                .getLength()} selected features (last operation selected ${
+                e.selected.length
+              } and deselected  ${e.deselected.length} features)`
+            );
+          }}
+        />
+      </Map>
+      <form className="form-inline">
+        <label htmlFor="type">Action type &nbsp;</label>
+        <select id="type" onChange={(e) => setSelectMethod(e.target.value)}>
+          <option value="click" selected>
+            Click
+          </option>
+          <option value="singleClick">Single-click</option>
+          <option value="pointerMove">Hover</option>
+          <option value="altClick">Alt+Click</option>
+          <option value="none">None</option>
+        </select>
+        <span>{displayText}</span>
+      </form>
+    </>
+  );
+};
