40780b1cec16c98c213ea696d55e5b7043ddee00
Guilhem de Viry <gdeviry@hotmail.fr>
2021-06-11 14:58:00 +0200

    Add keyboard shortcut to select the bounding box tool

--- typescript/web-app/src/keymap.ts
+++ typescript/web-app/src/keymap.ts
@@ -8,6 +8,11 @@ export type Keymap = {
 
 export const keymap: Keymap = {
   toolSelect: { key: "v", description: "Selection tool", category: "Tools" },
+  toolBoundingBox: {
+    key: "b",
+    description: "Bounding box tool",
+    category: "Tools",
+  },
   goToPreviousImage: {
     key: "left",
     description: "Navigate to the previous image",
