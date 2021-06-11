bb7bbb7b20374bb6a203e75a5279ccfc7a57ea28
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2021-06-11 09:48:29 +0100

    Remove "useless" comments

--- typescript/web-app/src/pages/_app.tsx
+++ typescript/web-app/src/pages/_app.tsx
@@ -67,52 +67,9 @@ function App({ Component, pageProps }: AppProps) {
       // https://developers.google.com/web/tools/workbox/guides/advanced-recipes#offer_a_page_reload_for_users
       const promptNewVersionAvailable = (/* event: any */) => {
         setIsOpen(true);
-
-        // `event.wasWaitingBeforeRegister` will be false if this is the first time the updated service worker is waiting.
-        // When `event.wasWaitingBeforeRegister` is true, a previously updated service worker is still waiting.
-        // You may want to customize the UI prompt accordingly.
-        // if (
-        //   // eslint-disable-next-line no-alert
-        //   window.confirm(
-        //     "A newer version of this web app is available, reload to update?"
-        //   )
-        // ) {
-        // wb.addEventListener("controlling", (/* event: any */) => {
-        //   window.location.reload();
-        // });
-
-        // Send a message to the waiting service worker, instructing it to activate.
-        // wb.messageSkipWaiting();
-        // } else {
-        //   console.log(
-        //     "User rejected to reload the web app, keep using old version. New version will be automatically load when user open the app next time."
-        //   );
-        // }
       };
 
       wb.addEventListener("waiting", promptNewVersionAvailable);
-
-      // // ISSUE - this is not working as expected, why?
-      // // I could only make message event listener work when I manually add this listener into sw.js file
-      // wb.addEventListener("message", (event) => {
-      //   console.log(`Event ${event.type} is triggered.`);
-      //   console.log(event);
-      // });
-
-      /*
-      wb.addEventListener('redundant', event => {
-        console.log(`Event ${event.type} is triggered.`)
-        console.log(event)
-      })
-      wb.addEventListener('externalinstalled', event => {
-        console.log(`Event ${event.type} is triggered.`)
-        console.log(event)
-      })
-      wb.addEventListener('externalactivated', event => {
-        console.log(`Event ${event.type} is triggered.`)
-        console.log(event)
-      })
-      */
 
       // never forget to call register as auto register is turned off in next.config.js
       wb.register();
