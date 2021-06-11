24b3ef556cb4fb242d0ef6ea8a27af8915001bd1
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2021-06-11 09:38:40 +0100

    Fix basile comment

--- typescript/web-app/src/worker/apollo-server-service-worker/index.ts
+++ typescript/web-app/src/worker/apollo-server-service-worker/index.ts
@@ -23,8 +23,5 @@ export type {
   PlaygroundRenderPageOptions,
 } from "apollo-server-core";
 
-// export * from "graphql-tools";
-// export * from "graphql-subscriptions";
-
 // ApolloServer integration.
 export { ApolloServer } from "./apollo-server";
