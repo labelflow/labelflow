16e7435570d326b45813c3fcae7a15e230193b0d
Yvonnick Frin <frin.yvonnick@gmail.com>
2021-06-11 10:42:30 +0200

    Remove persist on labelling state
    It is not really useful for the moment and is source of bugs both in the app and the tests. Furthermore, we discussed about having the url as source of truth for what should be displayed on the UI so we might want to have a query param "selectedLabel".

--- typescript/web-app/src/connectors/labelling-state/index.ts
+++ typescript/web-app/src/connectors/labelling-state/index.ts
@@ -1,5 +1,4 @@
 import create from "zustand";
-import { persist } from "zustand/middleware";
 
 export enum Tools {
   SELECTION = "SELECTION",
@@ -13,16 +12,9 @@ export type LabellingState = {
   setSelectedLabelId: (labelId: string | null) => void;
 };
 
-export const useLabellingStore = create<LabellingState>(
-  persist(
-    (set) => ({
-      selectedTool: Tools.SELECTION,
-      selectedLabelId: null,
-      setSelectedTool: (tool) => set({ selectedTool: tool }),
-      setSelectedLabelId: (labelId) => set({ selectedLabelId: labelId }),
-    }),
-    {
-      name: "labelling-state",
-    }
-  )
-);
+export const useLabellingStore = create<LabellingState>((set) => ({
+  selectedTool: Tools.SELECTION,
+  selectedLabelId: null,
+  setSelectedTool: (tool) => set({ selectedTool: tool }),
+  setSelectedLabelId: (labelId) => set({ selectedLabelId: labelId }),
+}));
