700c4ea1cc2db7b4b180241ab76e624287950fd1
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2021-06-11 10:07:57 +0100

    Refactor duplicate

--- typescript/web-app/src/worker/file-server.ts
+++ typescript/web-app/src/worker/file-server.ts
@@ -3,6 +3,25 @@ import { db } from "../connectors/database";
 
 declare let self: ServiceWorkerGlobalScope;
 
+/**
+ * Given a rex to match URLs, and a request object, output the value at the `fileId` group in the regex.
+ * @param regex
+ * @param request
+ * @returns
+ */
+const getFileIdFromRequest = (
+  regex: RegExp,
+  request: Request
+): string | undefined => {
+  const url = new URL(request.url);
+  const found = url.pathname.match(regex);
+  if (!isString(found?.groups?.fileId)) {
+    return undefined;
+  }
+  const fileId = found?.groups?.fileId;
+  return fileId;
+};
+
 export const server = {
   installListener: (path = "/api/worker/files") => {
     const trimmedPath = trimCharsEnd("/", path);
@@ -12,12 +31,8 @@ export const server = {
       const { request } = event;
 
       if (request.method === "GET") {
-        const url = new URL(request.url);
-        const found = url.pathname.match(regex);
-        if (!isString(found?.groups?.fileId)) {
-          return;
-        }
-        const fileId = found?.groups?.fileId as string;
+        const fileId = getFileIdFromRequest(regex, request);
+        if (!fileId) return;
         const buildResponse = async () => {
           const file = await db.file.get(fileId);
           if (!file || !("blob" in file)) {
@@ -35,12 +50,8 @@ export const server = {
       }
 
       if (request.method === "PUT") {
-        const url = new URL(request.url);
-        const found = url.pathname.match(regex);
-        if (!isString(found?.groups?.fileId)) {
-          return;
-        }
-        const fileId = found?.groups?.fileId as string;
+        const fileId = getFileIdFromRequest(regex, request);
+        if (!fileId) return;
         const buildResponse = async () => {
           const blob = await request.blob();
           await db.file.add({ id: fileId, blob });
