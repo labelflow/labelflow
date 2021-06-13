2cf830c6a045e7422ef91669de91d07dae28f212
Hugo Raynal <raynalhugo@gmail.com>
2021-06-13 13:09:03 +0200

    Slight design adjustments to the format export card

--- typescript/web-app/src/components/export-button/modal/export-format-card.tsx
+++ typescript/web-app/src/components/export-button/modal/export-format-card.tsx
@@ -1,10 +1,11 @@
-import { Box, Image, HStack, VStack, Text, Tag } from "@chakra-ui/react";
+import { Image, HStack, VStack, Text, Tag } from "@chakra-ui/react";
 
 type Props = { colorScheme?: string };
 
 export const ExportFormatCard = ({ colorScheme = "brand" }: Props) => {
   return (
     <VStack
+      alignItems="flex-start"
       maxW="xs"
       borderWidth="1px"
       borderRadius="lg"
@@ -14,20 +15,35 @@ export const ExportFormatCard = ({ colorScheme = "brand" }: Props) => {
       p="2"
       transition="background-color ease 0.2s"
       _hover={{ backgroundColor: `${colorScheme}.100` }}
-      alignItems="flex-start"
+      cursor="pointer"
     >
-      <HStack justifyContent="flex-start">
-        <Image src="/assets/export-formats/coco.png" w="16" h="16" />
-        <VStack>
-          <Text as="h3" fontWeight="semibold" lineHeight="short">
-            Coco
+      <HStack justifyContent="flex-start" alignItems="flex-end">
+        <Image src="/assets/export-formats/coco.png" w="14" h="14" />
+        <VStack
+          alignItems="flex-start"
+          spacing="1"
+          justifyContent="flex-end"
+          height="100%"
+        >
+          <Text as="h3" fontSize="lg" fontWeight="semibold" lineHeight="short">
+            Export to COCO
           </Text>
-          <Tag borderRadius="full" variant="outline" colorScheme={colorScheme}>
+          <Tag
+            mt="0"
+            size="sm"
+            borderRadius="full"
+            variant="outline"
+            color={`${colorScheme}.700`}
+            // used to override chakra's default color
+            // shadow color will fallback to `color`
+            boxShadow="inset 0 0 0px 1px"
+            fontWeight="bold"
+          >
             JSON
           </Tag>
         </VStack>
       </HStack>
-      <Text lineHeight="short" fontWeight="light" mt="0">
+      <Text lineHeight="short" color="gray.600" mt="0">
         Annotation file used with Pytorch and Detectron 2
       </Text>
     </VStack>
