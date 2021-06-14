cca38d6c8880a839d44a16990874bd6e036c5912
Guilhem de Viry <gdeviry@hotmail.fr>
2021-06-14 10:54:39 +0200

    Add test for the selection tool shortcut

--- typescript/web-app/src/components/labelling-tool/drawing-tool-bar/__tests__/selection-tool.tsx
+++ typescript/web-app/src/components/labelling-tool/drawing-tool-bar/__tests__/selection-tool.tsx
@@ -1,10 +1,26 @@
 import { render, screen } from "@testing-library/react";
-
+import userEvent from "@testing-library/user-event";
 import { SelectionTool } from "../selection-tool";
 
 describe("Selection tool", () => {
   it("should be selected by default", () => {
     render(<SelectionTool />);
+
+    expect(screen.getByRole("checkbox", { checked: true })).toBeDefined();
+  });
+
+  it("should select the bounding box when pressing the 'b' key is pressed", () => {
+    const { container } = render(
+      <>
+        <SelectionTool />
+        <input type="checkbox" />
+      </>
+    );
+
+    userEvent.tab();
+    expect(screen.getByRole("checkbox", { checked: false })).toBeDefined();
+
+    userEvent.type(container, "{v}");
 
     expect(screen.getByRole("checkbox", { checked: true })).toBeDefined();
   });
