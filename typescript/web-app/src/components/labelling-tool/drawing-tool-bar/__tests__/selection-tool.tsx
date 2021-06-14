857ea1fdaa13947255c305ae8b84ab5721a4be55
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2021-06-14 11:46:14 +0100

    fix selection tool test

--- typescript/web-app/src/components/labelling-tool/drawing-tool-bar/__tests__/selection-tool.tsx
+++ typescript/web-app/src/components/labelling-tool/drawing-tool-bar/__tests__/selection-tool.tsx
@@ -2,6 +2,10 @@ import { render, screen } from "@testing-library/react";
 
 import { SelectionTool } from "../selection-tool";
 
+jest.mock("next/router", () => ({
+  query: {},
+}));
+
 describe("Selection tool", () => {
   it("should be selected by default", () => {
     render(<SelectionTool />);
