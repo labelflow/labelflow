760233807b65531b03051acad1d1785c29eaa60e
Hugo Raynal <raynalhugo@gmail.com>
2021-06-14 12:23:50 +0200

    Add export cards in the modal

--- typescript/web-app/src/components/export-button/modal/index.tsx
+++ typescript/web-app/src/components/export-button/modal/index.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import {
   Box,
   Button,
+  HStack,
   Heading,
   Modal,
   ModalBody,
@@ -11,6 +12,8 @@ import {
   ModalOverlay,
   Text,
 } from "@chakra-ui/react";
+
+import { ExportFormatCard } from "./export-format-card";
 
 export const ExportModal = ({
   isOpen = false,
@@ -50,7 +53,22 @@ export const ExportModal = ({
           overflowY="hidden"
           flexDirection="column"
         >
-          <Box>Hello</Box>
+          <HStack spacing="4">
+            <ExportFormatCard
+              colorScheme="brand"
+              logoSrc="/assets/export-formats/coco.png"
+              title="Export to COCO"
+              tag="JSON"
+              subtext="Annotation file used with Pytorch and Detectron 2"
+            />
+            <ExportFormatCard
+              colorScheme="gray"
+              logoSrc="/assets/export-formats/tensorflow-grey.png"
+              title="Export to TensorFlow (soon)"
+              tag="CSV"
+              subtext="TF Object Detection file in its human readable format"
+            />
+          </HStack>
         </ModalBody>
         <ModalCloseButton disabled={!isCloseable} />
       </ModalContent>
