08af8803275e0ec6161bb7d07e18143a046696e9
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2021-06-16 12:16:34 +0100

    Fix story

--- typescript/web-app/src/components/app-lifecycle-manager/welcome-modal/__stories__/welcome-modal.tsx
+++ typescript/web-app/src/components/app-lifecycle-manager/welcome-modal/__stories__/welcome-modal.tsx
@@ -1,10 +1,11 @@
+import { withNextRouter } from "storybook-addon-next-router";
 import { chakraDecorator } from "../../../../utils/chakra-decorator";
-
+import { queryParamsDecorator } from "../../../../utils/query-params-decorator";
 import { WelcomeModal } from "../welcome-modal";
 
 export default {
   title: "web-app/app lifecycle/welcome modal",
-  decorators: [chakraDecorator],
+  decorators: [chakraDecorator, queryParamsDecorator, withNextRouter],
 };
 
 export const Default = () => {
