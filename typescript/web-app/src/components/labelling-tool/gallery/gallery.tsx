9ec36e9a88b59bbff854575b33396f2a826e7b2c
Yvonnick Frin <frin.yvonnick@gmail.com>
2021-06-21 15:22:54 +0200

    change react-window for react virtual

--- typescript/web-app/src/components/labelling-tool/gallery/gallery.tsx
+++ typescript/web-app/src/components/labelling-tool/gallery/gallery.tsx
@@ -1,13 +1,11 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { useEffect, useRef } from "react";
+import { useCallback, useRef, memo, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useQuery } from "@apollo/client";
 import gql from "graphql-tag";
 import { Image, Box, Skeleton, Badge, AspectRatio } from "@chakra-ui/react";
-import { FixedSizeList as List } from "react-window";
-import InfiniteLoader from "react-window-infinite-loader";
-import useMeasure from "react-use-measure";
+import { useVirtual } from "react-virtual";
 import { Image as ImageType } from "../../../graphql-types.generated";
 import { mergeAtIndex } from "./merge-at-index";
 
@@ -29,13 +27,10 @@ const paginatedImagesQuery = gql`
   }
 `;
 
+const batchSize = 10;
 const itemHeight = 90;
 const itemWidth = 150;
 const scrollbarHeight = 17;
-
-const getImageOffset = (width: number) => {
-  return Math.floor(width / itemWidth / 2);
-};
 
 const usePaginatedImages = (): {
   count: number;
@@ -44,169 +39,196 @@ const usePaginatedImages = (): {
         images: Array<ImageType | null>;
       }
     | undefined;
-  loading: boolean;
-  loadMoreItems: (min: number, max: number) => Promise<any>;
+  loadMoreItems: (min: number, max: number) => void;
 } => {
   /* We need to replace it with a proper count query */
   const imagesResult =
     useQuery<{ images: Pick<ImageType, "id">[] }>(imagesCountQuery);
   const count = imagesResult?.data?.images?.length ?? 0;
 
-  const { data, loading, fetchMore } = useQuery<{
+  // We need to find a way to reset this on images count changes.
+  const alreadyLoadedItems = useRef<Array<Boolean>>([true]);
+
+  const { data, fetchMore } = useQuery<{
     images: Array<ImageType | null>;
-  }>(paginatedImagesQuery, { variables: { first: 10, skip: 10 } });
+  }>(paginatedImagesQuery, { variables: { first: batchSize * 100, skip: 0 } });
 
   const loadMoreItems = (min: number, max: number) => {
-    return fetchMore({
-      variables: { first: max - min, skip: min },
-      updateQuery: (previousResult, { fetchMoreResult, variables }) => {
-        const newImageStartIndex = variables?.skip;
+    const fetchOneBatch = (batchIndex: number) => {
+      if (alreadyLoadedItems.current[batchIndex] === true) {
+        return;
+      }
 
-        if (typeof newImageStartIndex !== "number") {
-          throw new Error(
-            "Paginated Images query has been called without a 'first', this shouldn't happen"
-          );
-        }
+      alreadyLoadedItems.current[batchIndex] = true;
+      fetchMore({
+        variables: { first: batchSize, skip: batchSize * batchIndex },
+        updateQuery: (previousResult, { fetchMoreResult, variables }) => {
+          const newImageStartIndex = variables?.skip;
 
-        const previousImages = previousResult?.images ?? [];
-        const newImages = fetchMoreResult?.images ?? [];
+          if (typeof newImageStartIndex !== "number") {
+            throw new Error(
+              "Paginated Images query has been called without a 'first', this shouldn't happen"
+            );
+          }
 
-        return {
-          ...previousResult,
-          images: mergeAtIndex(previousImages, newImages, newImageStartIndex),
-        };
-      },
-    });
+          const previousImages = previousResult?.images ?? [];
+          const newImages = fetchMoreResult?.images ?? [];
+
+          return {
+            ...previousResult,
+            images: mergeAtIndex(previousImages, newImages, newImageStartIndex),
+          };
+        },
+      }).catch(() => {
+        alreadyLoadedItems.current[batchIndex] = false;
+      });
+    };
+
+    const minBatchIndex = Math.floor(min / batchSize);
+    const maxBatchIndex = Math.ceil(max / batchSize);
+
+    for (
+      let batchIndex = minBatchIndex;
+      batchIndex <= maxBatchIndex;
+      batchIndex += 1
+    ) {
+      fetchOneBatch(batchIndex);
+    }
   };
 
   return {
     count,
     data,
-    loading: imagesResult.loading || loading,
     loadMoreItems,
   };
 };
 
+const GalleryItem = memo(
+  ({
+    size,
+    start,
+    url,
+    id,
+    isSelected,
+    index,
+  }: {
+    size: number;
+    start: number;
+    url?: string;
+    id?: string;
+    isSelected: boolean;
+    index: number;
+  }) => {
+    return (
+      <Box
+        position="absolute"
+        top={0}
+        left={0}
+        height={itemHeight}
+        width={`${size}px`}
+        transform={`translateX(${start}px)`}
+        pl="7.5px"
+        pr="7.5px"
+      >
+        {id ? (
+          <Link href={`/images/${id}`} passHref>
+            <a aria-current={isSelected ? "page" : undefined}>
+              <Badge
+                pointerEvents="none"
+                position="absolute"
+                top="5px"
+                left="12.5px"
+                bg="rgba(0, 0, 0, 0.6)"
+                color="white"
+                borderRadius="full"
+                zIndex={2}
+              >
+                {index + 1}
+              </Badge>
+              <AspectRatio ratio={3 / 2}>
+                <Image
+                  src={url}
+                  fallback={
+                    <Skeleton height="100%" width="100%" borderRadius="md" />
+                  }
+                  height="100%"
+                  width="100%"
+                  align="center center"
+                  fit="cover"
+                  border="2px solid"
+                  borderColor={isSelected ? "brand.500" : "transparent"}
+                  borderRadius="md"
+                />
+              </AspectRatio>
+            </a>
+          </Link>
+        ) : (
+          <Skeleton height="100%" width="100%" borderRadius="md" />
+        )}
+      </Box>
+    );
+  }
+);
+
 export const Gallery = () => {
-  const listRef = useRef<List | null>(null);
-  const previousImageIndex = useRef<number | null>();
+  const listRef = useRef<HTMLDivElement>(null);
   const router = useRouter();
-  const imageId = router.query.id;
-  /* We need to replace it with a proper count query */
+  const imageId = router.query.id as string;
+
+  // This should be done on the backend side.
   const imagesResult =
     useQuery<{ images: Pick<ImageType, "id">[] }>(imagesCountQuery);
   const currentImageIndex = imagesResult?.data?.images.findIndex(
     (image) => image.id === imageId
   );
-  const [containerRef, { width }] = useMeasure();
 
-  const {
-    count: itemCount,
-    data,
-    loading,
-    loadMoreItems,
-  } = usePaginatedImages();
+  const { count: itemCount, data, loadMoreItems } = usePaginatedImages();
+
+  const { virtualItems, totalSize, scrollToIndex } = useVirtual({
+    size: itemCount,
+    parentRef: listRef,
+    estimateSize: useCallback(() => itemWidth, []),
+    horizontal: true,
+    overscan: 10,
+  });
 
   useEffect(() => {
-    if (!currentImageIndex || !listRef.current || !width) return;
-    // Initialize previousImageIndex when user just came in
-    if (!previousImageIndex.current) {
-      previousImageIndex.current = currentImageIndex;
+    if (!currentImageIndex || currentImageIndex === -1) {
+      return;
     }
-    if (previousImageIndex.current <= currentImageIndex) {
-      listRef.current.scrollToItem(currentImageIndex + getImageOffset(width));
-    } else {
-      listRef.current.scrollToItem(currentImageIndex - getImageOffset(width));
-    }
-    previousImageIndex.current = currentImageIndex;
-  }, [currentImageIndex, listRef.current, width]);
 
-  const isLoading = !width && loading && imagesResult.loading;
+    scrollToIndex(currentImageIndex, { align: "center" });
+  }, [currentImageIndex, scrollToIndex]);
+
+  useEffect(() => {
+    if (virtualItems.length === 0) {
+      return;
+    }
+
+    const min = virtualItems[0].index;
+    const max = virtualItems[virtualItems.length - 1].index;
+    loadMoreItems(min, max);
+  }, [virtualItems]);
 
   return (
-    <Box as="nav" ref={containerRef} pt={4}>
-      {!isLoading && (
-        <InfiniteLoader
-          isItemLoaded={(index: number) => data?.images?.[index] != null}
-          itemCount={itemCount}
-          loadMoreItems={loadMoreItems}
-        >
-          {({ onItemsRendered, ref: internalRef }) => (
-            <List
-              ref={(value) => {
-                if (typeof internalRef === "function") {
-                  internalRef(value);
-                }
-                listRef.current = value;
-              }}
-              className="List"
-              height={itemHeight + scrollbarHeight}
-              itemCount={itemCount}
-              itemSize={itemWidth}
-              onItemsRendered={onItemsRendered}
-              layout="horizontal"
-              width={width}
-            >
-              {({ index, style }) => (
-                <Box style={style} pl="7.5px" pr="7.5px" position="relative">
-                  {data?.images?.[index] ? (
-                    <Link
-                      href={`/images/${data?.images?.[index]?.id}`}
-                      passHref
-                    >
-                      <a
-                        aria-current={
-                          imageId === data?.images?.[index]?.id
-                            ? "page"
-                            : undefined
-                        }
-                      >
-                        <Badge
-                          pointerEvents="none"
-                          position="absolute"
-                          top="5px"
-                          left="12.5px"
-                          bg="rgba(0, 0, 0, 0.6)"
-                          color="white"
-                          borderRadius="full"
-                          zIndex={2}
-                        >
-                          {index + 1}
-                        </Badge>
-                        <AspectRatio ratio={3 / 2}>
-                          <Image
-                            src={data?.images?.[index]?.url}
-                            fallback={
-                              <Skeleton
-                                height="100%"
-                                width="100%"
-                                borderRadius="md"
-                              />
-                            }
-                            height="100%"
-                            width="100%"
-                            align="center center"
-                            fit="cover"
-                            border="2px solid"
-                            borderColor={
-                              imageId === data?.images?.[index]?.id
-                                ? "brand.500"
-                                : "transparent"
-                            }
-                            borderRadius="md"
-                          />
-                        </AspectRatio>
-                      </a>
-                    </Link>
-                  ) : (
-                    <Skeleton height="100%" width="100%" borderRadius="md" />
-                  )}
-                </Box>
-              )}
-            </List>
-          )}
-        </InfiniteLoader>
-      )}
+    <Box ref={listRef} as="nav" pt={4} overflow="auto">
+      <Box
+        height={itemHeight + scrollbarHeight}
+        width={totalSize}
+        position="relative"
+      >
+        {virtualItems.map((item) => (
+          <GalleryItem
+            key={item.index}
+            size={item.size}
+            id={data?.images?.[item.index]?.id}
+            url={data?.images?.[item.index]?.url}
+            isSelected={imageId === data?.images?.[item.index]?.id}
+            start={item.start}
+            index={item.index}
+          />
+        ))}
+      </Box>
     </Box>
   );
 };
