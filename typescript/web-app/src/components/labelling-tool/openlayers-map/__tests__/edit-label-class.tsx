34d138fb57a98441234931fa1b86e6752a925166
Guilhem de Viry <gdeviry@hotmail.fr>
2021-06-21 14:43:47 +0200

    Disable eslint rule "import first"

--- typescript/web-app/src/components/labelling-tool/openlayers-map/__tests__/edit-label-class.tsx
+++ typescript/web-app/src/components/labelling-tool/openlayers-map/__tests__/edit-label-class.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/first */
 import { ApolloProvider } from "@apollo/client";
 import { render, screen, waitFor, act } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
