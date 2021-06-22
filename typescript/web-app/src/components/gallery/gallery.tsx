b32a76f432e3d476f20933e5fcc96b92b288ded8
Hugo Raynal <raynalhugo@gmail.com>
2021-06-22 19:28:30 +0200

    Add comment for posterity

--- typescript/web-app/src/components/gallery/gallery.tsx
+++ typescript/web-app/src/components/gallery/gallery.tsx
@@ -8,6 +8,16 @@ import { useImagesNavigation } from "../../hooks/use-images-navigation";
 import { GalleryItem } from "./gallery-item";
 import { itemHeight, itemWidth, scrollbarHeight } from "./constants";
 
+/**
+ * Virtualized, unpaginated list of images.
+ *
+ * Side note:
+ * This component was paginated. We decided to revert this and go for a simpler solution.
+ * Now it queries every images at once. See `useImagesNavigation`.
+ * If you, beings of the future, wish to change paginate this component again,
+ * you can find the code here:
+ * https://github.com/Labelflow/labelflow/pull/179/files/22a9cf33c2c0af2d4b91762a6cc3d18f5f678274..427b488b7180272e7b7d8dcbb11a464a6bea32b7
+ */
 export const Gallery = () => {
   const listRef = useRef<HTMLDivElement>(null);
   const router = useRouter();
