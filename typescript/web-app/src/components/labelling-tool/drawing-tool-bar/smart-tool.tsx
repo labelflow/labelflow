c06013e860fe1342a3301122a5ef89d9942ff037
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-06-23 15:48:52 +0100

    We get predictions!

--- typescript/web-app/src/components/labelling-tool/drawing-tool-bar/smart-tool.tsx
+++ typescript/web-app/src/components/labelling-tool/drawing-tool-bar/smart-tool.tsx
@@ -1,9 +1,14 @@
+/* eslint-disable import/first */
+require("@tensorflow/tfjs-backend-cpu");
+require("@tensorflow/tfjs-backend-webgl");
+
 import { useState } from "react";
 import { useRouter } from "next/router";
 import { IconButton, Tooltip } from "@chakra-ui/react";
 import { useQuery } from "@apollo/client";
 import { BiBrain } from "react-icons/bi";
 import { gql } from "graphql-tag";
+import { load } from "@tensorflow-models/coco-ssd";
 // import { useHotkeys } from "react-hotkeys-hook";
 
 import { keymap } from "../../../keymap";
@@ -23,18 +28,28 @@ const runSmartTool = async (
   setSmartToolRunning: (b: boolean) => void,
   imageUrl: string
 ) => {
+  setSmartToolRunning(true);
+  const imageLoadPromise = new Promise<HTMLImageElement>((resolve) => {
+    const image = new Image();
+    image.onload = () => {
+      resolve(image);
+    };
+    image.src = imageUrl;
+  });
+  const image = await imageLoadPromise;
+  const model = await load();
+  const predictions = await model.detect(image);
+  console.log("Predictions", predictions);
   setSmartToolRunning(false);
-
-  const image = new Image();
-  image.src = imageUrl;
-  image.onload = () => {};
 };
 
 export const SmartTool = () => {
   const [smartToolRunning, setSmartToolRunning] = useState(false);
   const router = useRouter();
   const imageId = router.query.id;
-  const { data: image } = useQuery(queryImage, {
+  const {
+    data: { image },
+  } = useQuery(queryImage, {
     variables: { id: imageId },
     skip: imageId == null,
   });
@@ -49,7 +64,7 @@ export const SmartTool = () => {
       <IconButton
         icon={<BiBrain size="1.3em" />}
         role="checkbox"
-        onClick={() => runSmartTool(setSmartToolRunning, image)}
+        onClick={() => runSmartTool(setSmartToolRunning, image?.url)}
         backgroundColor="white"
         aria-label="Smart tool"
         pointerEvents="initial"
