14742a513fc241d0b76385f68a57c0dd0dba9268
Yvonnick Frin <frin.yvonnick@gmail.com>
2021-06-25 14:22:59 +0200

    Fix a bug that prevent to unzoom openlayers when zooming the browser

--- typescript/web-app/src/components/labelling-tool/openlayers-map/openlayers-map.tsx
+++ typescript/web-app/src/components/labelling-tool/openlayers-map/openlayers-map.tsx
@@ -183,13 +183,12 @@ export const OpenlayersMap = () => {
                       );
                       return false;
                     }}
-                    args={{ extent }}
+                    args={{ extent, maxResolution: resolution }}
                     center={center}
                     initialProjection={projection}
                     resolution={resolution}
                     // Max zoom = 16 pixels of screen per pixel of image
                     minResolution={1.0 / 16.0}
-                    maxResolution={resolution}
                     constrainOnlyCenter
                     showFullExtent
                     padding={viewPadding}
