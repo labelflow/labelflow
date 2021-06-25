9df501b78ec6e1fc0130e429ee27a7646970d356
Hugo Raynal <raynalhugo@gmail.com>
2021-06-25 18:22:37 +0200

    Split draw interaction file in 2

---
+++ typescript/web-app/src/components/labelling-tool/openlayers-map/draw-bounding-box-interaction/index.tsx
@@ -0,0 +1,114 @@
+import { useMemo } from "react";
+import { useRouter } from "next/router";
+import { createBox, DrawEvent } from "ol/interaction/Draw";
+import { Fill, Stroke, Style } from "ol/style";
+import GeometryType from "ol/geom/GeometryType";
+import { useApolloClient, useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+
+import {
+  useLabellingStore,
+  Tools,
+  BoxDrawingToolState,
+} from "../../../../connectors/labelling-state";
+import { useUndoStore } from "../../../../connectors/undo-store";
+import { noneClassColor } from "../../../../utils/class-color-generator";
+import { createLabelEffect } from "./create-label-effect";
+
+const labelClassQuery = gql`
+  query getLabelClass($id: ID!) {
+    labelClass(where: { id: $id }) {
+      id
+      name
+      color
+    }
+  }
+`;
+
+const geometryFunction = createBox();
+
+export const DrawBoundingBoxInteraction = () => {
+  const client = useApolloClient();
+  const imageId = useRouter().query?.id;
+
+  const selectedTool = useLabellingStore((state) => state.selectedTool);
+
+  const setBoxDrawingToolState = useLabellingStore(
+    (state) => state.setBoxDrawingToolState
+  );
+  const setSelectedLabelId = useLabellingStore(
+    (state) => state.setSelectedLabelId
+  );
+  const selectedLabelClassId = useLabellingStore(
+    (state) => state.selectedLabelClassId
+  );
+  const { data: dataLabelClass } = useQuery(labelClassQuery, {
+    variables: { id: selectedLabelClassId },
+    skip: selectedLabelClassId == null,
+  });
+  const { perform } = useUndoStore();
+
+  const selectedLabelClass = dataLabelClass?.labelClass;
+
+  const style = useMemo(() => {
+    const color = selectedLabelClass?.color ?? noneClassColor;
+
+    return new Style({
+      fill: new Fill({
+        color: `${color}10`,
+      }),
+      stroke: new Stroke({
+        color,
+        width: 2,
+      }),
+    });
+  }, [selectedLabelClass?.color]);
+
+  if (selectedTool !== Tools.BOX) {
+    return null;
+  }
+  if (typeof imageId !== "string") {
+    return null;
+  }
+
+  return (
+    <olInteractionDraw
+      args={{
+        type: GeometryType.CIRCLE,
+        geometryFunction,
+        style, // Needed here to trigger the rerender of the component when the selected class changes
+      }}
+      onDrawabort={() => {
+        setBoxDrawingToolState(BoxDrawingToolState.IDLE);
+        return true;
+      }}
+      onDrawstart={() => {
+        setBoxDrawingToolState(BoxDrawingToolState.DRAWING);
+        return true;
+      }}
+      onDrawend={(drawEvent: DrawEvent) => {
+        const [x, y, destX, destY] = drawEvent.feature
+          .getGeometry()
+          .getExtent();
+
+        perform(
+          createLabelEffect(
+            {
+              imageId,
+              x,
+              y,
+              width: destX - x,
+              height: destY - y,
+              selectedLabelClassId,
+            },
+            {
+              setSelectedLabelId,
+              client,
+            }
+          )
+        );
+        setBoxDrawingToolState(BoxDrawingToolState.IDLE);
+      }}
+    />
+  );
+};
