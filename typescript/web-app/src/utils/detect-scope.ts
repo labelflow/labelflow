540e1b592b7fdfb4ced0c2b4a78ecd62c330035e
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2021-06-29 14:01:32 +0100

    Fix for safari

--- typescript/web-app/src/utils/detect-scope.ts
+++ typescript/web-app/src/utils/detect-scope.ts
@@ -6,12 +6,28 @@ export let isInWindowScope: boolean | null = null;
 // eslint-disable-next-line import/no-mutable-exports
 export let isInServiceWorkerScope: boolean | null = null;
 
+// eslint-disable-next-line import/no-mutable-exports
+export let browserName: "chrome" | "safari" | "other" | null = null;
+
 // Robust way to detect if window exists, only once
 const detectScope = () => {
   if (isInWindowScope === null) {
     try {
       if (window) {
         isInWindowScope = true;
+
+        // Detect browser
+        // See https://stackoverflow.com/a/7944490/2371254
+        const ua = navigator.userAgent.toLowerCase();
+        if (ua.indexOf("safari") != -1) {
+          if (ua.indexOf("chrome") > -1) {
+            browserName = "chrome";
+          } else {
+            browserName = "safari";
+          }
+        } else {
+          browserName = "other";
+        }
       } else {
         isInWindowScope = false;
       }
