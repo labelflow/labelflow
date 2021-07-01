68c43914147ca81d054353c6164b04f393d86195
Guilhem de Viry <gdeviry@hotmail.fr>
2021-07-01 10:51:07 +0200

    New project card done

--- typescript/web-app/src/components/project-cards/new-project-card.tsx
+++ typescript/web-app/src/components/project-cards/new-project-card.tsx
@@ -1,12 +1,26 @@
-import { Center, chakra } from "@chakra-ui/react";
+import { Flex, Text, chakra } from "@chakra-ui/react";
 import { FaPlus } from "react-icons/fa";
 
 const PlusIcon = chakra(FaPlus);
 
 export const NewProjectCard = () => {
+  // TODO: Update the border style to set custom
   return (
-    <Center w="sm" h="44" borderWidth="0px" borderRadius="16px" bg="white">
-      <PlusIcon />
-    </Center>
+    <Flex
+      w="sm"
+      h="2xs"
+      border="2px"
+      borderRadius="16px"
+      borderStyle="dashed"
+      borderColor="gray.400"
+      direction="column"
+      alignItems="center"
+      justify="space-evenly"
+    >
+      <PlusIcon color="gray.400" h="60px" w="60" />
+      <Text fontSize="16px" fontWeight="semibold" color="gray.500">
+        Create new project...
+      </Text>
+    </Flex>
   );
 };
