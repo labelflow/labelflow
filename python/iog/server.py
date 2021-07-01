a0f82eb0377c0a4c96257209153e5e05563bb844
Hugo Raynal <raynalhugo@gmail.com>
2021-07-01 11:46:51 +0200

    WIP: Make graphql endpoint

--- python/iog/server.py
+++ python/iog/server.py
@@ -8,10 +8,89 @@ import cv2
 
 from inference_on_image import process
 
-# class ModelInference(HTTPEndpoint):
-#     async def get(self, request):
-#         return PlainTextResponse(f"Hello, world!")
 
+
+from ariadne import QueryType, MutationType, ObjectType, ScalarType, make_executable_schema
+from ariadne.asgi import GraphQL
+from starlette.applications import Starlette
+import uvicorn
+import databases
+import sqlalchemy
+import os
+
+################################################################################
+# GraphQL section, from the Ariadne docs
+# See https://ariadnegraphql.org/docs/starlette-integration
+
+type_defs = """
+    scalar DateTime
+    scalar Json
+
+    type iogInferenceResult {
+        polygons: [[Float!]]! 
+    }
+
+    type Query {
+        hello: String!
+        projects(first: Int): [Project!]!
+    }
+
+    type Mutation {
+        iogInference(
+            imageUrl: String!, 
+            x: Float!, 
+            y: Float!, 
+            width: Float!, 
+            height: Float!, 
+            pointsInside: [[Float!]],
+            pointsOutside: [[Float!]]
+        ): iogInferenceResult
+    }
+
+   type Project {
+        id:        Int
+        createdAt: DateTime
+        updatedAt: DateTime
+        name:      String
+    }
+"""
+
+datetime_scalar = ScalarType("DateTime")
+
+@datetime_scalar.serializer
+def serialize_datetime(value):
+    return value.isoformat()
+
+query = QueryType()
+
+@query.field("hello")
+def resolve_hello(*_):
+    return "Hello world!"
+
+
+mutation = MutationType()
+
+@mutation.field("iogInference")
+def resolve_iog_inference(*_, imageUrl, x, y, width, height, pointsInside = [], pointsOutside = []):
+    print("HEY")
+
+
+    # Decode image
+    image_b64 = imageUrl.split(",")[1]
+    binary = base64.b64decode(image_b64)
+    image = np.asarray(bytearray(binary), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+    cv2.imwrite("test.jpg", image)
+
+    roi = [x, image.shape[0] - y - height, width, height]
+    print(roi)
+    # process(image, roi)
+    return JSONResponse({"polygons": process(image, roi)})
+    return { "polygons": process(image, roi) }
+
+# Create executable schema instance
+schema = make_executable_schema(type_defs, query, mutation, datetime_scalar)
 
 async def model_inference(request):
     inputs = await request.json()
@@ -39,6 +118,10 @@ async def model_inference(request):
 routes = [Route("/", model_inference, methods=["POST"])]
 
 app = Starlette(debug=True, routes=routes)
+
+# Add Graphql endpoint
+app.mount("/graphql", GraphQL(schema, debug=True))
+
 
 ################################################################################
 # Launch uvicorn programmatically, from the uvicorn docs
