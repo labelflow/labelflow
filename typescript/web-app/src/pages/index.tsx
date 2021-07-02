89cb695752b2b6b7b2d7e9618c4f643144a449ff
Guilhem de Viry <gdeviry@hotmail.fr>
2021-07-02 10:53:51 +0200

    Revert main page redirection

--- typescript/web-app/src/pages/index.tsx
+++ typescript/web-app/src/pages/index.tsx
@@ -1,14 +1,29 @@
 import { useEffect } from "react";
 import { Spinner, Center } from "@chakra-ui/react";
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
 import { useRouter } from "next/router";
+import type { Image as ImageType } from "../graphql-types.generated";
 import { Layout } from "../components/layout";
+
+export const firstImageQuery = gql`
+  query getFirstImage {
+    images(first: 1) {
+      id
+    }
+  }
+`;
 
 const IndexPage = () => {
   const router = useRouter();
+  const { data: firstImageResult, error } =
+    useQuery<{ images: Pick<ImageType, "id">[] }>(firstImageQuery);
 
   useEffect(() => {
-    router.replace({ pathname: "/projects", query: router.query });
-  }, []);
+    if (firstImageResult && !error) {
+      router.replace({ pathname: "/images", query: router.query });
+    }
+  }, [firstImageResult, error]);
 
   return (
     <Layout>
