9d41ca7a148076db811b6b4ef87ecc1510980979
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-07-02 13:26:02 +0100

    Move state to ref to avoid recreating the resize interaction on each render

--- typescript/web-app/src/components/labelling-tool/openlayers-map/select-and-modify-feature/index.tsx
+++ typescript/web-app/src/components/labelling-tool/openlayers-map/select-and-modify-feature/index.tsx
@@ -1,8 +1,14 @@
-import { useEffect, useState, useCallback, MutableRefObject } from "react";
+import {
+  useEffect,
+  useState,
+  useCallback,
+  MutableRefObject,
+  useMemo,
+} from "react";
 import { Feature, Map as OlMap } from "ol";
 import { Geometry } from "ol/geom";
 import { Vector as OlSourceVector } from "ol/source";
-import { ResizeInteraction } from "./resize-interaction";
+import { BoxResizeTranslateInteraction } from "./resize-interaction";
 import { SelectInteraction } from "./select-interaction";
 import { TranslateFeature } from "./translate-interaction";
 import { useLabellingStore } from "../../../../connectors/labelling-state";
@@ -12,41 +18,58 @@ export const SelectAndModifyFeature = (props: {
   map: OlMap | null;
 }) => {
   const { sourceVectorLabelsRef, map } = props;
-  const [selectedFeature, setSelectedFeature] =
-    useState<Feature<Geometry> | null>(null);
+  // const [selectedFeature, setSelectedFeature] =
+  //   useState<Feature<Geometry> | null>(null);
   const selectedLabelId = useLabellingStore((state) => state.selectedLabelId);
 
-  const getSelectedFeature = useCallback(() => {
-    if (selectedFeature?.getProperties()?.id !== selectedLabelId) {
-      if (selectedLabelId == null) {
-        setSelectedFeature(null);
-      } else {
-        const featureFromSource = sourceVectorLabelsRef.current
-          ?.getFeatures()
-          ?.filter(
-            (feature) => feature.getProperties().id === selectedLabelId
-          )?.[0];
-        if (featureFromSource != null) {
-          setSelectedFeature(featureFromSource);
-        }
-      }
+  // const getSelectedFeature = useCallback(() => {
+  //   if (selectedFeature?.getProperties()?.id !== selectedLabelId) {
+  //     if (selectedLabelId == null) {
+  //       setSelectedFeature(null);
+  //     } else {
+  //       const featureFromSource = sourceVectorLabelsRef.current
+  //         ?.getFeatures()
+  //         ?.filter(
+  //           (feature) => feature.getProperties().id === selectedLabelId
+  //         )?.[0];
+  //       if (featureFromSource != null) {
+  //         setSelectedFeature(featureFromSource);
+  //       }
+  //     }
+  //   }
+  // }, [selectedLabelId, sourceVectorLabelsRef.current]);
+
+  // useEffect(() => {
+  //   sourceVectorLabelsRef.current?.on("addfeature", getSelectedFeature);
+  //   return () =>
+  //     sourceVectorLabelsRef.current?.un("addfeature", getSelectedFeature);
+  // }, [sourceVectorLabelsRef.current]);
+
+  // useEffect(() => {
+  //   getSelectedFeature();
+  // }, [selectedLabelId]);
+
+  const selectedFeature = useMemo(() => {
+    if (selectedLabelId == null) {
+      return null;
     }
+    const featureFromSource = sourceVectorLabelsRef.current
+      ?.getFeatures()
+      ?.filter(
+        (feature) => feature.getProperties().id === selectedLabelId
+      )?.[0];
+
+    return featureFromSource ?? null;
   }, [selectedLabelId, sourceVectorLabelsRef.current]);
 
-  useEffect(() => {
-    sourceVectorLabelsRef.current?.on("addfeature", getSelectedFeature);
-    return () =>
-      sourceVectorLabelsRef.current?.un("addfeature", getSelectedFeature);
-  }, [sourceVectorLabelsRef.current]);
-
-  useEffect(() => {
-    getSelectedFeature();
-  }, [selectedLabelId]);
   return (
     <>
       <SelectInteraction {...props} />
       <TranslateFeature selectedFeature={selectedFeature} />
-      <ResizeInteraction selectedFeature={selectedFeature} map={map} />
+      <BoxResizeTranslateInteraction
+        selectedFeature={selectedFeature}
+        map={map}
+      />
     </>
   );
 };
--- typescript/web-app/src/components/labelling-tool/openlayers-map/select-and-modify-feature/resize-interaction.tsx
+++ typescript/web-app/src/components/labelling-tool/openlayers-map/select-and-modify-feature/resize-interaction.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState, useCallback, useRef } from "react";
+import { useEffect, useState, useCallback, useRef, useMemo } from "react";
 import { Feature, MapBrowserEvent, Map as OlMap } from "ol";
 import { Geometry } from "ol/geom";
 import { Coordinate, distance } from "ol/coordinate";
-import { fromExtent } from "ol/geom/Polygon";
+import Polygon, { fromExtent } from "ol/geom/Polygon";
 import { Extent } from "ol/extent";
 import { TranslateEvent } from "ol/interaction/Translate";
 
@@ -62,39 +62,57 @@ const getNewFeatureExtentFromDragEvent = ({
   return extent;
 };
 
-export const ResizeInteraction = ({
+export const BoxResizeTranslateInteraction = ({
   selectedFeature,
   map,
-  pixelTolerance = 10,
+  pixelTolerance = 20,
 }: {
-  selectedFeature: Feature<Geometry> | null;
+  selectedFeature: Feature<Polygon> | null;
   map: OlMap | null;
   pixelTolerance?: number;
 }) => {
-  const [featureVertices, setFeatureVertices] =
-    useState<FeatureVertices | null>(null);
-  const [selectedVertex, setSelectedVertex] = useState<string | null>(null);
-  const [mapTarget, setMapTarget] = useState<HTMLElement | null>(null);
+  // const [featureVertices, setFeatureVertices] =
+  //   useState<FeatureVertices | null>(null);
+  const featureVerticesRef = useRef(null);
+  // const [selectedVertex, setSelectedVertex] = useState<string | null>(null);
+  const selectedVertexRef = useRef(null);
+  // const [mapTarget, setMapTarget] = useState<HTMLElement | null>(null);
+  // useEffect(() => {
+  //   if (map != null) {
+  //     setMapTarget(map.getTarget() as HTMLElement);
+  //   } else {
+  //     setMapTarget(null);
+  //   }
+  // }, [map]);
+
   useEffect(() => {
-    if (map != null) {
-      setMapTarget(map.getTarget() as HTMLElement);
+    selectedFeatureRef.current = selectedFeature;
+    if (selectedFeatureRef.current != null) {
+      const [x, y, destX, destY] = selectedFeatureRef.current
+        .getGeometry()
+        .getExtent();
+      // setFeatureVertices(getFeatureVerticesFromExtent({ x, y, destX, destY }));
+      featureVerticesRef.current = getFeatureVerticesFromExtent({
+        x,
+        y,
+        destX,
+        destY,
+      });
     } else {
-      setMapTarget(null);
-    }
-  }, [map]);
-  useEffect(() => {
-    if (selectedFeature != null) {
-      const [x, y, destX, destY] = selectedFeature.getGeometry().getExtent();
-      setFeatureVertices(getFeatureVerticesFromExtent({ x, y, destX, destY }));
-    } else {
-      setFeatureVertices(null);
+      // setFeatureVertices(null);
+      featureVerticesRef.current = null;
     }
   }, [selectedFeature]);
-  const getClosestVertex = useCallback(
-    (coordinate) => {
-      if (featureVertices != null && coordinate != null) {
-        const distanceToVertices = featureVertices.map((vertex) =>
-          distance(coordinate, vertex)
+  console.log("Selected feature", selectedFeature);
+  const pointerInteractionRef = useRef(null);
+  const selectedFeatureRef = useRef<Feature<Polygon>>(selectedFeature);
+
+  const args = useMemo(() => {
+    const getClosestVertex = (coordinate) => {
+      if (featureVerticesRef.current != null && coordinate != null) {
+        const coordinateInPixels = map?.getPixelFromCoordinate(coordinate);
+        const distanceToVertices = featureVerticesRef.current?.map((vertex) =>
+          distance(coordinateInPixels, map?.getPixelFromCoordinate(vertex))
         );
         const minimalDistanceIndex = distanceToVertices.indexOf(
           Math.min(...distanceToVertices)
@@ -107,65 +125,233 @@ export const ResizeInteraction = ({
         };
       }
       return {
-        distance: null,
+        distanceToVertex: null,
         vertex: null,
         insideTolerance: null,
       };
-    },
-    [featureVertices]
-  );
-  const pointerInteractionRef = useRef(null);
-  return selectedFeature != null ? (
-    <olInteractionPointer
-      ref={pointerInteractionRef}
-      args={{
-        handleDownEvent: (e: MapBrowserEvent) => {
-          const { insideTolerance, vertex } = getClosestVertex(e.coordinate);
-          if (insideTolerance) {
-            console.log("Selected index", vertex);
-            map?.getInteractions().getArray()[2].setActive(false);
-            console.log("Deactivated!");
-            setSelectedVertex(vertex);
-            return true;
+    };
+
+    const getClosestLine = (coordinate) => {
+      if (featureVerticesRef.current != null && coordinate != null) {
+        const closestPoint = selectedFeatureRef.current
+          .getGeometry()
+          .getClosestPoint(coordinate);
+        const coordinateInPixels = map?.getPixelFromCoordinate(coordinate);
+        const distanceToClosestPoint = distance(
+          coordinateInPixels,
+          map?.getPixelFromCoordinate(closestPoint)
+        );
+        if (distanceToClosestPoint < pixelTolerance) {
+          if (closestPoint[0] === featureVerticesRef.current[0][0]) {
+            return {
+              distanceToLine: distanceToClosestPoint,
+              line: "left",
+              insideTolerance: true,
+            };
           }
-          return false;
-        },
-        handleDragEvent: (e) => {
-          console.log("Drag event", e.coordinate);
-          if (selectedVertex != null) {
-            const extent = selectedFeature.getGeometry().getExtent();
-            const newExtent = getNewFeatureExtentFromDragEvent({
-              extent,
-              vertex: selectedVertex,
-              coordinate: e.coordinate,
-            });
-            selectedFeature.setGeometry(fromExtent(newExtent));
-            e.preventDefault();
-            e.stopPropagation();
+          if (closestPoint[0] === featureVerticesRef.current[2][0]) {
+            return {
+              distanceToLine: distanceToClosestPoint,
+              line: "right",
+              insideTolerance: true,
+            };
           }
-        },
-        handleMoveEvent: (e: MapBrowserEvent) => {
+          if (closestPoint[1] === featureVerticesRef.current[0][1]) {
+            return {
+              distanceToLine: distanceToClosestPoint,
+              line: "bottom",
+              insideTolerance: true,
+            };
+          }
+          if (closestPoint[1] === featureVerticesRef.current[2][1]) {
+            return {
+              distanceToLine: distanceToClosestPoint,
+              line: "top",
+              insideTolerance: true,
+            };
+          }
+        }
+      }
+      return {
+        distanceToLine: null,
+        line: null,
+        insideTolerance: null,
+      };
+    };
+
+    return {
+      handleDownEvent: (e: MapBrowserEvent) => {
+        console.log("Down event");
+        const { insideTolerance, vertex } = getClosestVertex(e.coordinate);
+        if (insideTolerance) {
+          console.log("Selected index", vertex);
+          console.log("Deactivated!");
+          // setSelectedVertex(vertex);
+          selectedVertexRef.current = vertex;
+          return true;
+        }
+        return false;
+      },
+      handleDragEvent: (e) => {
+        console.log("Drag event", e.coordinate);
+        if (selectedVertexRef.current != null) {
+          const extent = selectedFeatureRef.current.getGeometry().getExtent();
+          const newExtent = getNewFeatureExtentFromDragEvent({
+            extent,
+            vertex: selectedVertexRef.current,
+            coordinate: e.coordinate,
+          });
+          selectedFeatureRef.current.setGeometry(fromExtent(newExtent));
+          e.preventDefault();
+          e.stopPropagation();
+        }
+      },
+      handleEvent: (e: MapBrowserEvent) => {
+        if (e.type === "pointermove") {
+          const mapTarget = map?.getTarget();
           if (mapTarget != null) {
             const { insideTolerance, vertex } = getClosestVertex(e.coordinate);
             if (insideTolerance) {
+              console.log("Inside tolerance!");
               const cursor =
                 vertex === "bottomLeft" || vertex === "topRight"
                   ? "nesw-resize"
                   : "nwse-resize";
               mapTarget.style.cursor = cursor;
+              e.stopPropagation();
+              return false;
             }
+
+            const { insideTolerance: insideLineTolerance, line } =
+              getClosestLine(e.coordinate);
+            if (insideLineTolerance) {
+              console.log("Inside tolerance line!");
+              const cursor =
+                line === "left" || line === "right" ? "ew-resize" : "ns-resize";
+              mapTarget.style.cursor = cursor;
+              e.stopPropagation();
+              return false;
+            }
+            return true;
           }
-        },
-        handleUpEvent: () => {
-          map?.getInteractions().getArray()[2].setActive(true);
-          console.log("Activated!");
-          return false;
-        },
-        stopDown: (e: MapBrowserEvent) => {
-          const { insideTolerance } = getClosestVertex(e.coordinate);
-          return insideTolerance === true;
-        },
-      }}
+          return true;
+        }
+        return true;
+      },
+      handleMoveEvent: (e: MapBrowserEvent) => {
+        console.log("Move event");
+        const mapTarget = map?.getTarget();
+        if (mapTarget != null) {
+          const { insideTolerance, vertex } = getClosestVertex(e.coordinate);
+          if (insideTolerance) {
+            console.log("Inside tolerance!");
+            const cursor =
+              vertex === "bottomLeft" || vertex === "topRight"
+                ? "nesw-resize"
+                : "nwse-resize";
+            mapTarget.style.cursor = cursor;
+            e.stopPropagation();
+            return;
+          }
+
+          const { insideTolerance: insideLineTolerance, line } = getClosestLine(
+            e.coordinate
+          );
+          if (insideLineTolerance) {
+            console.log("Inside tolerance line!");
+            const cursor =
+              line === "left" || line === "right" ? "ew-resize" : "ns-resize";
+            mapTarget.style.cursor = cursor;
+            e.stopPropagation();
+          }
+        }
+      },
+      handleUpEvent: () => {
+        console.log("Up event");
+        if (selectedFeatureRef.current != null) {
+          const [x, y, destX, destY] = selectedFeatureRef.current
+            .getGeometry()
+            .getExtent();
+          // setFeatureVertices(getFeatureVerticesFromExtent({ x, y, destX, destY }));
+          featureVerticesRef.current = getFeatureVerticesFromExtent({
+            x,
+            y,
+            destX,
+            destY,
+          });
+        } else {
+          // setFeatureVertices(null);
+          featureVerticesRef.current = null;
+        }
+        console.log("Done");
+        return false;
+      },
+      stopDown: (handled) => {
+        console.log("Received here", handled);
+        return handled;
+      },
+    };
+  }, []);
+  return selectedFeatureRef.current != null ? (
+    <olInteractionPointer
+      ref={pointerInteractionRef}
+      args={args}
+      // handleDownEvent={(e: MapBrowserEvent) => {
+      //   console.log("Down event");
+      //   lastDownEvent.current = Date.now();
+      //   const { insideTolerance, vertex } = getClosestVertex(e.coordinate);
+      //   if (insideTolerance) {
+      //     console.log("Selected index", vertex);
+      //     map?.getInteractions().getArray()[2].setActive(false);
+      //     console.log("Deactivated!");
+      //     setSelectedVertex(vertex);
+      //     return true;
+      //   }
+      //   return false;
+      // }}
+      // handleDragEvent={(e) => {
+      //   console.log("Drag event", selectedVertex);
+      //   if (selectedVertex != null) {
+      //     const extent = selectedFeature.getGeometry().getExtent();
+      //     const newExtent = getNewFeatureExtentFromDragEvent({
+      //       extent,
+      //       vertex: selectedVertex,
+      //       coordinate: e.coordinate,
+      //     });
+      //     selectedFeature.setGeometry(fromExtent(newExtent));
+      //     e.preventDefault();
+      //     e.stopPropagation();
+      //   }
+      // }}
+      // handleMoveEvent={(e: MapBrowserEvent) => {
+      //   const viewport = e.map.getViewport();
+      //   // console.log("Map", olMap);
+      //   console.log("Move event", lastDownEvent.current);
+      //   if (viewport != null) {
+      //     const { insideTolerance, vertex, distanceToVertex } =
+      //       getClosestVertex(e.coordinate);
+      //     console.log(insideTolerance, vertex, distanceToVertex);
+      //     if (insideTolerance) {
+      //       console.log("Inside tolerance!");
+      //       const cursor =
+      //         vertex === "bottomLeft" || vertex === "topRight"
+      //           ? "nesw-resize"
+      //           : "nwse-resize";
+      //       viewport.style.cursor = cursor;
+      //     }
+      //   }
+      // }}
+      // handleUpEvent={() => {
+      //   console.log("Up event");
+      //   map?.getInteractions().getArray()[2].setActive(true);
+      //   console.log("Activated!");
+      //   return false;
+      // }}
+      // stopDown={(e: MapBrowserEvent) => {
+      //   const { insideTolerance } = getClosestVertex(e.coordinate);
+      //   console.log("Stopping down", insideTolerance === true);
+      //   return insideTolerance === true;
+      // }}
     />
   ) : null;
 };
