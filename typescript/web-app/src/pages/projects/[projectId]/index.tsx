421ea7ab910a2e8871ca3959b6ceb64c6a0ace06
ShauniArima <anderfriou@protonmail.com>
2021-07-07 18:24:12 +0200

    fix: Redirect from project page

--- typescript/web-app/src/pages/projects/[projectId]/index.tsx
+++ typescript/web-app/src/pages/projects/[projectId]/index.tsx
@@ -1,17 +1,38 @@
 import { useEffect } from "react";
 import { Spinner, Center } from "@chakra-ui/react";
 import { useRouter } from "next/router";
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
 import { Layout } from "../../../components/layout";
+
+const getProject = gql`
+  query getProject($id: ID!) {
+    project(where: { id: $id }) {
+      id
+    }
+  }
+`;
 
 const ProjectIndexPage = () => {
   const router = useRouter();
-  const projectId = router?.query?.projectId;
+  const { projectId } = router?.query;
+
+  const { error } = useQuery(getProject, {
+    variables: { id: projectId },
+    skip: typeof projectId !== "string",
+  });
 
   useEffect(() => {
-    router.replace({
-      pathname: `/projects/${projectId}/images`,
-    });
-  }, []);
+    if (!error) {
+      router.replace({
+        pathname: `/projects/${projectId}/images`,
+      });
+    } else {
+      router.replace({
+        pathname: `/projects`,
+      });
+    }
+  }, [error]);
 
   return (
     <Layout>
