2df5fb379ef513da1cf5115779ce3190ae17b0a4
Etienne Dupont <etienne.dupont@sterblue.com>
2021-07-13 14:52:56 +0100

    Add polygon tool in stack (to update later on)

--- typescript/web-app/src/components/labelling-tool/drawing-tool-bar/drawing-tool.tsx
+++ typescript/web-app/src/components/labelling-tool/drawing-tool-bar/drawing-tool.tsx
@@ -1,5 +1,6 @@
 import { IconButton, Tooltip } from "@chakra-ui/react";
 import { RiCheckboxBlankLine } from "react-icons/ri";
+import { FaDrawPolygon } from "react-icons/fa";
 import { useHotkeys } from "react-hotkeys-hook";
 
 import { useLabellingStore, Tools } from "../../../connectors/labelling-state";
@@ -19,24 +20,48 @@ export const DrawingTool = () => {
     {},
     []
   );
-
+  useHotkeys(
+    keymap.toolPolygon.key,
+    () => setSelectedTool(Tools.POLYGON),
+    {},
+    []
+  );
   return (
-    <Tooltip
-      label={`Drawing tool [${keymap.toolBoundingBox.key}]`}
-      placement="right"
-      openDelay={300}
-    >
-      <IconButton
-        icon={<RiCheckboxBlankLine size="1.3em" />}
-        isDisabled={isImageLoading}
-        role="checkbox"
-        aria-checked={selectedTool === Tools.BOX}
-        backgroundColor="white"
-        aria-label="Drawing tool"
-        pointerEvents="initial"
-        onClick={() => setSelectedTool(Tools.BOX)}
-        isActive={selectedTool === Tools.BOX}
-      />
-    </Tooltip>
+    <>
+      <Tooltip
+        label={`Drawing tool [${keymap.toolBoundingBox.key}]`}
+        placement="right"
+        openDelay={300}
+      >
+        <IconButton
+          icon={<RiCheckboxBlankLine size="1.3em" />}
+          isDisabled={isImageLoading}
+          role="checkbox"
+          aria-checked={selectedTool === Tools.BOX}
+          backgroundColor="white"
+          aria-label="Drawing tool"
+          pointerEvents="initial"
+          onClick={() => setSelectedTool(Tools.BOX)}
+          isActive={selectedTool === Tools.BOX}
+        />
+      </Tooltip>
+      <Tooltip
+        label={`Drawing tool [${keymap.toolPolygon.key}]`}
+        placement="right"
+        openDelay={300}
+      >
+        <IconButton
+          icon={<FaDrawPolygon size="1.3em" />}
+          isDisabled={isImageLoading}
+          role="checkbox"
+          aria-checked={selectedTool === Tools.POLYGON}
+          backgroundColor="white"
+          aria-label="Drawing tool"
+          pointerEvents="initial"
+          onClick={() => setSelectedTool(Tools.POLYGON)}
+          isActive={selectedTool === Tools.POLYGON}
+        />
+      </Tooltip>
+    </>
   );
 };
