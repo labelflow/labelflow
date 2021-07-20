0d722b155609109deedc3fe9cb6834580729365b
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-07-20 11:35:18 +0200

    Use new geometry and not old one in optimistic update

--- typescript/web-app/src/components/labelling-tool/openlayers-map/select-and-modify-feature/index.tsx
+++ typescript/web-app/src/components/labelling-tool/openlayers-map/select-and-modify-feature/index.tsx
@@ -105,7 +105,7 @@ const updateLabelEffect = (
     const originalGeometry = label.geometry;
     const boundedGeometry = getBoundedGeometryFromImage(
       imageDimensions,
-      originalGeometry
+      geometry
     );
 
     client.mutate({
