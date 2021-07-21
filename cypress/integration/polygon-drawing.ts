5ddc583e72112ad58ca80fa326b053eb31fe9af3
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-07-21 11:21:09 +0200

    Integration test for drawing tool selection

---
+++ cypress/integration/polygon-drawing.ts
@@ -0,0 +1,143 @@
+import { gql } from "@apollo/client";
+
+import { client } from "../../typescript/web-app/src/connectors/apollo-client-schema";
+import { LabelCreateInput } from "../../typescript/web-app/src/graphql-types.generated";
+
+const createProject = async (name: string) => {
+  const mutationResult = await client.mutate({
+    mutation: gql`
+      mutation createProject($name: String) {
+        createProject(data: { name: $name }) {
+          id
+        }
+      }
+    `,
+    variables: {
+      name,
+    },
+  });
+
+  const {
+    data: {
+      createProject: { id },
+    },
+  } = mutationResult;
+
+  return id;
+};
+
+async function createImage(url: string, projectId: string) {
+  const mutationResult = await client.mutate({
+    mutation: gql`
+      mutation createImage($url: String, $projectId: ID!) {
+        createImage(data: { url: $url, projectId: $projectId }) {
+          id
+          name
+          width
+          height
+          url
+        }
+      }
+    `,
+    variables: {
+      projectId,
+      url,
+    },
+  });
+
+  const {
+    data: { createImage: image },
+  } = mutationResult;
+
+  return image;
+}
+
+const createLabel = (data: LabelCreateInput) => {
+  return client.mutate({
+    mutation: gql`
+      mutation createLabel($data: LabelCreateInput!) {
+        createLabel(data: $data) {
+          id
+        }
+      }
+    `,
+    variables: {
+      data,
+    },
+  });
+};
+
+const createLabelClass = async (
+  name: String,
+  color = "#ffffff",
+  projectId: string
+) => {
+  const {
+    data: {
+      createLabelClass: { id },
+    },
+  } = await client.mutate({
+    mutation: gql`
+      mutation createLabelClass(
+        $name: String!
+        $color: String!
+        $projectId: ID!
+      ) {
+        createLabelClass(
+          data: { name: $name, color: $color, projectId: $projectId }
+        ) {
+          id
+          name
+          color
+        }
+      }
+    `,
+    variables: {
+      name,
+      color,
+      projectId,
+    },
+  });
+
+  return id;
+};
+
+describe("Polygon drawing", () => {
+  let projectId: string;
+  let imageId: string;
+  beforeEach(() =>
+    cy.window().then(async () => {
+      projectId = await createProject("cypress test project");
+
+      const { id } = await createImage(
+        "https://images.unsplash.com/photo-1579513141590-c597876aefbc?auto=format&fit=crop&w=882&q=80",
+        projectId
+      );
+      imageId = id;
+
+      await createLabelClass("A new class", "#F87171", projectId);
+    })
+  );
+
+  it("switches between drawing tools", () => {
+    // See https://docs.cypress.io/guides/core-concepts/conditional-testing#Welcome-wizard
+    cy.visit(
+      `/projects/${projectId}/images/${imageId}?modal-welcome=closed&modal-update-service-worker=update`
+    );
+    cy.get('[aria-label="loading indicator"]').should("not.exist");
+    cy.get('[aria-label="Drawing polygon tool"]').should("not.exist");
+    cy.get('[aria-label="Drawing box tool"]').click();
+    cy.get('[aria-label="Change Drawing tool"]').click();
+    cy.get('[aria-label="Select bounding box tool"]').should(
+      "have.attr",
+      "aria-checked",
+      "true"
+    );
+    cy.get('[aria-label="Select polygon tool"]')
+      .should("have.attr", "aria-checked", "false")
+      .click();
+
+    cy.get('[aria-label="Drawing polygon tool"]').should("exist");
+    cy.get('[aria-label="Drawing box tool"]').should("not.exist");
+  });
+});
