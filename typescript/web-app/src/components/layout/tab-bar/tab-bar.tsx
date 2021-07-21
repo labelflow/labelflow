c4cec8130f69e88c2e370cd90b650e94d0a5a9a8
Basile <basile.diougoant@sterblue.com>
2021-07-21 11:34:19 +0200

    Updated tabs style

--- typescript/web-app/src/components/layout/tab-bar/tab-bar.tsx
+++ typescript/web-app/src/components/layout/tab-bar/tab-bar.tsx
@@ -19,12 +19,11 @@ export const TabBar = ({ tabs }: Props) => {
   return (
     <Flex
       alignItems="center"
-      bg="gray.100"
       padding={0}
-      h="64px"
+      height="64px"
       flex={0}
-      borderBottom="2px solid"
-      borderColor="gray.200"
+      borderTop="1px solid"
+      borderColor="gray.100"
       aria-label="tab bar"
     >
       {tabs.map(({ name, url, isActive }) => (
@@ -39,13 +38,10 @@ export const TabBar = ({ tabs }: Props) => {
             paddingTop="4"
             paddingInlineStart="4"
             paddingInlineEnd="4"
-            mt="0"
-            mr="0"
-            ml="0"
-            mb="-2px"
+            margin="0"
             borderBottom="2px solid"
             color={isActive ? "brand.500" : "inherit"}
-            borderColor={isActive ? "currentColor" : "inherit"}
+            borderColor={isActive ? "currentColor" : "transparent"}
             {...(isActive ? { "aria-current": "location" } : {})}
           >
             {name}
