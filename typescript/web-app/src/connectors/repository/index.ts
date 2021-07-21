e598d4bebbcf96b09f6d34642b2ba707c7f7da5c
Hugo Raynal <raynalhugo@gmail.com>
2021-07-21 14:28:34 +0200

    Order alphabetically function in reposiory

--- typescript/web-app/src/connectors/repository/index.ts
+++ typescript/web-app/src/connectors/repository/index.ts
@@ -13,37 +13,37 @@ type PartialWithNullAllowed<T> = { [P in keyof T]?: T[P] | undefined | null };
 
 export const repository = {
   image: {
-    list: list<DbImage, ImageWhereInput>(db.image),
-    getById: (id: string) => db.image.get(id),
     add: (image: DbImage) => db.image.add(image),
     count: (where?: ImageWhereInput) =>
       where ? db.image.where(where).count() : db.image.count(),
+    getById: (id: string) => db.image.get(id),
+    list: list<DbImage, ImageWhereInput>(db.image),
   },
   label: {
+    add: (label: DbLabel) => db.label.add(label),
+    count: countLabels,
+    delete: (id: string) => db.label.delete(id),
     getById: (id: string) => db.label.get(id),
     list: listLabels,
-    add: (label: DbLabel) => db.label.add(label),
-    delete: (id: string) => db.label.delete(id),
     update: (id: string, changes: PartialWithNullAllowed<DbLabel>) =>
       db.label.update(id, changes),
-    count: countLabels,
   },
   labelClass: {
-    getById: (id: string) => db.labelClass.get(id),
-    list: list<DbLabelClass, LabelClassWhereInput>(db.labelClass),
     add: (labelClass: DbLabelClass) => db.labelClass.add(labelClass),
-    delete: (id: string) => db.labelClass.delete(id),
     count: (where?: LabelClassWhereInput) =>
       where ? db.labelClass.where(where).count() : db.labelClass.count(),
+    delete: (id: string) => db.labelClass.delete(id),
+    getById: (id: string) => db.labelClass.get(id),
+    list: list<DbLabelClass, LabelClassWhereInput>(db.labelClass),
   },
   project: {
-    list: list<DbProject, null>(db.project),
     add: (project: DbProject) => db.project.add(project),
+    delete: (id: string) => db.project.delete(id),
     getById: (id: string) => db.project.get(id),
     getByName: (name: string) => db.project.get({ name }),
+    list: list<DbProject, null>(db.project),
     update: (id: string, changes: PartialWithNullAllowed<DbProject>) =>
       db.project.update(id, changes),
-    delete: (id: string) => db.project.delete(id),
   },
 };
 
