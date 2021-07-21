e3df085a08a6dd1319d613fe0217c5fa2c44bd64
Etienne Dupont <etienne.dupont@sterblue.com>
2021-07-21 12:00:07 +0100

    Breakdown code in select-and-modify-feature

--- typescript/web-app/src/components/labelling-tool/openlayers-map/select-and-modify-feature/index.tsx
+++ typescript/web-app/src/components/labelling-tool/openlayers-map/select-and-modify-feature/index.tsx
@@ -18,36 +18,13 @@ import {
   ResizeAndTranslateEvent,
 } from "./resize-and-translate-box-interaction";
 import { Effect, useUndoStore } from "../../../../connectors/undo-store";
-import { GeometryInput, LabelType } from "../../../../graphql-types.generated";
-import { getBoundedGeometryFromImage } from "../../../../connectors/resolvers/label";
+import { updateLabelEffect } from "./update-label-effect";
+import { LabelType } from "../../../../graphql-types.generated";
 
 // Extend react-openlayers-catalogue to include resize and translate interaction
 extend({
   ResizeAndTranslateBox: { object: ResizeAndTranslateBox, kind: "Interaction" },
 });
-
-const updateLabelMutation = gql`
-  mutation updateLabel($id: ID!, $geometry: GeometryInput, $labelClassId: ID) {
-    updateLabel(
-      where: { id: $id }
-      data: { geometry: $geometry, labelClassId: $labelClassId }
-    ) {
-      id
-      type
-      geometry {
-        type
-        coordinates
-      }
-      x
-      y
-      width
-      height
-      labelClass {
-        id
-      }
-    }
-  }
-`;
 
 const getLabelQuery = gql`
   query getLabel($id: ID!) {
@@ -67,126 +44,6 @@ const getLabelQuery = gql`
   }
 `;
 
-const imageDimensionsQuery = gql`
-  query imageDimensions($id: ID!) {
-    image(where: { id: $id }) {
-      id
-      width
-      height
-    }
-  }
-`;
-
-const updateLabelEffect = (
-  {
-    labelId,
-    geometry,
-    imageId,
-  }: {
-    labelId: string;
-    geometry: GeometryInput;
-    imageId?: string;
-  },
-  {
-    client,
-  }: {
-    client: ApolloClient<object>;
-  }
-): Effect => ({
-  do: async () => {
-    const { cache } = client;
-    const imageResponse = cache.readQuery<{
-      image: { width: number; height: number };
-    }>({
-      query: imageDimensionsQuery,
-      variables: { id: imageId },
-    });
-    if (imageResponse == null) {
-      throw new Error(`Missing image with id ${imageId}`);
-    }
-    const { image } = imageResponse;
-
-    const labelResponse = cache.readQuery<{
-      label: {
-        id: string;
-        geometry: GeometryInput;
-        imageId: string;
-        labelClass: {
-          id: string;
-          color: string;
-        };
-        type: LabelType;
-      };
-    }>({
-      query: getLabelQuery,
-      variables: { id: labelId },
-    });
-    if (labelResponse == null) {
-      throw new Error(`Missing label with id ${imageId}`);
-    }
-    const { label } = labelResponse;
-    const imageDimensions = {
-      width: image.width,
-      height: image.height,
-    };
-    const originalGeometry = label.geometry;
-    const boundedGeometry = getBoundedGeometryFromImage(
-      imageDimensions,
-      geometry
-    );
-
-    client.mutate({
-      mutation: updateLabelMutation,
-      variables: {
-        id: labelId,
-        geometry,
-      },
-      refetchQueries: ["getImageLabels"],
-      optimisticResponse: {
-        updateLabel: {
-          id: labelId,
-          geometry: boundedGeometry.geometry,
-          x: boundedGeometry.x,
-          y: boundedGeometry.y,
-          width: boundedGeometry.width,
-          height: boundedGeometry.height,
-          labelClass: label.labelClass,
-          type: label.type,
-          __typename: "Label",
-        },
-      },
-      update: (apolloCache, { data }) => {
-        apolloCache.writeQuery({
-          query: getLabelQuery,
-          data: data?.updateLabel,
-        });
-      },
-    });
-
-    return { id: labelId, originalGeometry };
-  },
-  undo: async ({
-    id,
-    originalGeometry,
-  }: {
-    id: string;
-    originalGeometry: GeometryInput;
-  }): Promise<string> => {
-    await client.mutate({
-      mutation: updateLabelMutation,
-      variables: {
-        id: labelId,
-        geometry: {
-          type: originalGeometry.type,
-          coordinates: originalGeometry.coordinates,
-        },
-      },
-      refetchQueries: ["getImageLabels"],
-    });
-    return id;
-  },
-});
-
 export const interactionEnd = async (
   e: TranslateEvent | ModifyEvent | ResizeAndTranslateEvent | null,
   perform: (effect: Effect<any>) => Promise<void>,
@@ -194,7 +51,7 @@ export const interactionEnd = async (
   imageId: string,
   toast: (options: UseToastOptions) => void
 ) => {
-  const feature = e.features.item(0) as Feature<Polygon>;
+  const feature = e?.features?.item(0) as Feature<Polygon>;
   if (feature != null) {
     const coordinates = feature.getGeometry().getCoordinates();
     const geometry = { type: "Polygon", coordinates };
