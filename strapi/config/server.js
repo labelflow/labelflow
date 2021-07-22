c817c72f3c8285d09e785fccfd3a72bb88e02464
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2021-07-22 17:00:40 +0100

    Heroku compat

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('STRAPI_HOST', '0.0.0.0'),
-  port: env.int('STRAPI_PORT', 1337),
+  host: env('STRAPI_HOST', env('HOST', '0.0.0.0')),
+  port: env.int('STRAPI_PORT', env.int('PORT', 1337)),
   admin: {
     auth: {
       secret: env('STRAPI_ADMIN_JWT_SECRET', ''),
