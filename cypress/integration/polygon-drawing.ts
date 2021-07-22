c6da0a685d666295d0b701dfad44e07a47b76cc6
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-07-22 16:34:00 +0200

    Fix lint issues

--- cypress/integration/polygon-drawing.ts
+++ cypress/integration/polygon-drawing.ts
@@ -1,7 +1,6 @@
 import { gql } from "@apollo/client";
 
 import { client } from "../../typescript/web-app/src/connectors/apollo-client-schema";
-import { LabelCreateInput } from "../../typescript/web-app/src/graphql-types.generated";
 
 const createProject = async (name: string) => {
   const mutationResult = await client.mutate({
