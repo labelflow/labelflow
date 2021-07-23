3135d32a8311aeaa57c5f6eb95bee7cc427bf96b
Hugo Raynal <raynalhugo@gmail.com>
2021-07-23 19:30:52 +0200

    Basic Apollo server

--- typescript/db/src/index.ts
+++ typescript/db/src/index.ts
@@ -1,16 +1,41 @@
 import { PrismaClient } from "@prisma/client";
+import { ApolloServer } from "apollo-server";
+import { join } from "path";
+import { loadSchemaSync } from "@graphql-tools/load";
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import { addResolversToSchema } from "@graphql-tools/schema";
+
+const schema = loadSchemaSync(
+  join(__dirname, "../../../data/__generated__/schema.graphql"),
+  {
+    loaders: [new GraphQLFileLoader()],
+  }
+);
 
 const prisma = new PrismaClient();
 
-async function main() {
-  console.log("Projects");
-  console.log(await prisma.project.findMany());
-}
+const resolvers = {
+  Query: {
+    projects: () => {
+      return prisma.project.findMany();
+    },
+  },
+};
 
-main()
-  .catch((e) => {
-    throw e;
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+const schemaWithResolvers = addResolversToSchema({
+  schema,
+  resolvers,
+});
+
+const server = new ApolloServer({
+  introspection: true,
+  context: {},
+  schema: schemaWithResolvers,
+});
+
+server.listen({ port: 5000 }).then(async ({ url }) => {
+  console.log(`\
+  🚀 Server ready at: ${url}
+  ⭐️ See sample queries: http://pris.ly/e/ts/graphql#using-the-graphql-api
+    `);
+});
