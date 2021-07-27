de80c066cfd4c8a562f88d10d20e297e21b97aa4
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-07-27 16:33:20 +0200

    Add tests for class item component

---
+++ typescript/web-app/src/components/project-class-list/__tests__/class-item.tsx
@@ -0,0 +1,143 @@
+import { PropsWithChildren } from "react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { ApolloProvider } from "@apollo/client";
+import { ClassItem } from "../class-item";
+import { client } from "../../../connectors/apollo-client-schema";
+import { theme } from "../../../theme";
+
+const classDefault = {
+  color: "#F59E0B",
+  name: "someClass",
+  shortcut: "myShortcut",
+  id: "myClassId",
+};
+
+/**
+ * Mock the apollo client to avoid creating corrupted files that allows
+ * us to identify a behaviour.
+ */
+jest.mock("../../../connectors/apollo-client-schema", () => {
+  const original = jest.requireActual(
+    "../../../connectors/apollo-client-schema"
+  );
+
+  return {
+    client: { ...original.client, mutate: jest.fn(original.client.mutate) },
+  };
+});
+
+const wrapper = ({ children }: PropsWithChildren<{}>) => (
+  <ApolloProvider client={client}>
+    <ChakraProvider theme={theme} resetCSS>
+      {children}
+    </ChakraProvider>
+  </ApolloProvider>
+);
+
+const setEditClassId = jest.fn();
+
+describe("Project class list item tests", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+
+  it("Should display a class with the possibility to edit and delete it", () => {
+    render(
+      <ClassItem
+        edit={false}
+        setEditClassId={setEditClassId}
+        projectId="myProjectId"
+        {...classDefault}
+      />,
+      { wrapper }
+    );
+    expect(screen.getByText(/someClass/i)).toBeDefined();
+    expect(
+      screen.getByRole("button", { name: "Edit class someClass name" })
+    ).toBeDefined();
+    expect(
+      screen.getByRole("button", { name: "Delete class someClass" })
+    ).toBeDefined();
+    expect(screen.getByText(/myShortcut/i)).toBeDefined();
+  });
+
+  it("Should call function to edit name when edit button is clicked", async () => {
+    render(
+      <ClassItem
+        edit={false}
+        setEditClassId={setEditClassId}
+        projectId="myProjectId"
+        {...classDefault}
+      />,
+      { wrapper }
+    );
+
+    fireEvent.click(
+      screen.getByRole("button", { name: "Edit class someClass name" })
+    );
+    expect(setEditClassId).toHaveBeenCalledWith("myClassId");
+  });
+
+  it("Should display clear and save options in edit mode", async () => {
+    render(
+      <ClassItem
+        edit
+        setEditClassId={setEditClassId}
+        projectId="myProjectId"
+        {...classDefault}
+      />,
+      { wrapper }
+    );
+    await waitFor(() =>
+      expect(screen.getByRole("button", { name: "Save" })).toBeDefined()
+    );
+    await waitFor(() =>
+      expect(screen.getByRole("button", { name: "Cancel" })).toBeDefined()
+    );
+  });
+
+  it("Should call update label mutation with new name when clicking on save button", async () => {
+    render(
+      <ClassItem
+        edit
+        setEditClassId={setEditClassId}
+        projectId="myProjectId"
+        {...classDefault}
+      />,
+      { wrapper }
+    );
+    fireEvent.change(
+      screen.getByRole("textbox", { name: "edit-class-name-input" }),
+      {
+        target: { value: "NewClassName" },
+      }
+    );
+    fireEvent.click(screen.getByRole("button", { name: "Save" }));
+    expect(client.mutate).toHaveBeenCalledWith(
+      expect.objectContaining({
+        variables: { id: "myClassId", name: "NewClassName" },
+      })
+    );
+  });
+
+  it("Should cancel name edition when clicking on cancel", async () => {
+    render(
+      <ClassItem
+        edit
+        setEditClassId={setEditClassId}
+        projectId="myProjectId"
+        {...classDefault}
+      />,
+      { wrapper }
+    );
+    fireEvent.change(
+      screen.getByRole("textbox", { name: "edit-class-name-input" }),
+      {
+        target: { value: "NewClassName" },
+      }
+    );
+    fireEvent.click(screen.getByRole("button", { name: "Cancel" }));
+    expect(client.mutate).toHaveBeenCalledTimes(0);
+  });
+});
