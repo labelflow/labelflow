f47ed32b1b225c1b6ae2f8dc58fd55963e30585c
Etienne Dupont <etienne.dupont@sterblue.com>
2021-07-29 17:04:34 +0100

    Fix deterministic test

--- python/iog/test_model_deterministic.py
+++ python/iog/test_model_deterministic.py
@@ -1,18 +1,21 @@
-from inference_on_image import process, refine, clear_cache
+from inference_on_image import process, refine
 import json
-import os
-import shutil
+# import os
+# import shutil
+from cache import Cache
+import time
 
 inputs_inference = json.load(open("inputs/inputs_inference.json", "r"))
 inputs_refine = json.load(open("inputs/inputs_refinement.json", "r"))
+cache = Cache()
 
 
 def main():
-    clear_cache()
-    shutil.rmtree("results")
-    os.mkdir("results")
-    shutil.rmtree("outputs")
-    os.mkdir("outputs")
+    cache.clear()
+    # shutil.rmtree("results")
+    # os.mkdir("results")
+    # shutil.rmtree("outputs")
+    # os.mkdir("outputs")
     id = inputs_inference["id"]
     imageUrl = inputs_inference["imageUrl"]
     x = inputs_inference["x"]
@@ -20,11 +23,17 @@ def main():
     width = inputs_inference["width"]
     height = inputs_inference["height"]
     # Run inference
-    process(imageUrl, x, y, width, height, id)
+    time_start = time.time()
+    process(imageUrl, x, y, width, height, id, cache=cache)
+    time_end = time.time()
+    print(f"Inference took {time_end-time_start} s")
     # Run refinement
     pointsInside = inputs_refine.get("pointsInside", [])
     pointsOutside = inputs_refine.get("pointsOutside", [])
-    refine(pointsInside, pointsOutside, id)
+    time_start = time.time()
+    refine(pointsInside, pointsOutside, id, cache=cache)
+    time_end = time.time()
+    print(f"Refinement took {time_end-time_start} s")
 
 
 if __name__ == "__main__":
