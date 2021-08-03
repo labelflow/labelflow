56095ef005e91a732323b98f52373e4367089785
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-08-03 16:12:14 +0200

    Update labels and delete class sequentially
    Co-authored-by: Yvonnick FRIN <frin.yvonnick@gmail.com>

--- typescript/web-app/src/connectors/repository/label-class.ts
+++ typescript/web-app/src/connectors/repository/label-class.ts
@@ -1,14 +1,11 @@
 import { db } from "../database";
 
 export const deleteLabelClass = async (id: string) => {
-  const labelsWithLabelClassToDelete = await db.label.filter(
+  await db.label.filter(
     (label) => label.labelClassId === id
-  );
-
-  const setLabelsToNoneLabelClassPromise = labelsWithLabelClassToDelete.modify({
+  ).modify({
     labelClassId: null,
   });
 
   await db.labelClass.delete(id);
-  await setLabelsToNoneLabelClassPromise;
 };
