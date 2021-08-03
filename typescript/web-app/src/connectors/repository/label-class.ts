24a7e7901f93a6c5904cbd57360e853fee0144ac
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-08-03 16:41:42 +0200

    Fix lint errors

--- typescript/web-app/src/connectors/repository/label-class.ts
+++ typescript/web-app/src/connectors/repository/label-class.ts
@@ -1,11 +1,11 @@
 import { db } from "../database";
 
 export const deleteLabelClass = async (id: string) => {
-  await db.label.filter(
-    (label) => label.labelClassId === id
-  ).modify({
-    labelClassId: null,
-  });
+  await db.label
+    .filter((label) => label.labelClassId === id)
+    .modify({
+      labelClassId: null,
+    });
 
   await db.labelClass.delete(id);
 };
