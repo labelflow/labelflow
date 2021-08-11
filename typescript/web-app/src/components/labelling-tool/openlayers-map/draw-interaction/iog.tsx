fa48f596a4160e715f5a8796791754e9fc6f7f26
Hugo Raynal <raynalhugo@gmail.com>
2021-08-11 18:02:58 +0200

    Prevent adding a point inside or outside when moving the entire image

--- typescript/web-app/src/components/labelling-tool/openlayers-map/draw-interaction/iog.tsx
+++ typescript/web-app/src/components/labelling-tool/openlayers-map/draw-interaction/iog.tsx
@@ -15,6 +15,7 @@ import { Coordinate } from "ol/coordinate";
 import CircleStyle from "ol/style/Circle";
 import { LabelType } from "@labelflow/graphql-types";
 import { ModifyEvent } from "ol/interaction/Modify";
+import { useMap } from "@labelflow/react-openlayers-fiber";
 import {
   useLabellingStore,
   DrawingToolState,
@@ -76,6 +77,7 @@ const runIogMutation = gql`
 const geometryFunction = createBox();
 
 export const DrawIogInteraction = ({ imageId }: { imageId: string }) => {
+  const map = useMap();
   const drawRef = useRef<OlDraw>(null);
   const client = useApolloClient();
 
@@ -260,6 +262,13 @@ export const DrawIogInteraction = ({ imageId }: { imageId: string }) => {
     [selectedLabelId]
   );
 
+  useEffect(() => {
+    if (selectedLabelId != null) {
+      map?.on("click", createPointInsideOrOutside);
+      return () => map?.un("click", createPointInsideOrOutside);
+    }
+  }, [map, selectedLabelId, createPointInsideOrOutside]);
+
   const performIOGFromDrawEvent = async (drawEvent: DrawEvent) => {
     const openLayersGeometry = drawEvent.feature.getGeometry();
     const geometry = new GeoJSON().writeGeometryObject(
@@ -360,9 +369,6 @@ export const DrawIogInteraction = ({ imageId }: { imageId: string }) => {
     />
   ) : (
     <>
-      <olInteractionPointer
-        args={{ handleDownEvent: createPointInsideOrOutside }}
-      />
       {centerPointFeature != null && (
         <olInteractionModify
           args={{ features: new Collection([centerPointFeature]) }}
