2a4752acbe28b697f2681e86a602f8580d7e151b
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-08-12 15:44:04 +0100

    Add API to query strapi articles

---
+++ typescript/web-app/src/connectors/strapi/index.ts
@@ -0,0 +1,172 @@
+async function fetchAPI(query, { variables } = {}) {
+  const res = await fetch(`https://strapi.labelflow.ai/graphql`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+
+  const json = await res.json();
+  if (json.errors) {
+    console.error(json.errors);
+    throw new Error("Failed to fetch API");
+  }
+
+  return json.data;
+}
+
+export async function getPreviewArticleBySlug(slug: string): Promise<{
+  articles: { slug: string }[];
+}> {
+  const data = await fetchAPI(
+    `
+    query ArticleBySlug($where: JSON) {
+      articles(where: $where) {
+        slug
+      }
+    }
+    `,
+    {
+      variables: {
+        where: {
+          slug,
+        },
+      },
+    }
+  );
+  return data?.articles[0];
+}
+
+export async function getAllArticlesWithSlug(): Promise<{
+  articles: { slug: string }[];
+}> {
+  const data = await fetchAPI(`
+      {
+        articles {
+          slug
+        }
+      }
+    `);
+  return data?.allArticles;
+}
+
+export async function getAllArticlesForHome(): Promise<
+  {
+    id: string;
+    title: string;
+    slug: string;
+    created_at: string;
+    published_at: string;
+    description: string;
+    category: {
+      id: string;
+      name: string;
+    };
+    image: {
+      url: string;
+    };
+    author: {
+      name: string;
+      picture: {
+        url: string;
+      };
+    };
+  }[]
+> {
+  const data = await fetchAPI(
+    `
+    query Articles($where:JSON) {
+        articles(sort: "created_at:desc", limit: 3, publicationState:LIVE) {
+          id
+          title
+          slug
+          created_at
+          published_at
+          description
+          category {
+            id
+            name
+          }
+          image {
+            url
+          }
+          author {
+            name
+            picture {
+              url
+            }
+          }
+        }
+      }
+      `
+  );
+  return data?.articles;
+}
+
+export async function getArticle(slug: string) {
+  const data = await fetchAPI(
+    `
+    query ArticleBySlug($where: JSON, $where_ne: JSON) {
+      articles(where: $where, publicationState:LIVE) {
+          id
+          title
+          slug
+          created_at
+          published_at
+          description
+          category {
+            id
+            name
+          }
+          image {
+            url
+          }
+          author {
+            name
+            picture {
+              url
+            }
+          }
+        }
+      }
+      moreArticles: articles(sort: "created_at:desc", limit: 3, where: $where_ne, publicationState:LIVE) {
+          id
+          title
+          slug
+          created_at
+          published_at
+          description
+          category {
+            id
+            name
+          }
+          image {
+            url
+          }
+          author {
+            name
+            picture {
+              url
+            }
+          }
+        }
+      }
+    }
+    `,
+    {
+      variables: {
+        where: {
+          slug,
+        },
+        where_ne: {
+          slug_ne: slug,
+        },
+      },
+    }
+  );
+  return data;
+}
