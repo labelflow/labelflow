fb352009e4e779e6a7be2d05a6983c6b7dd79215
Etienne Dupont <etiennedupont>
2021-08-18 14:09:42 +0200

    Make mypath more generic

--- python/iog/mypath.py
+++ python/iog/mypath.py
@@ -1,17 +1,16 @@
-
 class Path(object):
     @staticmethod
     def db_root_dir(database):
-        if database == 'pascal':
-            return '/Users/etiennedupont/Code/iog/data/VOCdevkit/VOC2012'  # folder that contains VOCdevkit/.
+        if database == "pascal":
+            return "data/VOCdevkit/VOC2012"  # folder that contains VOCdevkit/.
 
-        elif database == 'sbd':
-            return '/path/to/SBD/'  # folder with img/, inst/, cls/, etc.
+        elif database == "sbd":
+            return "/path/to/SBD/"  # folder with img/, inst/, cls/, etc.
         else:
-            print('Database {} not available.'.format(database))
+            print("Database {} not available.".format(database))
             raise NotImplementedError
 
     @staticmethod
     def models_dir():
-        return '/Users/etiennedupont/Code/iog/data/IOG_PASCAL_SBD_REFINEMENT.pth'    
+        return "data/IOG_PASCAL_SBD_REFINEMENT.pth"
         #'resnet101-5d3b4d8f.pth' #resnet50-19c8e357.pth'
