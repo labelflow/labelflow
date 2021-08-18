f62843d9fcfeac276d5213eab1a7ace3623500a3
Etienne Dupont <etiennedupont>
2021-08-18 19:23:51 +0200

    Format

--- python/iog/dataloaders/helpers.py
+++ python/iog/dataloaders/helpers.py
@@ -15,11 +15,22 @@ def tens2image(im):
         return tmp.transpose((1, 2, 0))
 
 
-def crop2fullmask(crop_mask, bbox, im=None, im_size=None, zero_pad=False, relax=0, mask_relax=True,
-                  interpolation=cv2.INTER_CUBIC, scikit=False):
+def crop2fullmask(
+    crop_mask,
+    bbox,
+    im=None,
+    im_size=None,
+    zero_pad=False,
+    relax=0,
+    mask_relax=True,
+    interpolation=cv2.INTER_CUBIC,
+    scikit=False,
+):
     if scikit:
         from skimage.transform import resize as sk_resize
-    assert(not(im is None and im_size is None)), 'You have to provide an image or the image size  True'  
+    assert not (
+        im is None and im_size is None
+    ), "You have to provide an image or the image size  True"
     if im is None:
         im_si = im_size
     else:
@@ -28,39 +39,49 @@ def crop2fullmask(crop_mask, bbox, im=None, im_size=None, zero_pad=False, relax=
     bounds = (0, 0, im_si[1] - 1, im_si[0] - 1)
 
     # Valid bounding box locations as (x_min, y_min, x_max, y_max)
-    bbox_valid = (max(bbox[0], bounds[0]),
-                  max(bbox[1], bounds[1]),
-                  min(bbox[2], bounds[2]),
-                  min(bbox[3], bounds[3]))
+    bbox_valid = (
+        max(bbox[0], bounds[0]),
+        max(bbox[1], bounds[1]),
+        min(bbox[2], bounds[2]),
+        min(bbox[3], bounds[3]),
+    )
 
     # Bounding box of initial mask
-    bbox_init = (bbox[0] + relax,
-                 bbox[1] + relax,
-                 bbox[2] - relax,
-                 bbox[3] - relax)
+    bbox_init = (bbox[0] + relax, bbox[1] + relax, bbox[2] - relax, bbox[3] - relax)
 
     if zero_pad:
         # Offsets for x and y
         offsets = (-bbox[0], -bbox[1])
     else:
-#        assert((bbox == bbox_valid).all())
+        #        assert((bbox == bbox_valid).all())
         offsets = (-bbox_valid[0], -bbox_valid[1])
 
     # Simple per element addition in the tuple
     inds = tuple(map(sum, zip(bbox_valid, offsets + offsets)))
 
     if scikit:
-        crop_mask = sk_resize(crop_mask, (bbox[3] - bbox[1] + 1, bbox[2] - bbox[0] + 1), order=0, mode='constant').astype(crop_mask.dtype)
+        crop_mask = sk_resize(
+            crop_mask,
+            (bbox[3] - bbox[1] + 1, bbox[2] - bbox[0] + 1),
+            order=0,
+            mode="constant",
+        ).astype(crop_mask.dtype)
     else:
-        crop_mask = cv2.resize(crop_mask, (bbox[2] - bbox[0] + 1, bbox[3] - bbox[1] + 1), interpolation=interpolation)
+        crop_mask = cv2.resize(
+            crop_mask,
+            (bbox[2] - bbox[0] + 1, bbox[3] - bbox[1] + 1),
+            interpolation=interpolation,
+        )
     result_ = np.zeros(im_si)
-    result_[bbox_valid[1]:bbox_valid[3] + 1, bbox_valid[0]:bbox_valid[2] + 1] = \
-        crop_mask[inds[1]:inds[3] + 1, inds[0]:inds[2] + 1]
+    result_[
+        bbox_valid[1] : bbox_valid[3] + 1, bbox_valid[0] : bbox_valid[2] + 1
+    ] = crop_mask[inds[1] : inds[3] + 1, inds[0] : inds[2] + 1]
 
     result = np.zeros(im_si)
     if mask_relax:
-        result[bbox_init[1]:bbox_init[3]+1, bbox_init[0]:bbox_init[2]+1] = \
-            result_[bbox_init[1]:bbox_init[3]+1, bbox_init[0]:bbox_init[2]+1]
+        result[
+            bbox_init[1] : bbox_init[3] + 1, bbox_init[0] : bbox_init[2] + 1
+        ] = result_[bbox_init[1] : bbox_init[3] + 1, bbox_init[0] : bbox_init[2] + 1]
     else:
         result = result_
 
@@ -70,21 +91,25 @@ def crop2fullmask(crop_mask, bbox, im=None, im_size=None, zero_pad=False, relax=
 def overlay_mask(im, ma, colors=None, alpha=0.5):
     assert np.max(im) <= 1.0
     if colors is None:
-        colors = np.load(os.path.join(os.path.dirname(__file__), 'pascal_map.npy'))/255.
+        colors = (
+            np.load(os.path.join(os.path.dirname(__file__), "pascal_map.npy")) / 255.0
+        )
     else:
-        colors = np.append([[0.,0.,0.]], colors, axis=0);
+        colors = np.append([[0.0, 0.0, 0.0]], colors, axis=0)
 
     if ma.ndim == 3:
-        assert len(colors) >= ma.shape[0], 'Not enough colors'
+        assert len(colors) >= ma.shape[0], "Not enough colors"
     ma = ma.astype(np.bool)
     im = im.astype(np.float32)
 
     if ma.ndim == 2:
-        fg = im * alpha+np.ones(im.shape) * (1 - alpha) * colors[1, :3]   # np.array([0,0,255])/255.0
+        fg = (
+            im * alpha + np.ones(im.shape) * (1 - alpha) * colors[1, :3]
+        )  # np.array([0,0,255])/255.0
     else:
         fg = []
         for n in range(ma.ndim):
-            fg.append(im * alpha + np.ones(im.shape) * (1 - alpha) * colors[1+n, :3])
+            fg.append(im * alpha + np.ones(im.shape) * (1 - alpha) * colors[1 + n, :3])
     # Whiten background
     bg = im.copy()
     if ma.ndim == 2:
@@ -101,89 +126,105 @@ def overlay_mask(im, ma, colors=None, alpha=0.5):
         bg[total_ma == 0] = im[total_ma == 0]
 
     # [-2:] is s trick to be compatible both with opencv 2 and 3
-    contours = cv2.findContours(total_ma.copy().astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+    contours = cv2.findContours(
+        total_ma.copy().astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+    )[-2:]
     cv2.drawContours(bg, contours[0], -1, (0.0, 0.0, 0.0), 1)
 
     return bg
+
+
 import PIL
+
+
 def overlay_masks(im, masks, alpha=0.5):
-    colors = np.load(os.path.join(os.path.dirname(__file__), 'pascal_map.npy'))/255.
-    
+    colors = np.load(os.path.join(os.path.dirname(__file__), "pascal_map.npy")) / 255.0
+
     if isinstance(masks, np.ndarray):
         masks = [masks]
 
-    assert len(colors) >= len(masks), 'Not enough colors'
+    assert len(colors) >= len(masks), "Not enough colors"
 
     ov = im.copy()
-    ov_black = im.copy()*0
-    
-    imgZero = np.zeros(np.array(masks, dtype = np.uint8).shape,np.uint8)
+    ov_black = im.copy() * 0
+
+    imgZero = np.zeros(np.array(masks, dtype=np.uint8).shape, np.uint8)
     im = im.astype(np.float32)
     total_ma = np.zeros([im.shape[0], im.shape[1]])
     i = 1
     for ma in masks:
         ma = ma.astype(np.bool)
-        fg = im * alpha+np.ones(im.shape) * (1 - alpha) * colors[i, :3]   # np.array([0,0,255])/255.0
+        fg = (
+            im * alpha + np.ones(im.shape) * (1 - alpha) * colors[i, :3]
+        )  # np.array([0,0,255])/255.0
         i = i + 1
         ov[ma == 1] = fg[ma == 1]
         total_ma += ma
 
         # [-2:] is s trick to be compatible both with opencv 2 and 3
-        contours = cv2.findContours(ma.copy().astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2:]
+        contours = cv2.findContours(
+            ma.copy().astype(np.uint8), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE
+        )[-2:]
         cv2.drawContours(ov, contours[0], -1, (0.0, 0.0, 0.0), 1)
-        cv2.drawContours(ov_black, contours[0], -1, (255, 255, 255), -1)#only draw a round
+        cv2.drawContours(
+            ov_black, contours[0], -1, (255, 255, 255), -1
+        )  # only draw a round
     ov[total_ma == 0] = im[total_ma == 0]
 
     return ov_black
 
-from scipy import ndimage    
+
+from scipy import ndimage
+
+
 def getPositon(distance_transform):
-    a = np.mat(distance_transform) 
-    raw, column = a.shape# get the matrix of a raw and column
-    _positon = np.argmax(a)# get the index of max in the a
+    a = np.mat(distance_transform)
+    raw, column = a.shape  # get the matrix of a raw and column
+    _positon = np.argmax(a)  # get the index of max in the a
     m, n = divmod(_positon, column)
-    raw=m
-    column=n
-#    print "The raw is " ,m
-#    print "The column is ",  n
-#    print "The max of the a is ", a[m , n]
-#    print(raw,column,a[m , n])
-    return  raw,column
+    raw = m
+    column = n
+    #    print "The raw is " ,m
+    #    print "The column is ",  n
+    #    print "The max of the a is ", a[m , n]
+    #    print(raw,column,a[m , n])
+    return raw, column
+
 
 def iog_points(mask, pad_pixel=10):
     def find_point(id_x, id_y, ids):
         sel_id = ids[0][random.randint(0, len(ids[0]) - 1)]
         return [id_x[sel_id], id_y[sel_id]]
 
-    inds_y, inds_x = np.where(mask > 0.5)   
-    [h,w]=mask.shape
-    left = find_point(inds_x, inds_y, np.where(inds_x <= np.min(inds_x))) 
-    right = find_point(inds_x, inds_y, np.where(inds_x >= np.max(inds_x))) 
-    top = find_point(inds_x, inds_y, np.where(inds_y <= np.min(inds_y))) 
+    inds_y, inds_x = np.where(mask > 0.5)
+    [h, w] = mask.shape
+    left = find_point(inds_x, inds_y, np.where(inds_x <= np.min(inds_x)))
+    right = find_point(inds_x, inds_y, np.where(inds_x >= np.max(inds_x)))
+    top = find_point(inds_x, inds_y, np.where(inds_y <= np.min(inds_y)))
     bottom = find_point(inds_x, inds_y, np.where(inds_y >= np.max(inds_y)))
 
-    x_min=left[0]
-    x_max=right[0]
-    y_min=top[1]
-    y_max=bottom[1]
+    x_min = left[0]
+    x_max = right[0]
+    y_min = top[1]
+    y_max = bottom[1]
 
-    map_xor = (mask > 0.5)
-    h,w = map_xor.shape
-    map_xor_new = np.zeros((h+2,w+2))
-    map_xor_new[1:(h+1),1:(w+1)] = map_xor[:,:]
-    distance_transform=ndimage.distance_transform_edt(map_xor_new)
-    distance_transform_back = distance_transform[1:(h+1),1:(w+1)]    
-    raw,column=getPositon(distance_transform_back)
-    center_point = [column,raw]
+    map_xor = mask > 0.5
+    h, w = map_xor.shape
+    map_xor_new = np.zeros((h + 2, w + 2))
+    map_xor_new[1 : (h + 1), 1 : (w + 1)] = map_xor[:, :]
+    distance_transform = ndimage.distance_transform_edt(map_xor_new)
+    distance_transform_back = distance_transform[1 : (h + 1), 1 : (w + 1)]
+    raw, column = getPositon(distance_transform_back)
+    center_point = [column, raw]
 
-    left_top=[max(x_min-pad_pixel,0),  max(y_min-pad_pixel,0)]
-    left_bottom=[max(x_min-pad_pixel ,0),     min(y_max+pad_pixel,h)]
-    right_top=[min(x_max+pad_pixel,w),          max(y_min-pad_pixel,0)]
-    righr_bottom=[min(x_max+pad_pixel ,w),    min(y_max+pad_pixel,h)]
-    a=[center_point,left_top,left_bottom,right_top,righr_bottom]  
+    left_top = [max(x_min - pad_pixel, 0), max(y_min - pad_pixel, 0)]
+    left_bottom = [max(x_min - pad_pixel, 0), min(y_max + pad_pixel, h)]
+    right_top = [min(x_max + pad_pixel, w), max(y_min - pad_pixel, 0)]
+    righr_bottom = [min(x_max + pad_pixel, w), min(y_max + pad_pixel, h)]
+    a = [center_point, left_top, left_bottom, right_top, righr_bottom]
 
     return np.array(a)
-    
+
 
 def get_bbox(mask, points=None, pad=0, zero_pad=False):
     if points is not None:
@@ -218,10 +259,12 @@ def crop_from_bbox(img, bbox, zero_pad=False):
     bounds = (0, 0, img.shape[1] - 1, img.shape[0] - 1)
 
     # Valid bounding box locations as (x_min, y_min, x_max, y_max)
-    bbox_valid = (max(bbox[0], bounds[0]),
-                  max(bbox[1], bounds[1]),
-                  min(bbox[2], bounds[2]),
-                  min(bbox[3], bounds[3]))
+    bbox_valid = (
+        max(bbox[0], bounds[0]),
+        max(bbox[1], bounds[1]),
+        min(bbox[2], bounds[2]),
+        min(bbox[3], bounds[3]),
+    )
 
     if zero_pad:
         # Initialize crop size (first 2 dimensions)
@@ -231,8 +274,11 @@ def crop_from_bbox(img, bbox, zero_pad=False):
         offsets = (-bbox[0], -bbox[1])
 
     else:
-        assert(bbox == bbox_valid)
-        crop = np.zeros((bbox_valid[3] - bbox_valid[1] + 1, bbox_valid[2] - bbox_valid[0] + 1), dtype=img.dtype)
+        assert bbox == bbox_valid
+        crop = np.zeros(
+            (bbox_valid[3] - bbox_valid[1] + 1, bbox_valid[2] - bbox_valid[0] + 1),
+            dtype=img.dtype,
+        )
         offsets = (-bbox_valid[0], -bbox_valid[1])
 
     # Simple per element addition in the tuple
@@ -240,12 +286,14 @@ def crop_from_bbox(img, bbox, zero_pad=False):
 
     img = np.squeeze(img)
     if img.ndim == 2:
-        crop[inds[1]:inds[3] + 1, inds[0]:inds[2] + 1] = \
-            img[bbox_valid[1]:bbox_valid[3] + 1, bbox_valid[0]:bbox_valid[2] + 1]
+        crop[inds[1] : inds[3] + 1, inds[0] : inds[2] + 1] = img[
+            bbox_valid[1] : bbox_valid[3] + 1, bbox_valid[0] : bbox_valid[2] + 1
+        ]
     else:
         crop = np.tile(crop[:, :, np.newaxis], [1, 1, 3])  # Add 3 RGB Channels
-        crop[inds[1]:inds[3] + 1, inds[0]:inds[2] + 1, :] = \
-            img[bbox_valid[1]:bbox_valid[3] + 1, bbox_valid[0]:bbox_valid[2] + 1, :]
+        crop[inds[1] : inds[3] + 1, inds[0] : inds[2] + 1, :] = img[
+            bbox_valid[1] : bbox_valid[3] + 1, bbox_valid[0] : bbox_valid[2] + 1, :
+        ]
 
     return crop
 
@@ -260,7 +308,11 @@ def fixed_resize(sample, resolution, flagval=None):
 
     if isinstance(resolution, int):
         tmp = [resolution, resolution]
-        tmp[np.argmax(sample.shape[:2])] = int(round(float(resolution)/np.min(sample.shape[:2])*np.max(sample.shape[:2])))
+        tmp[np.argmax(sample.shape[:2])] = int(
+            round(
+                float(resolution) / np.min(sample.shape[:2]) * np.max(sample.shape[:2])
+            )
+        )
         resolution = tuple(tmp)
 
     if sample.ndim == 2 or (sample.ndim == 3 and sample.shape[2] == 3):
@@ -269,15 +321,19 @@ def fixed_resize(sample, resolution, flagval=None):
         tmp = sample
         sample = np.zeros(np.append(resolution, tmp.shape[2]), dtype=np.float32)
         for ii in range(sample.shape[2]):
-            sample[:, :, ii] = cv2.resize(tmp[:, :, ii], resolution[::-1], interpolation=flagval)
+            sample[:, :, ii] = cv2.resize(
+                tmp[:, :, ii], resolution[::-1], interpolation=flagval
+            )
     return sample
 
 
 def crop_from_mask(img, mask, relax=0, zero_pad=False):
     if mask.shape[:2] != img.shape[:2]:
-        mask = cv2.resize(mask, dsize=tuple(reversed(img.shape[:2])), interpolation=cv2.INTER_NEAREST)
+        mask = cv2.resize(
+            mask, dsize=tuple(reversed(img.shape[:2])), interpolation=cv2.INTER_NEAREST
+        )
 
-    assert(mask.shape[:2] == img.shape[:2])
+    assert mask.shape[:2] == img.shape[:2]
     bbox = get_bbox(mask, pad=relax, zero_pad=zero_pad)
 
     if bbox is None:
@@ -289,7 +345,7 @@ def crop_from_mask(img, mask, relax=0, zero_pad=False):
 
 
 def make_gaussian(size, sigma=10, center=None, d_type=np.float64):
-    """ Make a square gaussian kernel.
+    """Make a square gaussian kernel.
     size: is the dimensions of the output gaussian
     sigma: is full-width-half-maximum, which
     can be thought of as an effective radius.
@@ -305,11 +361,13 @@ def make_gaussian(size, sigma=10, center=None, d_type=np.float64):
         x0 = center[0]
         y0 = center[1]
 
-    return np.exp(-4 * np.log(2) * ((x - x0) ** 2 + (y - y0) ** 2) / sigma ** 2).astype(d_type)
+    return np.exp(-4 * np.log(2) * ((x - x0) ** 2 + (y - y0) ** 2) / sigma ** 2).astype(
+        d_type
+    )
 
-    
+
 def make_gt(img, labels, sigma=10, one_mask_per_point=False):
-    """ Make the ground-truth for  landmark.
+    """Make the ground-truth for  landmark.
     img: the original color image
     labels: label with the Gaussian center(s) [[x0, y0],[x1, y1],...]
     sigma: sigma of the Gaussian.
@@ -318,47 +376,54 @@ def make_gt(img, labels, sigma=10, one_mask_per_point=False):
 
     h, w = img.shape[:2]
     if labels is None:
-        gt = make_gaussian((h, w), center=(h//2, w//2), sigma=sigma)
+        gt = make_gaussian((h, w), center=(h // 2, w // 2), sigma=sigma)
         gt_0 = np.zeros(shape=(h, w), dtype=np.float64)
         gt_0 = gt
         gt_1 = np.zeros(shape=(h, w), dtype=np.float64)
-        
+
         gtout = np.zeros(shape=(h, w, 2))
-        gtout[:, :, 0]=gt_0
-        gtout[:, :, 1]=gt_1
-        gtout = gtout.astype(dtype=img.dtype) #(0~1)        
+        gtout[:, :, 0] = gt_0
+        gtout[:, :, 1] = gt_1
+        gtout = gtout.astype(dtype=img.dtype)  # (0~1)
         return gtout
     else:
         labels = np.array(labels)
         if labels.ndim == 1:
-            labels = labels[np.newaxis]       
+            labels = labels[np.newaxis]
             gt_0 = np.zeros(shape=(h, w), dtype=np.float64)
             gt_1 = np.zeros(shape=(h, w), dtype=np.float64)
-            gt_0 = np.maximum(gt_0, make_gaussian((h, w), center=labels[0, :], sigma=sigma))
-           
-        else:   
+            gt_0 = np.maximum(
+                gt_0, make_gaussian((h, w), center=labels[0, :], sigma=sigma)
+            )
+
+        else:
             gt_0 = np.zeros(shape=(h, w), dtype=np.float64)
             gt_1 = np.zeros(shape=(h, w), dtype=np.float64)
-            for ii in range(1,labels.shape[0]):
-                gt_1 = np.maximum(gt_1, make_gaussian((h, w), center=labels[ii, :], sigma=sigma))
-            gt_0 = np.maximum(gt_0, make_gaussian((h, w), center=labels[0, :], sigma=sigma))
-            
+            for ii in range(1, labels.shape[0]):
+                gt_1 = np.maximum(
+                    gt_1, make_gaussian((h, w), center=labels[ii, :], sigma=sigma)
+                )
+            gt_0 = np.maximum(
+                gt_0, make_gaussian((h, w), center=labels[0, :], sigma=sigma)
+            )
+
     gt = np.zeros(shape=(h, w, 2))
-    gt[:, :, 0]=gt_0
-    gt[:, :, 1]=gt_1
-    gt = gt.astype(dtype=img.dtype) #(0~1)
-    return gt  
+    gt[:, :, 0] = gt_0
+    gt[:, :, 1] = gt_1
+    gt = gt.astype(dtype=img.dtype)  # (0~1)
+    return gt
+
 
 def cstm_normalize(im, max_value):
     """
     Normalize image to range 0 - max_value
     """
-    imn = max_value*(im - im.min()) / max((im.max() - im.min()), 1e-8)
+    imn = max_value * (im - im.min()) / max((im.max() - im.min()), 1e-8)
     return imn
 
 
 def generate_param_report(logfile, param):
-    log_file = open(logfile, 'w')
+    log_file = open(logfile, "w")
     for key, val in param.items():
-        log_file.write(key+':'+str(val)+'\n')
+        log_file.write(key + ":" + str(val) + "\n")
     log_file.close()
