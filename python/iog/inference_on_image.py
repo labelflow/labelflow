9b3a57fa8d72c5a8b1b99738d30869265e4d3ba9
Etienne Dupont <etiennedupont>
2021-08-18 14:09:57 +0200

    Avoid magic numbers in main script

--- python/iog/inference_on_image.py
+++ python/iog/inference_on_image.py
@@ -1,25 +1,27 @@
+import os
 from datetime import datetime
-import numpy as np
 import base64
-
-# PyTorch includes
+import numpy as np
+import cv2
 import torch
 from torchvision import transforms
+from shapely.geometry import Polygon
 
-# Custom includes
-from dataloaders import pascal as pascal
-from dataloaders import sbd as sbd
 from dataloaders import custom_transforms as tr
-
-# from dataloaders.helpers import *
 from dataloaders.helpers import tens2image, get_bbox, crop2fullmask
 
 from networks.refinementnetwork import Network
-from shapely.geometry import Polygon
 
-import cv2
-import os
+# from networks.mainnetwork import Network
 from cache import Cache
+
+# Constants for experiments
+PADDING_PIXEL = 10
+RELAX_PIXEL = 30
+SIGMA_IOG_POINT_PIXEL = 10
+SHAPE_IMAGE_MODEL = (512, 512)
+MAX_PIXEL_INTENSITY = 255
+THRESHOLD_PIXEL_INTENSITY = 100
 
 
 def transform_contour_to_geojson_polygon(
@@ -69,12 +71,14 @@ def convert_net_output_to_geojson_polygon(fine_net_output, gt_input, image, roi)
     pred = 1 / (1 + np.exp(-pred))
     pred = np.squeeze(pred)
     gt = tens2image(gt_input)
-    bbox = get_bbox(gt, pad=30, zero_pad=True)  # TODO: try to use bbox = roi directly?
+    bbox = get_bbox(
+        gt, pad=RELAX_PIXEL, zero_pad=True
+    )  # TODO: try to use bbox = roi directly?
     # print(
     #     f"""
     # gt.shape = {gt.shape}
     # bbox = {bbox}
-    # roi = {[roi[0] - 30, roi[1] - 30, roi[0] + roi[2] + 30, roi[1] + roi[3] + 30]}
+    # roi = {[roi[0] - RELAX_PIXEL, roi[1] - RELAX_PIXEL, roi[0] + roi[2] + RELAX_PIXEL, roi[1] + roi[3] + RELAX_PIXEL]}
     # """
     # )
     result = crop2fullmask(
@@ -82,27 +86,29 @@ def convert_net_output_to_geojson_polygon(fine_net_output, gt_input, image, roi)
     )
 
     # find contours
-    im_mask = (result * 255).astype(np.uint8)
-    _, thresh = cv2.threshold(im_mask, 127, 255, 0)
+    im_mask = (result * MAX_PIXEL_INTENSITY).astype(np.uint8)
+    _, thresh = cv2.threshold(
+        im_mask, THRESHOLD_PIXEL_INTENSITY, MAX_PIXEL_INTENSITY, 0
+    )
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (1, 1))
     opening = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=1)
     contours, _ = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     if os.environ.get("DEBUG", False):
         light = np.zeros_like(image)
-        light[:, :, 2] = 255.0
+        light[:, :, 2] = float(MAX_PIXEL_INTENSITY)
         alpha = 0.5
         blending = (alpha * light + (1 - alpha) * image) * result[..., None] + (
             1 - result[..., None]
         ) * image
-        blending[blending > 255.0] = 255
+        blending[blending > float(MAX_PIXEL_INTENSITY)] = MAX_PIXEL_INTENSITY
         # im_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        # cv2.drawContours(im_rgb, contours, -1, (0, 255, 0), 3)
+        # cv2.drawContours(im_rgb, contours, -1, (0, MAX_PIXEL_INTENSITY, 0), 3)
         now = datetime.now()
         os.makedirs("results", exist_ok=True)
         cv2.imwrite(f"results/result-{now}.jpg", blending)
         cv2.imwrite(f"results/mask-{now}.jpg", im_mask)
-        cv2.imwrite(f"results/pred-{now}.jpg", pred * 255)
+        cv2.imwrite(f"results/pred-{now}.jpg", pred * MAX_PIXEL_INTENSITY)
         # print(transform_contours_to_geojson_polygons(contours))
 
     return transform_contours_to_geojson_polygons(
@@ -118,13 +124,14 @@ net = Network(
     output_stride=16,
     sync_bn=None,
     freeze_bn=False,
+    pretrained=False,
 )
 
 # load pretrain_dict
 pretrain_dict = torch.load("data/IOG_PASCAL_SBD_REFINEMENT.pth")
-
 net.load_state_dict(pretrain_dict)
 net.eval()
+
 # Set gpu_id to -1 to run in CPU mode, otherwise set the id of the corresponding gpu
 gpu_id = 0
 device = torch.device(
@@ -138,20 +145,20 @@ if torch.cuda.is_available():
     else:
         print("GPU available, will use it")
 else:
-    print("GPU unavailable")
+    print("GPU unavailable, will use CPU")
 net.to(device)
 
 trns = transforms.Compose(
     [
         tr.CropFromMask(
-            crop_elems=("image", "gt", "void_pixels"), relax=30, zero_pad=True
+            crop_elems=("image", "gt", "void_pixels"), relax=RELAX_PIXEL, zero_pad=True
         ),
         tr.FixedResize(
             resolutions={
                 "gt": None,
-                "crop_image": (512, 512),
-                "crop_gt": (512, 512),
-                "crop_void_pixels": (512, 512),
+                "crop_image": SHAPE_IMAGE_MODEL,
+                "crop_gt": SHAPE_IMAGE_MODEL,
+                "crop_void_pixels": SHAPE_IMAGE_MODEL,
             },
             flagvals={
                 "gt": cv2.INTER_LINEAR,
@@ -160,7 +167,9 @@ trns = transforms.Compose(
                 "crop_void_pixels": cv2.INTER_LINEAR,
             },
         ),
-        tr.IOGPoints(sigma=10, elem="crop_gt", pad_pixel=10),
+        tr.IOGPoints(
+            sigma=SIGMA_IOG_POINT_PIXEL, elem="crop_gt", pad_pixel=PADDING_PIXEL
+        ),
         tr.ToImage(norm_elem="IOG_points"),
         tr.ConcatInputs(elems=("crop_image", "IOG_points")),
         tr.ToTensor(),
@@ -170,17 +179,21 @@ trns = transforms.Compose(
 
 trns_refinement = transforms.Compose(
     [
-        tr.CropFromMask(crop_elems=("point_refinement_mask",), relax=30, zero_pad=True),
+        tr.CropFromMask(
+            crop_elems=("point_refinement_mask",), relax=RELAX_PIXEL, zero_pad=True
+        ),
         tr.FixedResize(
             resolutions={
-                "crop_point_refinement_mask": (512, 512),
+                "crop_point_refinement_mask": SHAPE_IMAGE_MODEL,
             },
             flagvals={
                 "crop_point_refinement_mask": cv2.INTER_LINEAR,
             },
         ),
         tr.IOGPointRefinement(
-            sigma=10, elem="crop_point_refinement_mask", pad_pixel=10
+            sigma=SIGMA_IOG_POINT_PIXEL,
+            elem="crop_point_refinement_mask",
+            pad_pixel=PADDING_PIXEL,
         ),
         tr.ToImage(norm_elem="IOG_points"),
         tr.ToTensor(),
@@ -223,8 +236,6 @@ def inference(image, roi, center_point):
     IOG_points[0, 0:1, :, :] = trns_refinement(sample)["IOG_points"].unsqueeze(0)[
         0, 0:1, :, :
     ]
-    # inputs = inputs.to(device)
-    # res = net.inference(inputs, IOG_points)
     res = net.inference(inputs.to(device), IOG_points.to(device))
 
     backbone_features = res[0]
@@ -302,7 +313,6 @@ def refine(
             np.transpose((points_bg * 1).numpy().astype(np.uint8), (1, 2, 0)),
         )
 
-    # fine_net_output = net.refine(backbone_features, IOG_points)
     fine_net_output = net.refine(backbone_features, IOG_points.to(device))
     return convert_net_output_to_geojson_polygon(
         fine_net_output.to("cpu"), tr_sample["gt"], image, roi
