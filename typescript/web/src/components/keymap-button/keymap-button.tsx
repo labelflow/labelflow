5190a96286e1c7b59d4c56b52755d0ce8c446a77
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2021-08-18 12:48:06 +0100

    Help menu

--- typescript/web/src/components/keymap-button/keymap-button.tsx
+++ typescript/web/src/components/keymap-button/keymap-button.tsx
@@ -2,34 +2,23 @@ import { IconButton, Tooltip, ButtonProps, chakra } from "@chakra-ui/react";
 import { FaRegKeyboard } from "react-icons/fa";
 import { useQueryParam } from "use-query-params";
 import { BoolParam } from "../../utils/query-param-bool";
-import { KeymapModal } from "./keymap-modal";
 
 const KeymapIcon = chakra(FaRegKeyboard);
 
-type Props = ButtonProps & {
-  showModal?: boolean;
-};
+type Props = ButtonProps & {};
 
-export const KeymapButton = ({ showModal = true, ...props }: Props) => {
-  const [isOpen, setIsOpen] = useQueryParam("modal-keymap", BoolParam);
+export const KeymapButton = ({ ...props }: Props) => {
+  const [, setIsOpen] = useQueryParam("modal-keymap", BoolParam);
 
   return (
-    <>
-      {showModal && (
-        <KeymapModal
-          isOpen={isOpen}
-          onClose={() => setIsOpen(false, "replaceIn")}
-        />
-      )}
-      <Tooltip label="Keyboard shortcuts">
-        <IconButton
-          aria-label="Add images"
-          icon={<KeymapIcon fontSize="xl" />}
-          onClick={() => setIsOpen(true, "replaceIn")}
-          variant="ghost"
-          {...props}
-        />
-      </Tooltip>
-    </>
+    <Tooltip label="Keyboard shortcuts">
+      <IconButton
+        aria-label="Keyboard shortcuts"
+        icon={<KeymapIcon fontSize="xl" />}
+        onClick={() => setIsOpen(true, "replaceIn")}
+        variant="ghost"
+        {...props}
+      />
+    </Tooltip>
   );
 };
