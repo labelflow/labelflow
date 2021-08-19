b8ec1d5350618fac9294ff634f064639b4bf2461
Etienne Dupont <etienne.dupont@labelflow.ai>
2021-08-19 18:15:20 +0200

    WIP: many unsuccessfull attemps

--- typescript/common-resolvers/src/utils/get-bounded-geometry-from-image.ts
+++ typescript/common-resolvers/src/utils/get-bounded-geometry-from-image.ts
@@ -1,8 +1,26 @@
 import bboxPolygon from "@turf/bbox-polygon";
-import { polygon } from "@turf/helpers";
+import { MultiPolygon, Polygon, polygon } from "@turf/helpers";
 import intersect from "@turf/intersect";
+import unkinkPolygon from "@turf/unkink-polygon";
 import bbox from "@turf/bbox";
+import union from "@turf/union";
+import flatten from "@turf/flatten";
+import buffer from "@turf/buffer";
+// import geojsonValidation from "geojson-validation";
 import { GeometryInput } from "@labelflow/graphql-types";
+
+const transformMultiPolygonToPolygon = (
+  multiPolygon: MultiPolygon
+): Polygon => ({
+  type: "Polygon",
+  coordinates: multiPolygon.coordinates.reduce(
+    (polygonAggregatedCoordinates, polygonCoordinates) => [
+      ...polygonAggregatedCoordinates,
+      ...polygonCoordinates,
+    ],
+    []
+  ),
+});
 
 export const getBoundedGeometryFromImage = (
   imageDimensions: { width: number; height: number },
@@ -15,18 +33,61 @@ export const getBoundedGeometryFromImage = (
     imageDimensions.width,
     imageDimensions.height,
   ]);
+  // const bufferedGeometry = buffer(geometryPolygon, 1, { units: "degrees" });
+  // console.log(`
+  // geometryPolygon.geometry = ${JSON.stringify(
+  //   geometryPolygon.geometry,
+  //   null,
+  //   1
+  // )}
+  // bufferedGeometry.geometry = ${JSON.stringify(
+  //   bufferedGeometry.geometry,
+  //   null,
+  //   1
+  // )}
+  // `);
   const clippedGeometryObject = intersect(imagePolygon, geometryPolygon);
+  // const clippedGeometryObject = unkinkPolygon(
+  //   intersect(imagePolygon, geometryPolygon)
+  // ).features.reduce((polygonA, polygonB) => union(polygonA, polygonB) as any);
+  // if (!geojsonValidation.isMultiPolygon(clippedGeometryObject)) {
+  //   console.warn(
+  //     "Couldn't intersect polygon with image canvas on first try, will try to validate the polygon."
+  //   );
+  //   clippedGeometryObject = unkinkPolygon(clippedGeometryObject);
+  //   // Try to buffer the polygon with 1px
+  //   // intersect(
+  //   //   imagePolygon,
+  //   //   buffer(geometryPolygon, 1, { units: "degrees" })
+  //   // );
+  // }
 
   if (clippedGeometryObject?.geometry == null) {
     throw new Error("Label out of image bounds");
   }
 
-  const [minX, minY, maxX, maxY] = bbox(clippedGeometryObject.geometry);
+  const clippedPolygon = unkinkPolygon(clippedGeometryObject).features.reduce(
+    (aggregatedPolygon, polygonCurrent) => ({
+      ...aggregatedPolygon,
+      coordinates: [
+        ...aggregatedPolygon.coordinates,
+        ...polygonCurrent.geometry.coordinates,
+      ],
+    }),
+    { type: "Polygon", coordinates: [] }
+  );
+
+  // const clippedPolygon =
+  //   clippedGeometryObject.geometry.type === "Polygon"
+  //     ? clippedGeometryObject.geometry
+  // : transformMultiPolygonToPolygon(clippedGeometryObject.geometry);
+  const [minX, minY, maxX, maxY] = bbox(clippedPolygon);
   const width = maxX - minX;
   const height = maxY - minY;
 
   return {
-    geometry: clippedGeometryObject.geometry,
+    geometry: clippedPolygon,
+    // geometry: geometryPolygon.geometry,
     x: minX,
     y: minY,
     width,
