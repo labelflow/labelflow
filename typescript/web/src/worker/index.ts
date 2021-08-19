9456245acab61d5fef788e9f67d857150db15a91
Etienne Dupont <etienne.dupont@labelflow.ai>
2021-08-19 10:59:49 +0200

    Make a dummy change

--- typescript/web/src/worker/index.ts
+++ typescript/web/src/worker/index.ts
@@ -29,7 +29,7 @@ import { UploadServer } from "./upload-server";
 import { repository } from "../connectors/repository";
 
 declare let self: ServiceWorkerGlobalScope;
-
+// Dummy comment
 // To disable all workbox logging during development, you can set self.__WB_DISABLE_DEV_LOGS to true
 // https://developers.google.com/web/tools/workbox/guides/configure-workbox#disable_logging
 // eslint-disable-next-line no-underscore-dangle
