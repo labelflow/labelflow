e75acd64b0e2e0e39582489b407eb33bb902ab72
Hugo Raynal <raynalhugo@gmail.com>
2021-08-26 12:11:13 +0200

    Use date-fns to compute dates

--- typescript/release/src/update-license.ts
+++ typescript/release/src/update-license.ts
@@ -1,21 +1,14 @@
 import { writeFile, readFile } from "fs/promises";
 import { join } from "path";
-
-const padToTwo = (number: number) => String(number).padStart(2, "0");
+import { add, format } from "date-fns";
 
 const main = async () => {
-  const now = new Date();
-  now.setFullYear(now.getFullYear() + 2);
-
-  let formattedDate;
-  if ((now.getMonth() === 2, now.getDate() === 29)) {
-    // We take into account that the 29th of february doesn't exist in 2 years
-    formattedDate = `${now.getFullYear()}-03-01`;
-  } else {
-    formattedDate = `${now.getFullYear()}-${padToTwo(
-      now.getMonth()
-    )}-${padToTwo(now.getDate())}`;
-  }
+  const newDate = format(
+    add(new Date(), {
+      years: 2,
+    }),
+    "yyyy-MM-dd"
+  );
 
   const previousLicense = await (
     await readFile(join(__dirname, "../../../LICENSE"))
@@ -23,7 +16,7 @@ const main = async () => {
 
   const newLicense = previousLicense.replace(
     /Change Date:(.*?)\n/,
-    `Change Date:          ${formattedDate}\n`
+    `Change Date:          ${newDate}\n`
   );
 
   await writeFile(join(__dirname, "../../../LICENSE"), newLicense);
