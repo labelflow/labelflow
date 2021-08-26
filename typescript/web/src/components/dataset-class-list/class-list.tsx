5eafc58f079f946ff5c7803ef221cab5ad509fd3
Etienne Dupont <etienne.dupont@labelflow.ai>
2021-08-26 18:13:45 +0200

    Add more typing

--- typescript/web/src/components/dataset-class-list/class-list.tsx
+++ typescript/web/src/components/dataset-class-list/class-list.tsx
@@ -17,7 +17,7 @@ const reorderLabelClasseMutation = gql`
     }
   }
 `;
-const reorder = (list, startIndex, endIndex) => {
+const reorder = (list: any[], startIndex: number, endIndex: number) => {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
   result.splice(endIndex, 0, removed);
