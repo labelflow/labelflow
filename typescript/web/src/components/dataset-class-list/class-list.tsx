747b68de2150cf787e0e181f8bd38efeec44fdef
Etienne Dupont <etienne.dupont@labelflow.ai>
2021-08-26 15:21:16 +0200

    Implement drag and drop logic with beautiful react dnd

--- typescript/web/src/components/dataset-class-list/class-list.tsx
+++ typescript/web/src/components/dataset-class-list/class-list.tsx
@@ -1,6 +1,8 @@
 import { useMemo, useState } from "react";
-import { useQuery } from "@apollo/client";
+import { useQuery, useMutation, gql, useApolloClient } from "@apollo/client";
 import { Box, Text, Divider } from "@chakra-ui/react";
+
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import {
   ClassItem,
   datasetLabelClassesQuery,
@@ -8,18 +10,36 @@ import {
 } from "./class-item";
 import { DeleteLabelClassModal } from "./delete-class-modal";
 
+const reorderLabelClasseMutation = gql`
+  mutation reorderLabelClass($id: ID!, $index: Int!) {
+    reorderLabelClass(where: { id: $id }, data: { index: $index }) {
+      id
+    }
+  }
+`;
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
 export const ClassesList = ({ datasetId }: { datasetId: string }) => {
+  const client = useApolloClient();
   const [editClassId, setEditClassId] = useState<string | null>(null);
   const [deleteClassId, setDeleteClassId] = useState<string | null>(null);
 
-  const { data: datasetResult, loading } = useQuery<DatasetClassesQueryResult>(
-    datasetLabelClassesQuery,
-    {
-      variables: {
-        datasetId,
-      },
-    }
-  );
+  const {
+    data: datasetResult,
+    loading,
+    refetch,
+    updateQuery,
+  } = useQuery<DatasetClassesQueryResult>(datasetLabelClassesQuery, {
+    variables: {
+      datasetId,
+    },
+  });
   const labelClasses = datasetResult?.dataset.labelClasses ?? [];
 
   const labelClassWithShortcut = useMemo(
@@ -30,6 +50,31 @@ export const ClassesList = ({ datasetId }: { datasetId: string }) => {
       })),
     [labelClasses]
   );
+  const onDragEnd = async (result) => {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+    updateQuery((prev) => {
+      const labelClassesPrevious = prev?.dataset?.labelClasses;
+      return {
+        ...prev,
+        dataset: {
+          ...prev?.dataset,
+          labelClasses: reorder(
+            labelClassesPrevious,
+            result.source.index,
+            result.destination.index
+          ),
+        },
+      };
+    });
+    await client.mutate({
+      mutation: reorderLabelClasseMutation,
+      variables: { id: result.draggableId, index: result.destination.index },
+    });
+    refetch();
+  };
 
   return (
     <>
@@ -54,20 +99,37 @@ export const ClassesList = ({ datasetId }: { datasetId: string }) => {
             fontWeight="bold"
           >{`${labelClassWithShortcut.length} Classes`}</Text>
           <Divider />
-          {!loading &&
-            labelClassWithShortcut.map(({ id, name, color, shortcut }) => (
-              <ClassItem
-                key={id}
-                id={id}
-                name={name}
-                color={color}
-                shortcut={shortcut}
-                edit={editClassId === id}
-                datasetId={datasetId}
-                onClickEdit={setEditClassId}
-                onClickDelete={setDeleteClassId}
-              />
-            ))}
+          {!loading && (
+            <DragDropContext onDragEnd={onDragEnd}>
+              <Droppable droppableId="droppable">
+                {(provided) => (
+                  <Box
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    // style={getListStyle(snapshot.isDraggingOver)}
+                  >
+                    {labelClassWithShortcut.map(
+                      ({ id, name, color, shortcut, index }) => (
+                        <ClassItem
+                          key={id}
+                          id={id}
+                          index={index}
+                          name={name}
+                          color={color}
+                          shortcut={shortcut}
+                          edit={editClassId === id}
+                          datasetId={datasetId}
+                          onClickEdit={setEditClassId}
+                          onClickDelete={setDeleteClassId}
+                        />
+                      )
+                    )}
+                    {provided.placeholder}
+                  </Box>
+                )}
+              </Droppable>
+            </DragDropContext>
+          )}
         </>
       </Box>
     </>
