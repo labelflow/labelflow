53471b60c1e14465a5f2c4a85c67ceac5245c89c
Vincent Lecrubier <vincent.lecrubier@sterblue.com>
2021-08-27 15:17:30 +0100

    Add all graphics

---
+++ typescript/web/src/components/graphics/browser-link.tsx
@@ -0,0 +1,73 @@
+import * as React from "react";
+
+function SvgBrowserLink(
+  props: React.SVGProps<SVGSVGElement>,
+  svgRef?: React.Ref<SVGSVGElement>
+) {
+  return (
+    <svg
+      id="browser-link_svg__Layer_1"
+      data-name="Layer 1"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 96 96"
+      ref={svgRef}
+      {...props}
+    >
+      <defs>
+        <style>
+          {
+            ".browser-link_svg__cls-4{fill:#f6b756}.browser-link_svg__cls-9{fill:#c3e1f5}.browser-link_svg__cls-10{fill:#39426a}"
+          }
+        </style>
+      </defs>
+      <path fill="#ccd1dc" d="M85 88h10V8H1v80h84z" />
+      <path d="M94 79V8H4v59a18 18 0 0018 18h66a6 6 0 006-6z" fill="#dee1e7" />
+      <path
+        d="M11 24h76a3 3 0 013 3v51a3 3 0 01-3 3H62A54 54 0 018 27a3 3 0 013-3z"
+        fill="#f3f4f5"
+      />
+      <path
+        className="browser-link_svg__cls-4"
+        d="M60.12 44a2.1 2.1 0 01-.62 1.5l-7.6 7.6-4.8 4.8L44 61l-4.5 4.5a2.12 2.12 0 01-3-3L41 58l3.1-3.1 4.8-4.8 7.6-7.6a2.12 2.12 0 013.62 1.5z"
+      />
+      <path
+        className="browser-link_svg__cls-4"
+        d="M47.1 57.9l-3-3a10.61 10.61 0 00-13.35 1.35l-8 8a10.61 10.61 0 0015 15l8-8A10.64 10.64 0 0047.1 57.9zm-4.35 10.35l-8 8a6.36 6.36 0 11-9-9l8-8A6.34 6.34 0 0141 58l-4.5 4.5a2.12 2.12 0 003 3L44 61a6.25 6.25 0 01.63 2.74 6.35 6.35 0 01-1.88 4.51z"
+      />
+      <path
+        className="browser-link_svg__cls-4"
+        d="M44 61l-4.5 4.5a2.12 2.12 0 01-3-3L41 58zM73.25 28.75a10.61 10.61 0 00-15 0l-8 8A10.64 10.64 0 0048.9 50.1l3 3a10.61 10.61 0 0013.35-1.35l8-8a10.62 10.62 0 000-15zm-3 12l-8 8A6.34 6.34 0 0155 50l4.5-4.5a2.12 2.12 0 00-3-3L52 47a6.25 6.25 0 01-.63-2.74 6.35 6.35 0 011.87-4.5l8-8a6.36 6.36 0 119 9z"
+      />
+      <path
+        className="browser-link_svg__cls-4"
+        d="M60.12 44a2.1 2.1 0 01-.62 1.5L55 50l-3-3 4.5-4.5a2.12 2.12 0 013.62 1.5z"
+      />
+      <path fill="#90d1d7" d="M1 8h94v12H1z" />
+      <path d="M4 8h91v9H13a9 9 0 01-9-9z" fill="#abd9e6" />
+      <circle cx={9} cy={14} r={2} fill="#cf4055" />
+      <circle className="browser-link_svg__cls-4" cx={19} cy={14} r={2} />
+      <circle cx={29} cy={14} r={2} fill="#85bd79" />
+      <rect
+        className="browser-link_svg__cls-9"
+        x={45}
+        y={11}
+        width={46}
+        height={4}
+        rx={2}
+      />
+      <circle className="browser-link_svg__cls-9" cx={40} cy={13} r={2} />
+      <path
+        className="browser-link_svg__cls-10"
+        d="M95 7H1a1 1 0 00-1 1v80a1 1 0 001 1h94a1 1 0 001-1V8a1 1 0 00-1-1zM2 9h92v10H2zm0 78V21h92v66z"
+      />
+      <path
+        className="browser-link_svg__cls-10"
+        d="M9 11a3 3 0 103 3 3 3 0 00-3-3zm0 4a1 1 0 111-1 1 1 0 01-1 1zM19 11a3 3 0 103 3 3 3 0 00-3-3zm0 4a1 1 0 111-1 1 1 0 01-1 1zM29 11a3 3 0 103 3 3 3 0 00-3-3zm0 4a1 1 0 111-1 1 1 0 01-1 1zM65.75 24.65A11.53 11.53 0 0057.54 28l-8 8a11.64 11.64 0 00-1.9 14l-3.7 3.69A11.64 11.64 0 0030 55.54l-8 8A11.61 11.61 0 0038.46 80l8-8a11.64 11.64 0 001.9-13.91l3.69-3.69a11.54 11.54 0 005.68 1.49A11.65 11.65 0 0066 52.46l8-8a11.6 11.6 0 00-8.21-19.81zm-13.37 19.6A5.3 5.3 0 0154 40.46l8-8a5.37 5.37 0 017.58 0 5.34 5.34 0 010 7.58l-8 8a5.32 5.32 0 01-4.66 1.49l3.33-3.32a3.14 3.14 0 000-4.42 3.2 3.2 0 00-4.42 0l-3.33 3.33a5 5 0 01-.12-.87zm4.83-1a1.12 1.12 0 011.58 1.58l-20 20a1.13 1.13 0 01-1.58 0 1.11 1.11 0 010-1.58zm-13.59 20.5A5.3 5.3 0 0142 67.54l-8 8a5.37 5.37 0 01-7.58 0 5.34 5.34 0 010-7.58l8-8a5.32 5.32 0 014.66-1.49l-3.33 3.32a3.14 3.14 0 000 4.42 3.14 3.14 0 004.42 0l3.33-3.33a5 5 0 01.12.87zm4.23 0A9.54 9.54 0 0145 70.54l-8 8A9.6 9.6 0 0123.46 65l8-8a9.63 9.63 0 0111-1.84l-1.72 1.72a7.36 7.36 0 00-7.7 1.7l-8 8A7.37 7.37 0 0035.46 77l8-8a7.32 7.32 0 001.71-7.72l1.7-1.7a9.5 9.5 0 01.98 4.17zM72.54 43l-8 8a9.63 9.63 0 01-11 1.84l1.71-1.71A7.37 7.37 0 0063 49.46l8-8A7.37 7.37 0 1060.54 31l-8 8a7.32 7.32 0 00-1.71 7.72l-1.7 1.7a9.58 9.58 0 011.83-11l8-8A9.6 9.6 0 0172.54 43z"
+      />
+    </svg>
+  );
+}
+
+const ForwardRef = React.forwardRef(SvgBrowserLink);
+const MemoForwardRef = React.memo(ForwardRef);
+export default MemoForwardRef;
