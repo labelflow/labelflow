3ded480e59e86b1a0b3b9aee5587ea842042e79c
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2021-09-06 23:44:20 +0100

    Revert "Fix type"
    This reverts commit dc811e69521f9da3f8bf43d0f51f94df3dd5c0c4.

--- typescript/web/src/components/labelling-tool/openlayers-map/draw-bounding-box-and-polygon-interaction/index.tsx
+++ typescript/web/src/components/labelling-tool/openlayers-map/draw-bounding-box-and-polygon-interaction/index.tsx
@@ -2,7 +2,7 @@ import { useMemo, useRef } from "react";
 import { useRouter } from "next/router";
 import { Draw as OlDraw } from "ol/interaction";
 import { createBox, DrawEvent } from "ol/interaction/Draw";
-import GeoJSON, { GeoJSONPolygon } from "ol/format/GeoJSON";
+import GeoJSON from "ol/format/GeoJSON";
 import { Fill, Stroke, Style } from "ol/style";
 import GeometryType from "ol/geom/GeometryType";
 import { useApolloClient, useQuery, gql } from "@apollo/client";
@@ -122,7 +122,7 @@ export const DrawBoundingBoxAndPolygonInteraction = () => {
       onDrawend={async (drawEvent: DrawEvent) => {
         const geometry = new GeoJSON().writeGeometryObject(
           drawEvent.feature.getGeometry()
-        ) as GeoJSONPolygon;
+        ) as GeoJSON.Polygon;
         const createLabelPromise = perform(
           createLabelEffect(
             {
