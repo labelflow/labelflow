dc2a663960ffda6f4a5d1854c1e4d107a269002f
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2021-09-07 17:56:22 +0100

    Add clarity tracking code

--- typescript/web/src/pages/_document.tsx
+++ typescript/web/src/pages/_document.tsx
@@ -84,6 +84,21 @@ class MyDocument extends Document {
           `,
             }}
           />
+          {/* Microsoft Clarity, See https://docs.microsoft.com/en-us/clarity/clarity-setup */}
+          <script
+            type="text/javascript"
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "8cfef08lrk");
+          `,
+            }}
+          />
+          __
         </Head>
         <body>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
