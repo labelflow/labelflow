11047f7eb731a324ee3facdcb232b1afe3aaa728
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-09-08 17:10:05 +0100

    Add environments setup in Sentry

--- typescript/web/src/worker/index.ts
+++ typescript/web/src/worker/index.ts
@@ -33,6 +33,7 @@ import { repository } from "../connectors/repository";
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 Sentry.init({
   dsn: SENTRY_DSN,
+  environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT ?? "development",
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
