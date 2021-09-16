a355daa5e9c31cef477224a6057a4dfced5b6999
Etienne Dupont <etienne.dupont@labelflow.ai>
2021-09-16 14:26:19 +0200

    Fix bug in image naming when avoiding collisions

--- typescript/common-resolvers/src/export/format-yolo/export-to-yolo.ts
+++ typescript/common-resolvers/src/export/format-yolo/export-to-yolo.ts
@@ -77,12 +77,16 @@ export const exportToYolo: ExportFunction = async (
   );
   await Promise.all(
     images.map(async (image) => {
+      const imageName = getImageName(
+        image,
+        options?.avoidImageNameCollisions ?? false
+      );
       if (options?.exportImages) {
         const blob = new Blob([await repository.upload.get(image.url)], {
           type: image.mimetype,
         });
         zip.file(
-          `${datasetName}/obj_train_data/${image.name}.${mime.extension(
+          `${datasetName}/obj_train_data/${imageName}.${mime.extension(
             image.mimetype
           )}`,
           blob
@@ -90,7 +94,7 @@ export const exportToYolo: ExportFunction = async (
       }
       const labelsOfImage = await repository.label.list({ imageId: image.id });
       zip.file(
-        `${datasetName}/obj_train_data/${image.name}.txt`,
+        `${datasetName}/obj_train_data/${imageName}.txt`,
         generateLabelsOfImageFile(labelsOfImage, image, labelClasses, options)
       );
     })
