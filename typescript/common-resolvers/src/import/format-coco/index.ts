be55d99a66b0114c8a7c753f8bc6fb2f443b5b33
Etienne Dupont <etienne.dupont@labelflow.ai>
2021-09-21 16:28:57 +0200

    Labels are correctly added

--- typescript/common-resolvers/src/import/format-coco/index.ts
+++ typescript/common-resolvers/src/import/format-coco/index.ts
@@ -2,10 +2,18 @@ import JSZip from "jszip";
 import path from "path";
 import { v4 as uuidv4 } from "uuid";
 
-import type { UploadTargetHttp } from "@labelflow/graphql-types";
+import type {
+  UploadTargetHttp,
+  GeometryInput,
+  LabelType,
+} from "@labelflow/graphql-types";
+import { LabelType as LabelTypeOptions } from "@labelflow/graphql-types";
 
+import { range } from "lodash/fp";
 import { ImportFunction } from "../types";
 import imageResolvers from "../../image";
+import labelClassResolvers from "../../label-class";
+import labelResolvers from "../../label";
 import { CocoDataset } from "../../export/format-coco/coco-core/types";
 
 const uploadImage = async (file, name, datasetId, { repository }) => {
@@ -18,7 +26,7 @@ const uploadImage = async (file, name, datasetId, { repository }) => {
     (uploadTarget as UploadTargetHttp)?.downloadUrl,
     fileBlob
   );
-  await imageResolvers.Mutation.createImage(
+  return await imageResolvers.Mutation.createImage(
     null,
     {
       data: {
@@ -29,8 +37,47 @@ const uploadImage = async (file, name, datasetId, { repository }) => {
     },
     { repository }
   );
-  console.log("uploaded image ", name);
 };
+
+const isCocoSegmentationBox = (cocoSegmentation: number[][]): boolean => {
+  if (cocoSegmentation.length === 0) {
+    throw new Error("received segmentation without any items inside.");
+  }
+  if (cocoSegmentation.length > 1 || cocoSegmentation[0].length !== 10) {
+    return false;
+  }
+  const maybeBox = cocoSegmentation[0].map(Math.floor);
+  const { x: xValues, y: yValues } = maybeBox.reduce(
+    ({ x, y }, value, index) => {
+      if (index % 2 === 0) {
+        x.add(value);
+      } else {
+        y.add(value);
+      }
+      return { x, y };
+    },
+    { x: new Set(), y: new Set() }
+  );
+  return xValues.size === 2 && yValues.size === 2;
+};
+
+const convertCocoSegmentationToLabel = (
+  cocoSegmentation: number[][],
+  imageHeight: number
+): { geometry: GeometryInput; type: LabelType } => ({
+  type: isCocoSegmentationBox(cocoSegmentation)
+    ? LabelTypeOptions.Box
+    : LabelTypeOptions.Polygon,
+  geometry: {
+    type: "Polygon",
+    coordinates: cocoSegmentation.map((cocoPolygon) =>
+      range(0, cocoPolygon.length / 2).map((index) => [
+        cocoPolygon[2 * index],
+        imageHeight - cocoPolygon[2 * index + 1],
+      ])
+    ),
+  },
+});
 
 export const importCoco: ImportFunction = async (
   zipBlob,
@@ -52,7 +99,6 @@ export const importCoco: ImportFunction = async (
   const annotationFile: CocoDataset = JSON.parse(
     await annotationsFilesJSZip[0].async("string", () => {})
   );
-  //   console.log(JSON.stringify(annotationFile, null, 1));
   const imageFiles = zip.filter((relativePath) =>
     path.dirname(relativePath).endsWith("images")
   );
@@ -63,11 +109,85 @@ export const importCoco: ImportFunction = async (
     },
     new Map()
   );
-  const { images } = annotationFile;
+  // Manage coco images => labelflow images
+  const cocoImageIdToLabelFlowImageId = new Map<number, string>();
+  const images = await repository.image.list({ datasetId });
+  const imagesCoco = annotationFile.images.filter((imageCoco) => {
+    const labelFlowImage = images.find(
+      (image) =>
+        image.name ===
+        imageCoco.file_name.replace(path.extname(imageCoco.file_name), "")
+    );
+    if (labelFlowImage) {
+      cocoImageIdToLabelFlowImageId.set(imageCoco.id, labelFlowImage.id);
+      return false;
+    }
+    return true;
+  });
   // eslint-disable-next-line no-restricted-syntax
-  for (const image of images) {
-    const imageFile = imageNameToFile.get(image.file_name);
+  for (const imageCoco of imagesCoco) {
+    const imageFile = imageNameToFile.get(imageCoco.file_name);
     // eslint-disable-next-line no-await-in-loop
-    await uploadImage(imageFile, image.file_name, datasetId, { repository });
+    const {
+      id: labelFlowImageId,
+      // eslint-disable-next-line no-await-in-loop
+    } = await uploadImage(imageFile, imageCoco.file_name, datasetId, {
+      repository,
+    });
+    cocoImageIdToLabelFlowImageId.set(imageCoco.id, labelFlowImageId);
+    console.log(`Created image ${imageCoco.file_name}`);
   }
+  // Manage coco categories => labelflow labelclasses
+  const cocoCategoryIdToLabelFlowLabelClassId = new Map<number, string>();
+  const labelClasses = await repository.labelClass.list({ datasetId });
+  const categoriesCoco = annotationFile.categories.filter((categoryCoco) => {
+    const labelFlowLabelClass = labelClasses.find(
+      (labelClass) => labelClass.name === categoryCoco.name
+    );
+    if (labelFlowLabelClass) {
+      cocoCategoryIdToLabelFlowLabelClassId.set(
+        categoryCoco.id,
+        labelFlowLabelClass.id
+      );
+      return false;
+    }
+    return true;
+  });
+  // eslint-disable-next-line no-restricted-syntax
+  for (const categoryCoco of categoriesCoco) {
+    const { id: labelFlowLabelClassId } =
+      // eslint-disable-next-line no-await-in-loop
+      await labelClassResolvers.Mutation.createLabelClass(
+        null,
+        {
+          data: { name: categoryCoco.name, datasetId, color: 0xbbbbbb }, // TODO: manage labelClass colors
+        },
+        { repository }
+      );
+    cocoCategoryIdToLabelFlowLabelClassId.set(
+      categoryCoco.id,
+      labelFlowLabelClassId
+    );
+    console.log(`Created category ${categoryCoco.name}`);
+  }
+  // Manage coco annotations => labelflow labels
+  await annotationFile.annotations.map(async (annotation) => {
+    await labelResolvers.Mutation.createLabel(
+      null,
+      {
+        data: {
+          imageId: cocoImageIdToLabelFlowImageId.get(annotation.image_id),
+          labelClassId: cocoCategoryIdToLabelFlowLabelClassId.get(
+            annotation.category_id
+          ),
+          ...convertCocoSegmentationToLabel(
+            annotation.segmentation,
+            annotationFile.images[annotation.image_id - 1].height
+          ),
+        },
+      },
+      { repository }
+    );
+    console.log(`Created annotation ${annotation.id}`);
+  });
 };
