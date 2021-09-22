097d4626e4601512b79c9b2ba4e4f21e177a46d6
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-09-22 12:07:49 +0200

    Only execute db resolver tests

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,8 @@ export default {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   collectCoverage: true,
-  testMatch: ["<rootDir>/typescript/**/__tests__/**/*.{ts,tsx}"],
+  // testMatch: ["<rootDir>/typescript/**/__tests__/**/*.{ts,tsx}"], //TODO: set back the testMatch to this
+  testMatch: ["<rootDir>/typescript/db/**/__tests__/**/*.{ts,tsx}"], // TODO: remove this line
   testPathIgnorePatterns: ["node_modules"],
   setupFilesAfterEnv: ["<rootDir>/setup-tests.ts"],
   globals: {
