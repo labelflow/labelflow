a6e68872966956954b01ca20334b9a98f0fef337
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2021-09-22 10:36:13 +0100

    Add fullscreen mode

--- typescript/web/src/@types/window.d.ts
+++ typescript/web/src/@types/window.d.ts
@@ -5,4 +5,19 @@ declare global {
     clarity?: (event: string) => void;
     workbox: Workbox;
   }
+
+  interface Document {
+    msFullscreenEnabled?: boolean;
+    fullscreenEnabled?: boolean;
+    webkitIsFullScreen?: boolean;
+    msFullscreenElement?: HTMLElement;
+    exitFullscreen?: () => void;
+    msExitFullscreen?: () => void;
+    webkitExitFullscreen?: () => void;
+  }
+  interface HTMLElement {
+    webkitRequestFullscreen?: () => void;
+    msRequestFullscreen?: () => void;
+    requestFullscreen?: () => void;
+  }
 }
