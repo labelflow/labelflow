15f545fa8b66342e74ad211cf07a6a1e15d93afd
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-09-28 15:03:05 +0200

    Add access control to user + update tests

--- typescript/db/src/resolvers/user.ts
+++ typescript/db/src/resolvers/user.ts
@@ -5,19 +5,30 @@ import {
   QueryUserArgs,
   QueryUsersArgs,
 } from "@labelflow/graphql-types";
-import { DbUser } from "@labelflow/common-resolvers";
+import { Context, DbUser } from "@labelflow/common-resolvers";
 
 import { prisma } from "../repository/prisma-client";
+import { checkUserAccessToUser } from "../repository/access-control";
 
-const users = async (_: any, args: QueryUsersArgs) => {
+const users = async (_: any, args: QueryUsersArgs, { user }: Context) => {
   return await prisma.user.findMany({
     skip: args.skip ?? undefined,
     take: args.first ?? undefined,
     orderBy: { createdAt: Prisma.SortOrder.asc },
+    where: {
+      memberships: {
+        some: { workspace: { memberships: { some: { userId: user?.id } } } },
+      },
+    },
   });
 };
 
-const user = async (_: any, args: QueryUserArgs) => {
+const user = async (
+  _: any,
+  args: QueryUserArgs,
+  { user: userData }: Context
+) => {
+  await checkUserAccessToUser({ where: args.where, user: userData });
   const userFromDb = await prisma.user.findUnique({
     where: args.where,
   });
@@ -29,16 +40,30 @@ const user = async (_: any, args: QueryUserArgs) => {
   return userFromDb;
 };
 
-const updateUser = async (_: any, args: MutationUpdateUserArgs) => {
+const updateUser = async (
+  _: any,
+  args: MutationUpdateUserArgs,
+  { user: userData }: Context
+) => {
+  if (userData?.id !== args.where.id) {
+    throw new Error("User not authorized to access user");
+  }
   return await prisma.user.update({
     where: args.where,
     data: { ...args.data, name: args.data.name ?? undefined },
   });
 };
 
-const memberships = async (parent: DbUser) => {
+const memberships = async (
+  parent: DbUser,
+  _: any,
+  { user: userData }: Context
+) => {
   return await prisma.membership.findMany({
-    where: { userId: parent.id },
+    where: {
+      userId: parent.id,
+      workspace: { memberships: { some: { userId: userData?.id } } },
+    },
     orderBy: { createdAt: Prisma.SortOrder.asc },
   });
 };
