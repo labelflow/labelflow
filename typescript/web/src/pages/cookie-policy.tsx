0529c02c473e3d573e7fb3794bda27dd712439c1
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2021-10-04 13:00:02 +0100

    Whoopsie

--- typescript/web/src/pages/cookie-policy.tsx
+++
@@ -1,5 +0,0 @@
-import LegalPage from "../components/legal-page";
-
-export default function CookiePolicy() {
-  return <LegalPage pageName="cookie-policy" />;
-}
---
+++ typescript/web/src/pages/request-access.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { Box } from "@chakra-ui/react";
+
+import { NavBar } from "../components/website/Navbar/NavBar";
+import { Footer } from "../components/website/Footer/Footer";
+import { RequestAccess } from "../components/website/RequestAccess/RequestAccess";
+import { Meta } from "../components/meta";
+import { ServiceWorkerManagerBackground } from "../components/service-worker-manager";
+import { CookieBanner } from "../components/cookie-banner";
+
+export default function RequestAccessPage() {
+  return (
+    <>
+      <ServiceWorkerManagerBackground />
+      <Meta title="LabelFlow | Request Access" />
+      <CookieBanner />
+      <Box minH="640px">
+        <NavBar />
+        <RequestAccess />
+        <Footer />
+      </Box>
+    </>
+  );
+}
