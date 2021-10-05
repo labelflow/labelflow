c6ce65d232c8091bc2f51ed8524250c65ffc5f16
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-10-05 12:16:19 +0100

    Refactor images page, pass content to components

---
+++ typescript/web/src/components/dataset-images-list/images-list.tsx
@@ -0,0 +1,149 @@
+import React, { useState } from "react";
+import NextLink from "next/link";
+import {
+  Box,
+  VStack,
+  Flex,
+  useColorModeValue as mode,
+  Image,
+  Center,
+  Spinner,
+  Text,
+  Heading,
+  SimpleGrid,
+  IconButton,
+  chakra,
+} from "@chakra-ui/react";
+import { isEmpty } from "lodash/fp";
+import { HiTrash } from "react-icons/hi";
+import type { Image as ImageType } from "@labelflow/graphql-types";
+import { ImportButton } from "../import-button";
+import { EmptyStateNoImages } from "../empty-state";
+import { DeleteImageModal } from "./delete-image-modal";
+
+const TrashIcon = chakra(HiTrash);
+
+export const ImagesList = ({
+  datasetSlug,
+  workspaceSlug,
+  images,
+}: {
+  datasetSlug: string;
+  workspaceSlug: string;
+  images: ImageType[] | undefined;
+}) => {
+  const [imageIdToDelete, setImageIdToDelete] = useState<string | null>(null);
+
+  const cardBackground = mode("white", "gray.700");
+  const imageBackground = mode("gray.100", "gray.800");
+  return (
+    <>
+      {!images && (
+        <Center h="full">
+          <Spinner size="xl" />
+        </Center>
+      )}
+      {isEmpty(images) && (
+        <Center h="full">
+          <Box as="section">
+            <Box
+              maxW="2xl"
+              mx="auto"
+              px={{ base: "6", lg: "8" }}
+              py={{ base: "16", sm: "20" }}
+              textAlign="center"
+            >
+              <EmptyStateNoImages w="full" />
+              <Heading as="h2">You don&apos;t have any images.</Heading>
+              <Text mt="4" fontSize="lg">
+                Fortunately, it’s very easy to add some.
+              </Text>
+
+              <ImportButton
+                colorScheme="brand"
+                variant="solid"
+                mt="8"
+                showModal={false}
+              />
+            </Box>
+          </Box>
+        </Center>
+      )}
+
+      {!isEmpty(images) && (
+        <>
+          <DeleteImageModal
+            isOpen={imageIdToDelete != null}
+            onClose={() => setImageIdToDelete(null)}
+            imageId={imageIdToDelete}
+          />
+          <SimpleGrid
+            minChildWidth="240px"
+            spacing={{ base: "2", md: "8" }}
+            padding={{ base: "2", md: "8" }}
+          >
+            {images?.map(({ id, name, url }) => (
+              <NextLink
+                href={`/${workspaceSlug}/datasets/${datasetSlug}/images/${id}`}
+                key={id}
+              >
+                <a
+                  href={`/${workspaceSlug}/datasets/${datasetSlug}/images/${id}`}
+                >
+                  <VStack
+                    maxW="486px"
+                    p={4}
+                    background={cardBackground}
+                    rounded={8}
+                    height="270px"
+                    justifyContent="space-between"
+                  >
+                    <Flex
+                      justifyContent="space-between"
+                      w="100%"
+                      alignItems="center"
+                    >
+                      <Heading
+                        as="h3"
+                        size="sm"
+                        overflow="hidden"
+                        textOverflow="ellipsis"
+                        whiteSpace="nowrap"
+                        flexGrow={0}
+                        flexShrink={0}
+                      >
+                        {name}
+                      </Heading>
+                      <IconButton
+                        icon={<TrashIcon />}
+                        aria-label="delete image"
+                        isRound
+                        size="sm"
+                        onClick={(e) => {
+                          e.preventDefault();
+                          e.stopPropagation();
+                          setImageIdToDelete(id);
+                        }}
+                      />
+                    </Flex>
+                    <Image
+                      background={imageBackground}
+                      alt={name}
+                      src={url}
+                      ignoreFallback
+                      objectFit="contain"
+                      h="208px"
+                      w="full"
+                      flexGrow={0}
+                      flexShrink={0}
+                    />
+                  </VStack>
+                </a>
+              </NextLink>
+            ))}
+          </SimpleGrid>
+        </>
+      )}
+    </>
+  );
+};
--- typescript/web/src/pages/[workspaceSlug]/datasets/[datasetSlug]/images/index.tsx
+++ typescript/web/src/pages/[workspaceSlug]/datasets/[datasetSlug]/images/index.tsx
@@ -1,25 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import NextLink from "next/link";
-import {
-  Box,
-  VStack,
-  Flex,
-  useColorModeValue as mode,
-  Image,
-  Center,
-  Skeleton,
-  Spinner,
-  Text,
-  BreadcrumbLink,
-  Heading,
-  SimpleGrid,
-  IconButton,
-  chakra,
-} from "@chakra-ui/react";
+import { Skeleton, Text, BreadcrumbLink } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import { isEmpty } from "lodash/fp";
-import { HiTrash } from "react-icons/hi";
 import { useErrorHandler } from "react-error-boundary";
 import type { Dataset as DatasetType } from "@labelflow/graphql-types";
 import { ServiceWorkerManagerModal } from "../../../../../components/service-worker-manager";
@@ -28,16 +11,13 @@ import { ImportButton } from "../../../../../components/import-button";
 import { ExportButton } from "../../../../../components/export-button";
 import { Meta } from "../../../../../components/meta";
 import { Layout } from "../../../../../components/layout";
-import { EmptyStateNoImages } from "../../../../../components/empty-state";
 import { DatasetTabBar } from "../../../../../components/layout/tab-bar/dataset-tab-bar";
 import { Error404Content } from "../../../../404";
 import { AuthManager } from "../../../../../components/auth-manager";
 
 import { WelcomeManager } from "../../../../../components/welcome-manager";
 import { CookieBanner } from "../../../../../components/cookie-banner";
-import { DeleteImageModal } from "./delete-image-modal";
-
-const TrashIcon = chakra(HiTrash);
+import { ImagesList } from "../../../../../components/dataset-images-list";
 
 export const datasetDataQuery = gql`
   query getDatasetData($slug: String!, $workspaceSlug: String!) {
@@ -83,8 +63,6 @@ const ImagesPage = () => {
 
   const datasetName = datasetResult?.dataset.name;
 
-  const [imageIdToDelete, setImageIdToDelete] = useState<string | null>(null);
-
   const handleError = useErrorHandler();
   if (error && !loading) {
     if (!error.message.match(/Couldn't find dataset corresponding to/)) {
@@ -101,9 +79,6 @@ const ImagesPage = () => {
       </>
     );
   }
-
-  const cardBackground = mode("white", "gray.700");
-  const imageBackground = mode("gray.100", "gray.800");
   return (
     <>
       <ServiceWorkerManagerModal />
@@ -138,112 +113,11 @@ const ImagesPage = () => {
           />
         }
       >
-        {!datasetResult && (
-          <Center h="full">
-            <Spinner size="xl" />
-          </Center>
-        )}
-        {datasetResult && isEmpty(datasetResult?.dataset?.images) && (
-          <Center h="full">
-            <Box as="section">
-              <Box
-                maxW="2xl"
-                mx="auto"
-                px={{ base: "6", lg: "8" }}
-                py={{ base: "16", sm: "20" }}
-                textAlign="center"
-              >
-                <EmptyStateNoImages w="full" />
-                <Heading as="h2">You don&apos;t have any images.</Heading>
-                <Text mt="4" fontSize="lg">
-                  Fortunately, it’s very easy to add some.
-                </Text>
-
-                <ImportButton
-                  colorScheme="brand"
-                  variant="solid"
-                  mt="8"
-                  showModal={false}
-                />
-              </Box>
-            </Box>
-          </Center>
-        )}
-
-        {datasetResult && !isEmpty(datasetResult?.dataset?.images) && (
-          <>
-            <DeleteImageModal
-              isOpen={imageIdToDelete != null}
-              onClose={() => setImageIdToDelete(null)}
-              imageId={imageIdToDelete}
-            />
-            <SimpleGrid
-              minChildWidth="240px"
-              spacing={{ base: "2", md: "8" }}
-              padding={{ base: "2", md: "8" }}
-            >
-              {datasetResult?.dataset?.images?.map(({ id, name, url }) => (
-                <NextLink
-                  href={`/${workspaceSlug}/datasets/${datasetSlug}/images/${id}`}
-                  key={id}
-                >
-                  <a
-                    href={`/${workspaceSlug}/datasets/${datasetSlug}/images/${id}`}
-                  >
-                    <VStack
-                      maxW="486px"
-                      p={4}
-                      background={cardBackground}
-                      rounded={8}
-                      height="270px"
-                      justifyContent="space-between"
-                    >
-                      <Flex
-                        justifyContent="space-between"
-                        w="100%"
-                        alignItems="center"
-                      >
-                        <Heading
-                          as="h3"
-                          size="sm"
-                          overflow="hidden"
-                          textOverflow="ellipsis"
-                          whiteSpace="nowrap"
-                          flexGrow={0}
-                          flexShrink={0}
-                        >
-                          {name}
-                        </Heading>
-                        <IconButton
-                          icon={<TrashIcon />}
-                          aria-label="delete image"
-                          isRound
-                          size="sm"
-                          onClick={(e) => {
-                            e.preventDefault();
-                            e.stopPropagation();
-                            setImageIdToDelete(id);
-                          }}
-                        />
-                      </Flex>
-                      <Image
-                        background={imageBackground}
-                        alt={name}
-                        src={url}
-                        ignoreFallback
-                        objectFit="contain"
-                        h="208px"
-                        w="full"
-                        flexGrow={0}
-                        flexShrink={0}
-                      />
-                    </VStack>
-                  </a>
-                </NextLink>
-              ))}
-            </SimpleGrid>
-          </>
-        )}
+        <ImagesList
+          datasetSlug={datasetSlug}
+          workspaceSlug={workspaceSlug}
+          images={datasetResult?.dataset?.images}
+        />
       </Layout>
     </>
   );
