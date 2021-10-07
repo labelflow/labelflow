62f2136f1e93977f524e72c65eb8ceb55fc81f6b
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2021-10-07 15:36:16 +0100

    Update prisma client init

--- typescript/db/src/repository/prisma-client.ts
+++ typescript/db/src/repository/prisma-client.ts
@@ -1,3 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({
+  datasources: { db: { url: process.env.POSTGRES_EXTERNAL_URL } },
+});
