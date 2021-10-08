f35e31e6d118bc59872abdb1b27defb890781f80
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2021-10-08 12:24:31 +0100

    Fix warning

--- typescript/web/src/components/logo/nav-logo.tsx
+++ typescript/web/src/components/logo/nav-logo.tsx
@@ -8,7 +8,7 @@ export const NavLogo = () => {
   return (
     <NextLink href="/">
       <BreadcrumbLink flexShrink={0} flexGrow={0}>
-        <Box as="a" rel="home" cursor="pointer" mr="0" overflow="visible">
+        <Box rel="home" cursor="pointer" mr="0" overflow="visible">
           <VisuallyHidden>LabelFlow</VisuallyHidden>
           <Logo h="8" iconColor="brand.500" logoOnly />
         </Box>
