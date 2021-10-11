4516657c9a3e3c27877a84a917215916d16c9bcf
Etienne Dupont <etienne.dupont@labelflow.ai>
2021-10-11 15:31:26 +0200

    Copy work in import modal

---
+++ typescript/web/src/components/new-member/new-member-modal/__stories__/new-member-modal.tsx
@@ -0,0 +1,42 @@
+import { Button, useDisclosure } from "@chakra-ui/react";
+
+import { ImportImagesModal } from "../new-member-modal";
+import { chakraDecorator } from "../../../../utils/chakra-decorator";
+import { apolloDecorator } from "../../../../utils/apollo-decorator";
+import { queryParamsDecorator } from "../../../../utils/query-params-decorator";
+
+export default {
+  title: "web/New member modal",
+  parameters: {
+    nextRouter: {
+      path: "/images/[id]",
+      asPath: "/images/mock-image-id",
+      query: {
+        id: "mock-image-id",
+      },
+    },
+  },
+  decorators: [chakraDecorator, apolloDecorator, queryParamsDecorator],
+};
+
+export const Default = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <div>
+      <Button onClick={onOpen}>Display</Button>
+      <ImportImagesModal isOpen={isOpen} onClose={onClose} />
+    </div>
+  );
+};
+
+export const OpenedByDefault = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure({ defaultIsOpen: true });
+
+  return (
+    <div>
+      <Button onClick={onOpen}>Display</Button>
+      <ImportImagesModal isOpen={isOpen} onClose={onClose} />
+    </div>
+  );
+};
