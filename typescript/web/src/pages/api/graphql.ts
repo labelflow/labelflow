ad629e038713a6d872282ba61579e443212b3852
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2021-10-13 20:01:41 +0100

    [cherrypick this to main] Remove need for top level await

--- typescript/web/src/pages/api/graphql.ts
+++ typescript/web/src/pages/api/graphql.ts
@@ -1,26 +1,40 @@
+import { NextApiHandler } from "next";
 import { ApolloServer } from "apollo-server-micro";
 import { schemaWithResolvers, repository } from "@labelflow/db";
 import { getSession } from "next-auth/react";
 import { captureException } from "@sentry/nextjs";
 
-const apolloServer = new ApolloServer({
-  schema: schemaWithResolvers,
-  context: async ({ req }) => {
-    const session = await getSession({ req });
-    return { repository, session, user: session?.user, req };
-  },
-  introspection: true,
-  formatError: (error) => {
-    captureException(error);
-    return error;
-  },
-});
+const createHandler = async () => {
+  const apolloServer = new ApolloServer({
+    schema: schemaWithResolvers,
+    context: async ({ req }) => {
+      const session = await getSession({ req });
+      return { repository, session, user: session?.user, req };
+    },
+    introspection: true,
+    formatError: (error) => {
+      captureException(error);
+      return error;
+    },
+  });
+  await apolloServer.start();
+  return apolloServer.createHandler({ path: "/api/graphql" });
+};
+
+const handlerPromise = createHandler();
+let handler: NextApiHandler | null = null;
 
 export const config = {
   api: {
     bodyParser: false,
   },
 };
-await apolloServer.start();
 
-export default apolloServer.createHandler({ path: "/api/graphql" });
+const handleRequest: NextApiHandler = async (req, res) => {
+  if (handler == null) {
+    handler = await handlerPromise;
+  }
+  return await handler(req, res);
+};
+
+export default handleRequest;
