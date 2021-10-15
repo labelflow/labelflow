0d7548ade64447c3b6f051682c4244a890bda3db
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-10-15 18:19:30 +0100

    Add user page logic

--- typescript/web/src/pages/settings/profile/index.tsx
+++ typescript/web/src/pages/settings/profile/index.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { useCallback } from "react";
+import { useSession } from "next-auth/react";
+import { gql, useMutation, useQuery } from "@apollo/client";
 import { Meta } from "../../../components/meta";
 import { Layout } from "../../../components/layout";
 import { ServiceWorkerManagerModal } from "../../../components/service-worker-manager";
@@ -8,7 +10,47 @@ import { CookieBanner } from "../../../components/cookie-banner";
 import { NavLogo } from "../../../components/logo/nav-logo";
 import { Profile } from "../../../components/profile";
 
+const updateUserQuery = gql`
+  mutation updateUser($id: ID!, $data: UserUpdateInput!) {
+    updateUser(where: { id: $id }, data: $data) {
+      id
+    }
+  }
+`;
+
+const userQuery = gql`
+  query getUserProfileInfo($id: ID!) {
+    user(where: { id: $id }) {
+      id
+      createdAt
+      name
+      email
+      image
+    }
+  }
+`;
+
 const ProfilePage = () => {
+  const session = useSession({ required: false });
+  const userInfoFromSession = session?.data?.user;
+
+  const { data: userData } = useQuery(userQuery, {
+    variables: { id: userInfoFromSession?.id },
+    skip: userInfoFromSession?.id == null,
+  });
+  const user = userData?.user;
+  const [updateUser] = useMutation(updateUserQuery, {
+    refetchQueries: ["getUserProfileInfo"],
+  });
+  const changeUserName = useCallback(
+    (name: string) => {
+      updateUser({ variables: { id: user?.id, data: { name } } });
+    },
+    [updateUser, user]
+  );
+  if (user == null) {
+    return null;
+  }
   return (
     <>
       <ServiceWorkerManagerModal />
@@ -17,7 +59,7 @@ const ProfilePage = () => {
       <Meta title="LabelFlow | Profile" />
       <CookieBanner />
       <Layout breadcrumbs={[<NavLogo key={0} />]}>
-        <Profile />
+        <Profile user={user} changeUserName={changeUserName} />
       </Layout>
     </>
   );
