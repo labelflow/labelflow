3747bf9d7e867a899bf30027e585fe60fb8da6b5
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-10-18 14:42:30 +0100

    Make a common folder for workspace and user settings

--- typescript/web/src/pages/settings/profile/index.tsx
+++ typescript/web/src/pages/settings/profile/index.tsx
@@ -8,7 +8,7 @@ import { AuthManager } from "../../../components/auth-manager";
 import { WelcomeManager } from "../../../components/welcome-manager";
 import { CookieBanner } from "../../../components/cookie-banner";
 import { NavLogo } from "../../../components/logo/nav-logo";
-import { Profile } from "../../../components/profile";
+import { Profile } from "../../../components/settings/user/profile";
 
 const updateUserQuery = gql`
   mutation updateUser($id: ID!, $data: UserUpdateInput!) {
