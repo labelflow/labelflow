4dac2fd3e6d17d9e2eddc00137b56747bb4693a7
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2021-10-20 19:00:38 +0100

    Remove danger zone

--- typescript/web/src/components/settings/workspace/index.tsx
+++ typescript/web/src/components/settings/workspace/index.tsx
@@ -3,13 +3,13 @@ import { Workspace } from "@labelflow/graphql-types";
 
 import { SettingsContainer } from "../container";
 import { Profile } from "./profile";
-import { DangerZone } from "./danger-zone";
+// import { DangerZone } from "./danger-zone";
 
 export const WorkspaceSettings = ({ workspace }: { workspace?: Workspace }) => {
   return (
     <SettingsContainer>
       <Profile workspace={workspace} />
-      <DangerZone />
+      {/* <DangerZone /> */}
     </SettingsContainer>
   );
 };
