dd34a678407a04d256f521501d1c9f87406f718a
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2021-10-20 19:06:52 +0100

    lint

--- typescript/web/src/components/settings/workspace/profile.tsx
+++ typescript/web/src/components/settings/workspace/profile.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import { ApolloError, gql, useMutation, useQuery } from "@apollo/client";
 import {
   HStack,
