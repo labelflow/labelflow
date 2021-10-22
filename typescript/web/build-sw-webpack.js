9e80f7eeb3c60d377664467f9be5203ec3efa2b1
Vincent Lecrubier <vincent.lecrubier@gmail.com>
2021-10-22 16:54:49 +0100

    sw compiles but crashes like it did with next-pwa Problem with apollo new

---
+++ typescript/web/build-sw-webpack.js
@@ -0,0 +1,52 @@
+// DEV Webpack configuration used to build the service worker
+
+const path = require("path");
+
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
+module.exports = {
+  target: "webworker",
+  mode: "development",
+  // WARNING: commented out to disable source maps
+  devtool: 'inline-source-map',
+  entry: {
+    index: path.join(__dirname, "src", "worker", "index.ts"),
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".jsx", ".tsx"],
+    fallback: {
+      crypto: false,
+      module: false,
+      dgram: false,
+      dns: false,
+      fs: false,
+      http2: false,
+      net: false,
+      tls: false,
+      child_process: false
+    }
+  },
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "sw.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+          onlyCompileBundledFiles: true,
+          context: __dirname,
+          configFile: path.join(__dirname, 'tsconfig.worker.json')
+        },
+      },
+    ],
+  },
+  plugins: [
+    new NodePolyfillPlugin({
+      excludeAliases: ["console"],
+    }),
+  ],
+};
