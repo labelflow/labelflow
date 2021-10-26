f2856717045ed830b41619abd8b919735e612531
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-10-26 09:24:21 +0100

    Add workspace creation in online golden path

--- cypress/integration/golden-path-online.ts
+++ cypress/integration/golden-path-online.ts
@@ -1,6 +1,4 @@
-import imageSampleCollection from "../../typescript/web/src/utils/image-sample-collection";
-
-describe("Golden path", () => {
+describe("Golden path for online workspaces", () => {
   it("Should execute the golden path with online workspaces without errors", () => {
     cy.task("performLogin").then((token) => {
       cy.setCookie("next-auth.session-token", token as string);
@@ -13,5 +11,15 @@ describe("Golden path", () => {
 
     cy.get('[aria-label="User and Preferences"]').click();
     cy.contains("Cypress test user").should("be.visible");
+
+    cy.get('[aria-label="Open workspace selection popover"]').click();
+
+    cy.get('[aria-label="Workspace selection menu popover"]')
+      .contains("Create workspace")
+      .click({ force: true }); // TODO: fix this, we should not use {force:true}, but try to understand why Cypress thinks it's not visible
+    cy.contains("Workspace Name").should("be.visible");
+    cy.focused().type("Test workspace");
+    cy.get('[aria-label="Create workspace"]').click();
+    cy.url().should("match", /\/test-workspace\/datasets/);
   });
 });
