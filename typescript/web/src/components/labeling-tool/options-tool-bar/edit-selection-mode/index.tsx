f89d634a6ee975f8ca70a7d640c1b49672199877
Etienne Dupont <etienne.dupont@labelflow.ai>
2021-11-16 16:31:32 +0000

    Disable IOG edition mode on non IOG labels

--- typescript/web/src/components/labeling-tool/options-tool-bar/edit-selection-mode/index.tsx
+++ typescript/web/src/components/labeling-tool/options-tool-bar/edit-selection-mode/index.tsx
@@ -1,9 +1,11 @@
+import { gql, useQuery } from "@apollo/client";
 import { BiShapePolygon } from "react-icons/bi";
 import { IoColorWandOutline } from "react-icons/io5";
 import { chakra, useColorModeValue } from "@chakra-ui/react";
 import { useHotkeys } from "react-hotkeys-hook";
 import * as React from "react";
 
+import { useEffect } from "react";
 import {
   useLabelingStore,
   SelectionToolState,
@@ -15,24 +17,45 @@ import { ToggleButton } from "./ToggleButton";
 const ChakraBiShapePolygon = chakra(BiShapePolygon);
 const ChakraIoColorWandOutline = chakra(IoColorWandOutline);
 
+export const labelQuery = gql`
+  query getLabel($id: ID!) {
+    label(where: { id: $id }) {
+      id
+      smartToolInput
+    }
+  }
+`;
+
 export const EditSelectionMode = () => {
   const selectedLabelId = useLabelingStore((state) => state.selectedLabelId);
+  const { data: dataLabelQuery } = useQuery(labelQuery, {
+    variables: { id: selectedLabelId },
+    skip: selectedLabelId == null,
+  });
+  const isIogModeAvailable = dataLabelQuery?.label?.smartToolInput != null;
   const selectionToolState = useLabelingStore(
     (state) => state.selectionToolState
   );
   const setSelectionToolState = useLabelingStore(
     (state) => state.setSelectionToolState
   );
+  useEffect(() => {
+    if (selectionToolState === SelectionToolState.IOG && !isIogModeAvailable)
+      setSelectionToolState(SelectionToolState.DEFAULT);
+  }, [selectionToolState]);
   useHotkeys(
     keymap.changeSelectionMode.key,
     () => {
-      if (selectionToolState === SelectionToolState.DEFAULT) {
+      if (
+        selectionToolState === SelectionToolState.DEFAULT &&
+        isIogModeAvailable
+      ) {
         setSelectionToolState(SelectionToolState.IOG);
       } else {
         setSelectionToolState(SelectionToolState.DEFAULT);
       }
     },
-    [selectionToolState, setSelectionToolState]
+    [selectionToolState, setSelectionToolState, isIogModeAvailable]
   );
   const bg = useColorModeValue("white", "gray.800");
 
@@ -62,6 +85,7 @@ export const EditSelectionMode = () => {
         bg={bg}
         pointerEvents="initial"
         icon={<ChakraIoColorWandOutline size="1.3em" />}
+        disabled={!isIogModeAvailable}
       />
     </ToggleButtonGroup>
   );
