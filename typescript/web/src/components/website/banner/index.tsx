1f3a7617e6e6a05a8225b83d590aa7e749cdfc84
geoffreyvancassel <geoffrey.vancassel@sterblue.com>
2021-11-16 12:20:15 +0100

    banner change

--- typescript/web/src/components/website/banner/index.tsx
+++ typescript/web/src/components/website/banner/index.tsx
@@ -35,30 +35,48 @@ export const Banner = () => {
             letterSpacing="tight"
             color="gray.800"
           >
-            AI Stories by LabelFlow
+            Webinar - Get an AI model ready in no time
           </Heading>
 
           <Text mt="4" fontSize="lg" color="gray.800">
+<<<<<<< HEAD
             From preserving whales to preventing wild fires and developing a
             fitness app to workout during Covid. Discover how AI impacts your
             day to day life with leaders from around the world.
             <br />
             <br />
             <b> From Wednesday November 27th, 2021</b>
+=======
+            From image labeling to automatic inference, discover how to build a
+            proof of concept model for your custom use case in no time. We will
+            go through a real life use case: equipment inventory on the
+            distribution electrical system.
+            <br />
+            <br />
+            <b> Sept 28th - 4.30pm CET Time - Duration: 45min</b>
+>>>>>>> parent of 9a9efe53 (Update index.tsx)
           </Text>
 
           <LightMode>
             <Button
               mt="8"
               as="a"
+<<<<<<< HEAD
               href="https://labelflow.ai/posts
+=======
+              href="https://app.livestorm.co/labelflow/get-an-ai-model-ready-in-no-time
+>>>>>>> parent of 9a9efe53 (Update index.tsx)
             "
               target="blank"
               size="lg"
               colorScheme="brand"
               fontWeight="bold"
             >
+<<<<<<< HEAD
               Visit Now
+=======
+              Register now
+>>>>>>> parent of 9a9efe53 (Update index.tsx)
             </Button>
           </LightMode>
         </Box>
