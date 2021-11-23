c54c6e9ef969aea06007bf7ad546abf93d19b97a
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-11-23 16:15:10 +0000

    Decomment old code + adapt it to new API

--- typescript/db/src/repository/upload-s3.ts
+++ typescript/db/src/repository/upload-s3.ts
@@ -1,64 +1,79 @@
-// import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-// import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-// import "isomorphic-fetch";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import "isomorphic-fetch";
+import memoizeOne from "memoize-one";
+import { Repository } from "../../../common-resolvers/src";
+import { UploadTargetHttp } from "../../../graphql-types/src/graphql-types.generated";
 
-// import { Repository } from "../../../common-resolvers/src";
-// import { UploadTargetHttp } from "../../../graphql-types/src/graphql-types.generated";
+const bucket = "labelflow-images";
+const region = process.env?.AWS_REGION!;
 
-// const bucket = "labelflow-images";
-// const region = "eu-west-1";
-// const location = `https://${bucket}.s3.${region}.amazonaws.com`;
-// const s3Client = new S3Client({
-//   region,
-//   credentials: {
-//     accessKeyId: process.env?.LABELFLOW_AWS_ACCESS_KEY_ID!,
-//     secretAccessKey: process.env?.LABELFLOW_AWS_SECRET_ACCESS_KEY!,
-//   },
-// });
+const getClient = memoizeOne(
+  () =>
+    new S3Client({
+      region,
+      credentials: {
+        accessKeyId: process.env?.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env?.AWS_SECRET_ACCESS_KEY!,
+      },
+    })
+);
 
-// export const getUploadTargetHttp = async (
-//   key: string
-// ): Promise<UploadTargetHttp> => {
-//   const command = new PutObjectCommand({
-//     Bucket: bucket,
-//     Key: key,
-//   });
-//   const signedUrl = await getSignedUrl(s3Client, command, {
-//     expiresIn: 3600,
-//   });
-//   return {
-//     __typename: "UploadTargetHttp",
-//     uploadUrl: signedUrl,
-//     downloadUrl: `${location}/${key}`,
-//   };
-// };
+export const uploadsRoute = "/api/downloads";
 
-// export const getFromStorage: Repository["upload"]["get"] = async (url, req) => {
-//   const headers = new Headers();
-//   headers.set("Accept", "image/tiff,image/jpeg,image/png,image/*,*/*;q=0.8");
-//   headers.set("Sec-Fetch-Dest", "image");
-//   if ((req?.headers as any)?.cookie) {
-//     headers.set("Cookie", (req?.headers as any)?.cookie);
-//   }
+export const getUploadTargetHttp = async (
+  key: string,
+  origin: string
+): Promise<UploadTargetHttp> => {
+  const s3Client = getClient();
+  const command = new PutObjectCommand({
+    Bucket: bucket,
+    Key: key,
+  });
+  const signedUrl = await getSignedUrl(s3Client, command, {
+    expiresIn: 3600,
+  });
+  return {
+    __typename: "UploadTargetHttp",
+    uploadUrl: signedUrl,
+    downloadUrl: `${origin}${uploadsRoute}/${key}`,
+  };
+};
 
-//   const fetchResult = await fetch(url, {
-//     method: "GET",
-//     headers,
-//   });
+export const getFromStorage: Repository["upload"]["get"] = async (url, req) => {
+  const headers = new Headers();
+  headers.set("Accept", "image/tiff,image/jpeg,image/png,image/*,*/*;q=0.8");
+  headers.set("Sec-Fetch-Dest", "image");
+  if ((req?.headers as any)?.cookie) {
+    headers.set("Cookie", (req?.headers as any)?.cookie);
+  }
 
-//   if (fetchResult.status !== 200) {
-//     throw new Error(
-//       `Getting from S3 storage, could not fetch image at url ${url} properly, code ${fetchResult.status}`
-//     );
-//   }
-//   return await fetchResult.arrayBuffer();
-// };
+  const fetchResult = await fetch(url, {
+    method: "GET",
+    headers,
+    credentials: "include", // TODO: added this, is it needed?
+  });
 
-// export const putInStorage: Repository["upload"]["put"] = async (url, blob) => {
-//   await fetch(url, {
-//     method: "PUT",
-//     body: await blob.arrayBuffer(),
-//   });
-// };
+  if (fetchResult.status !== 200) {
+    throw new Error(
+      `Getting from S3 storage, could not fetch image at url ${url} properly, code ${fetchResult.status}`
+    );
+  }
+  return await fetchResult.arrayBuffer();
+};
 
-export const a = {};
+export const deleteFromStorage: Repository["upload"]["delete"] = async (
+  url
+) => {
+  console.warn(`
+      deleteFromStorage called on AWS S3 uploader with url ${url}.
+      Won't make anything
+      `);
+};
+
+export const putInStorage: Repository["upload"]["put"] = async (url, blob) => {
+  await fetch(url, {
+    method: "PUT",
+    body: await blob.arrayBuffer(),
+  });
+};
