268918258f888f62d8da36a9ea181ce38b97f375
Nacho Carnicero <ignacio.carnicero@sterblue.com>
2021-11-25 19:58:02 +0000

    Use broken image in svg format

--- typescript/web/src/components/empty-state/index.ts
+++ typescript/web/src/components/empty-state/index.ts
@@ -3,6 +3,7 @@ import Error from "../graphics/error";
 import ConnectionOff from "../graphics/connection-off";
 import CreditCard from "../graphics/credit-card";
 import Image from "../graphics/image";
+import ImageBroken from "../graphics/image-broken";
 import Search from "../graphics/search";
 import Coffee from "../graphics/coffee";
 
@@ -34,6 +35,15 @@ export const EmptyStateNoCreditCard = chakra(CreditCard, {
   },
 });
 export const EmptyStateNoImages = chakra(Image, {
+  baseStyle: {
+    width: 250,
+    height: 250,
+    padding: 6,
+    opacity: 0.7,
+    boxSizing: "border-box",
+  },
+});
+export const EmptyStateBrokenImage = chakra(ImageBroken, {
   baseStyle: {
     width: 250,
     height: 250,
--- typescript/web/src/components/labeling-tool/image-load-error.tsx
+++ typescript/web/src/components/labeling-tool/image-load-error.tsx
@@ -1,25 +1,18 @@
 import React from "react";
-import { Box, Center, Text, Heading, Image } from "@chakra-ui/react";
+import { Box, Center, Text, Heading } from "@chakra-ui/react";
+import { EmptyStateBrokenImage } from "../empty-state";
 
 export const ImageLoadError = () => (
   <Center h="full">
     <Box as="section">
       <Box
-        maxW="3xl"
+        maxW="2xl"
         mx="auto"
         px={{ base: "6", lg: "8" }}
         py={{ base: "16", sm: "20" }}
         textAlign="center"
-        alignItems="center"
-        justifyContent="center"
       >
-        <Image
-          src="/static/graphics/png/broken-image.png"
-          maxWidth={250}
-          padding={6}
-          opacity={0.7}
-          boxSizing="border-box"
-        />
+        <EmptyStateBrokenImage w="full" />
         <Heading as="h2">Oops, there is an error loading this image</Heading>
         <Text mt="4" fontSize="lg">
           You can try reloading the page
