8ae499cf5506dc00ecdb87d9bb24e77d4dd83f57
Hugo Raynal <raynalhugo@gmail.com>
2021-12-17 17:04:01 +0100

    Refactored dropzone uploading
    Still missing some features:
    - correct progress status
    - upload coco datasets

--- typescript/web/src/components/import-button/import-images-modal/modal-dropzone/modal-dropzone.tsx
+++ typescript/web/src/components/import-button/import-images-modal/modal-dropzone/modal-dropzone.tsx
@@ -9,67 +9,11 @@ import {
 } from "@chakra-ui/react";
 import { useApolloClient, useQuery, gql } from "@apollo/client";
 import { useRouter } from "next/router";
-import { v4 as uuidv4 } from "uuid";
-import mime from "mime-types";
-import Bluebird from "bluebird";
-import {
-  ExportFormat,
-  UploadTarget,
-  // ImportStatus,
-} from "@labelflow/graphql-types";
 import { Dropzone } from "./dropzone";
 import { FilesStatuses } from "./file-statuses";
 import { DroppedFile, UploadStatuses } from "../types";
 
-import { browser } from "../../../../utils/detect-scope";
-
-const createImageFromFileMutation = gql`
-  mutation createImageMutation(
-    $file: Upload!
-    $createdAt: DateTime
-    $datasetId: ID!
-  ) {
-    createImage(
-      data: { file: $file, createdAt: $createdAt, datasetId: $datasetId }
-    ) {
-      id
-    }
-  }
-`;
-
-const createImageFromUrlMutation = gql`
-  mutation createImageMutation(
-    $url: String!
-    $createdAt: DateTime
-    $name: String!
-    $datasetId: ID!
-  ) {
-    createImage(
-      data: {
-        url: $url
-        createdAt: $createdAt
-        name: $name
-        datasetId: $datasetId
-      }
-    ) {
-      id
-    }
-  }
-`;
-
-const getImageUploadTargetMutation = gql`
-  mutation getUploadTarget($key: String!) {
-    getUploadTarget(data: { key: $key }) {
-      ... on UploadTargetDirect {
-        direct
-      }
-      ... on UploadTargetHttp {
-        uploadUrl
-        downloadUrl
-      }
-    }
-  }
-`;
+import { createCreateImages } from "./create-create-images";
 
 const getDataset = gql`
   query getDataset($slug: String!, $workspaceSlug: String!) {
@@ -86,36 +30,6 @@ const getWorkspaceIdQuery = gql`
     }
   }
 `;
-
-const importDataset = gql`
-  mutation importDataset(
-    $where: DatasetWhereUniqueInput!
-    $data: DatasetImportInput!
-  ) {
-    importDataset(where: $where, data: $data) {
-      error
-    }
-  }
-`;
-
-const encodeFileToDataUrl = (file: File): Promise<string> => {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = (evt) => {
-      resolve(evt?.target?.result as string);
-    };
-    reader.onerror = reject;
-    reader.onabort = reject;
-    reader.readAsDataURL(file);
-  });
-};
-
-const getImageStoreKey = (
-  workspaceId: string,
-  datasetId: string,
-  fileId: string,
-  mimetype: string
-) => `${workspaceId}/${datasetId}/${fileId}.${mime.extension(mimetype)}`;
 
 export const ImportImagesModalDropzone = ({
   setMode,
@@ -157,164 +71,14 @@ export const ImportImagesModalDropzone = ({
     if (isEmpty(files)) return;
     if (!datasetId) return;
 
-    const createImages = async () => {
-      const now = new Date();
-      await Bluebird.Promise.map(
-        files.filter((file) => isEmpty(file.errors)),
-        async (acceptedFile, index) => {
-          try {
-            // Ask server how to upload image
-            const { data } = await apolloClient.mutate({
-              mutation: getImageUploadTargetMutation,
-              variables: {
-                key: getImageStoreKey(
-                  workspaceId as string,
-                  datasetId as string,
-                  uuidv4(),
-                  acceptedFile.file.type
-                ),
-              },
-            });
-
-            const target: UploadTarget = data.getUploadTarget;
-
-            // eslint-disable-next-line no-underscore-dangle
-            if (target.__typename === "UploadTargetDirect") {
-              // Direct file upload through graphql upload mutation
-              const createdAt = new Date();
-              createdAt.setTime(now.getTime() + index);
-              await apolloClient.mutate({
-                mutation: createImageFromFileMutation,
-                variables: {
-                  key: getImageStoreKey(
-                    workspaceId as string,
-                    datasetId as string,
-                    uuidv4(),
-                    acceptedFile.file.type
-                  ),
-                  file: acceptedFile.file,
-                  createdAt: createdAt.toISOString(),
-                  datasetId,
-                },
-              });
-
-              return setFileUploadStatuses((previousFileUploadStatuses) => {
-                return {
-                  ...previousFileUploadStatuses,
-                  [acceptedFile.file.path ?? acceptedFile.file.name]: true,
-                };
-              });
-            }
-
-            // eslint-disable-next-line no-underscore-dangle
-            if (target.__typename === "UploadTargetHttp") {
-              // File upload to the url provided by the server
-
-              if (browser?.name === "safari") {
-                // This special case is needed for Safari
-                // See https://github.com/labelflow/labelflow/issues/228
-                // See https://stackoverflow.com/questions/63144979/fetch-event-listener-not-triggering-in-service-worker-for-file-upload-via-mult
-                const url = await encodeFileToDataUrl(acceptedFile.file);
-
-                await apolloClient.mutate({
-                  mutation: createImageFromUrlMutation,
-                  variables: {
-                    url,
-                    name: acceptedFile.file.name,
-                    datasetId,
-                  },
-                });
-
-                return setFileUploadStatuses((previousFileUploadStatuses) => {
-                  return {
-                    ...previousFileUploadStatuses,
-                    [acceptedFile.file.path ?? acceptedFile.file.name]: true,
-                  };
-                });
-              }
-
-              // SUPABASE upload
-              // const form = new FormData();
-              // form.append("image", acceptedFile.file);
-              // await fetch(target.uploadUrl, {
-              //   method: "PUT",
-              //   body: form,
-              // });
-
-              // AWS upload
-              await fetch(target.uploadUrl, {
-                method: "PUT",
-                body: acceptedFile.file,
-              });
-              if (acceptedFile.file.type.startsWith("image")) {
-                const createdAt = new Date();
-                createdAt.setTime(now.getTime() + index);
-                await apolloClient.mutate({
-                  mutation: createImageFromUrlMutation,
-                  variables: {
-                    url: target.downloadUrl,
-                    createdAt: createdAt.toISOString(),
-                    name: acceptedFile.file.name,
-                    datasetId,
-                  },
-                });
-              } else {
-                // It's a dataset / annotations that we want to import
-                const dataImportDataset = await apolloClient.mutate({
-                  mutation: importDataset,
-                  variables: {
-                    where: { id: datasetId },
-                    data: {
-                      url: target.downloadUrl,
-                      format: ExportFormat.Coco,
-                      options: {
-                        coco: {
-                          annotationsOnly:
-                            acceptedFile.file.type === "application/json",
-                        },
-                      },
-                    },
-                  },
-                  refetchQueries: [
-                    "getDatasetData",
-                    "getImageLabels",
-                    "getLabel",
-                    "countLabelsOfDataset",
-                    "getDatasetLabelClasses",
-                  ],
-                });
-                if (dataImportDataset?.data?.importDataset?.error) {
-                  throw new Error(
-                    dataImportDataset?.data?.importDataset?.error
-                  );
-                }
-              }
-
-              return setFileUploadStatuses((previousFileUploadStatuses) => {
-                return {
-                  ...previousFileUploadStatuses,
-                  [acceptedFile.file.path ?? acceptedFile.file.name]: true,
-                };
-              });
-            }
-
-            throw new Error(
-              // eslint-disable-next-line no-underscore-dangle
-              `Unrecognized upload target provided by server: ${target.__typename}`
-            );
-          } catch (err) {
-            return setFileUploadStatuses((previousFileUploadStatuses) => {
-              return {
-                ...previousFileUploadStatuses,
-                [acceptedFile.file.path ?? acceptedFile.file.name]: err.message,
-              };
-            });
-          }
-        },
-        { concurrency: 5 }
-      );
-      onUploadEnd();
-    };
+    const createImages = createCreateImages(
+      files,
+      apolloClient,
+      workspaceId,
+      datasetId,
+      setFileUploadStatuses,
+      onUploadEnd
+    );
 
     onUploadStart();
     createImages();
