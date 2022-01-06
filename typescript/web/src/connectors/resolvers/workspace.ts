b92b6046a1691cfc17beed02d19e37248cd650c0
Valerian Gaudeau <valerian.gaudeau@labelflow.ai>
2022-01-06 18:30:09 +0100

    Fix bad aggregates count for workspace > datasets queries

---
+++ typescript/web/src/connectors/resolvers/workspace.ts
@@ -0,0 +1,46 @@
+import {
+  QueryWorkspaceArgs,
+  QueryWorkspacesArgs,
+} from "@labelflow/graphql-types";
+import { Context, DbWorkspaceWithType } from "@labelflow/common-resolvers";
+import { notImplementedInLocalWorkspaceRepository } from "../repository/utils";
+import { localWorkspace } from "../repository/workspace";
+
+const workspace = async (
+  _: any,
+  args: QueryWorkspaceArgs,
+  { repository, user }: Context
+): Promise<DbWorkspaceWithType> =>
+  (await repository.workspace.get(args.where, user)) as DbWorkspaceWithType;
+
+const workspaces = async (
+  _: any,
+  args: QueryWorkspacesArgs,
+  { repository }: Context
+) => await repository.workspace.list(args.where);
+
+const datasets = async (_parent: any, _args: any, { repository }: Context) => {
+  const queryResult = await repository.dataset.list();
+
+  return queryResult.map((datasetWithoutTypename) => ({
+    ...datasetWithoutTypename,
+    __typename: "Dataset",
+  }));
+};
+
+export default {
+  Query: {
+    workspace,
+    workspaces,
+    workspaceExists: notImplementedInLocalWorkspaceRepository,
+  },
+
+  Mutation: {
+    createWorkspace: notImplementedInLocalWorkspaceRepository,
+    updateWorkspace: notImplementedInLocalWorkspaceRepository,
+  },
+  Workspace: {
+    datasets,
+    memberships: () => [localWorkspace],
+  },
+};
