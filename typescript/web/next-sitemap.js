e6d6cc68615f2c6c57c80fbe334a289b6ae6805a
Jordan Le Lay <jordanlelay@protonmail.com>
2022-01-13 15:15:05 +0100

    Fix env variable for sitemap to work correctly

--- typescript/web/next-sitemap.js
+++ typescript/web/next-sitemap.js
@@ -1,15 +1,11 @@
-const { VERCEL_URL } = process.env;
+const { LABELFLOW_URL } = process.env;
 
-if (!VERCEL_URL) {
-  throw new Error("Missing required environment variable VERCEL_URL");
+if (!LABELFLOW_URL) {
+  throw new Error("Missing required environment variable LABELFLOW_URL");
 }
 
-const SITE_URL = VERCEL_URL.includes("://")
-  ? VERCEL_URL
-  : `https://${VERCEL_URL}`;
-
 module.exports = {
-  siteUrl: SITE_URL,
+  siteUrl: LABELFLOW_URL,
   exclude: [
     "/settings/profile",
     "/debug",
