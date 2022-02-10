5d3695d41affac7ade521532aaed4aad9905a10c
Jordan Le Lay <jordanlelay@protonmail.com>
2022-02-10 18:25:48 +0100

    Fix

--- typescript/web/src/components/invitation-manager/invitation-container.tsx
+++ typescript/web/src/components/invitation-manager/invitation-container.tsx
@@ -4,41 +4,40 @@ import {
   Heading,
   useColorModeValue as mode,
   VStack,
+  Text,
 } from "@chakra-ui/react";
 import { PropsWithChildren } from "react";
 
-type InvitationContainerProps = {
+export type InvitationContainerProps = PropsWithChildren<{
   header: string;
   details: string;
-};
-
-type CompleteProps = PropsWithChildren<InvitationContainerProps>;
+}>;
 
 export const InvitationContainer = ({
   header,
   details,
   children,
-}: CompleteProps) => {
-  return (
-    <Center h="full">
-      <VStack
-        p={6}
-        spacing={6}
-        align="start"
-        bg={mode("white", "gray.800")}
-        borderRadius="md"
-        boxShadow="lg"
-        maxW="2xl"
-        my="3.75rem"
-      >
-        <Heading fontSize="xl" fontWeight="semibold">
-          {header}
-        </Heading>
-        <Flex>{details}</Flex>
-        <Flex w="100%" justify="flex-end">
-          {children}
-        </Flex>
-      </VStack>
-    </Center>
-  );
-};
+}: InvitationContainerProps) => (
+  <Center h="full">
+    <VStack
+      role="dialog"
+      p={6}
+      spacing={6}
+      align="start"
+      bg={mode("white", "gray.800")}
+      borderRadius="md"
+      boxShadow="lg"
+      maxW="2xl"
+      my="3.75rem"
+      w="full"
+    >
+      <Heading fontSize="xl" fontWeight="semibold">
+        {header}
+      </Heading>
+      <Text>{details}</Text>
+      <Flex alignSelf="stretch" justify="flex-end">
+        {children}
+      </Flex>
+    </VStack>
+  </Center>
+);
