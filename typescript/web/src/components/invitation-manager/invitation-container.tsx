70d97746c3110bc9a3e6bf6535242f3888c3b47e
Jordan Le Lay <jordanlelay@protonmail.com>
2022-02-10 17:41:18 +0100

    Replace chakra factory by components

--- typescript/web/src/components/invitation-manager/invitation-container.tsx
+++ typescript/web/src/components/invitation-manager/invitation-container.tsx
@@ -1,4 +1,10 @@
-import { Center, chakra, useColorModeValue as mode } from "@chakra-ui/react";
+import {
+  Center,
+  Flex,
+  Heading,
+  useColorModeValue as mode,
+  VStack,
+} from "@chakra-ui/react";
 import { PropsWithChildren } from "react";
 
 type InvitationContainerProps = {
@@ -15,42 +21,24 @@ export const InvitationContainer = ({
 }: CompleteProps) => {
   return (
     <Center h="full">
-      <chakra.div
-        role="dialog"
+      <VStack
+        p={6}
+        spacing={6}
+        align="start"
         bg={mode("white", "gray.800")}
         borderRadius="md"
         boxShadow="lg"
-        color="inherit"
-        display="flex"
-        flexDirection="column"
         maxW="2xl"
         my="3.75rem"
-        outline={0}
-        position="relative"
-        width="100%"
       >
-        <chakra.header
-          fontSize="xl"
-          fontWeight="semibold"
-          px={6}
-          py={4}
-          flex={0}
-        >
+        <Heading fontSize="xl" fontWeight="semibold">
           {header}
-        </chakra.header>
-        <chakra.div flex={1} px={6} py={2}>
-          {details}
-        </chakra.div>
-        <chakra.footer
-          display="flex"
-          alignItems="center"
-          justifyContent="flex-end"
-          px={6}
-          py={4}
-        >
+        </Heading>
+        <Flex>{details}</Flex>
+        <Flex w="100%" justify="flex-end">
           {children}
-        </chakra.footer>
-      </chakra.div>
+        </Flex>
+      </VStack>
     </Center>
   );
 };
