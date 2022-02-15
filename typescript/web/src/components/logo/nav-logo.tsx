f122d05492f7b9b4618aee6241ce4d2db99c91b5
Baptist BENOIST <bbenoist@users.noreply.github.com>
2022-02-15 23:25:48 +0100

    Make NavLogo redirect to /workspaces if user is in a workspace

--- typescript/web/src/components/logo/nav-logo.tsx
+++ typescript/web/src/components/logo/nav-logo.tsx
@@ -1,12 +1,16 @@
 import { Box, VisuallyHidden, BreadcrumbLink } from "@chakra-ui/react";
+import { isNil } from "lodash/fp";
 
 import NextLink from "next/link";
+import { useOptionalWorkspace } from "../../hooks";
 
 import { Logo } from "./logo";
 
 export const NavLogo = () => {
+  const workspace = useOptionalWorkspace();
+  const href = isNil(workspace) ? "/" : "/workspaces";
   return (
-    <NextLink href="/">
+    <NextLink href={href}>
       <BreadcrumbLink flexShrink={0} flexGrow={0}>
         <Box rel="home" cursor="pointer" mr="0" overflow="visible">
           <VisuallyHidden>LabelFlow</VisuallyHidden>
