26befb958151ca1c0c8d9073276efbc643cfc4ea
geoffreyvancassel <geoffrey.vancassel@sterblue.com>
2022-02-22 15:51:31 +0100

    centering video

--- typescript/web/src/components/website/Video/movie.tsx
+++ typescript/web/src/components/website/Video/movie.tsx
@@ -1,21 +1,24 @@
-import { Box } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import * as React from "react";
-import { Header } from "./Header";
-import { IntroVideo } from "./IntroVideo";
+import { Header } from "./header";
+import { IntroVideo } from "./intro-video";
 
-export const Movie = () => {
-  return (
-    <Box id="movie" as="section">
-      <Box
-        maxW="2xl"
-        mx="auto"
-        px={{ base: "6", lg: "8" }}
-        py={{ base: "16", sm: "20" }}
-        textAlign="center"
-      >
-        <Header />
-        <IntroVideo />
-      </Box>
-    </Box>
-  );
-};
+export const Movie = () => (
+  <Flex
+    id="movie"
+    direction="column"
+    as="section"
+    px={{ base: "6", lg: "8" }}
+    py={{ base: "16", sm: "20" }}
+    align={{ base: "stretch", md: "center" }}
+  >
+    <Flex
+      direction="column"
+      textAlign="center"
+      minW={{ base: undefined, md: "3xl" }}
+    >
+      <Header />
+      <IntroVideo />
+    </Flex>
+  </Flex>
+);
--- typescript/web/src/components/website/video/intro-video.tsx
+++ typescript/web/src/components/website/video/intro-video.tsx
@@ -1,19 +1,9 @@
-import { Flex, AspectRatio } from "@chakra-ui/react";
-import * as React from "react";
+import { Flex, AspectRatio, Center } from "@chakra-ui/react";
 
 export const IntroVideo = () => (
-  <Flex
-    direction="column"
-    justify="center"
-    flexGrow={1}
-    mt="4"
-    minW={{ base: "full", lg: "1000px" }}
-    h={{ base: "auto", lg: "560px" }}
-  >
-    <AspectRatio ratio={16 / 9}>
+  <Flex direction="column" mt="4" minH="315">
+    <AspectRatio ratio={16 / 9} flexGrow={1} maxW="800">
       <iframe
-        width="560"
-        height="315"
         src="https://www.youtube.com/embed/Uxo1KWe-PGQ?playlist=Uxo1KWe-PGQ&loop=1&rel=0&controls=1"
         title="Get started in 1min"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
