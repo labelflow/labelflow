998cfecd447459df7a5e8d380276034f5e541d2e
Jordan Le Lay <jordanlelay@protonmail.com>
2022-02-24 11:02:55 +0100

    Update story as UpdatePlanBanner now uses router and workspaceSlug

--- typescript/web/src/components/workspaces/update-plan-banner.stories.tsx
+++ typescript/web/src/components/workspaces/update-plan-banner.stories.tsx
@@ -1,13 +1,16 @@
-import {
-  chakraDecorator,
-  queryParamsDecorator,
-  storybookTitle,
-} from "../../utils/stories";
+import { createCommonDecorator, storybookTitle } from "../../utils/stories";
+import { WORKSPACE_DATA } from "../../utils/fixtures";
 import { UpdatePlanBanner } from "./update-plan-banner";
 
 export default {
   title: storybookTitle("Workspaces", "UpdatePlanBanner"),
-  decorators: [chakraDecorator, queryParamsDecorator],
+  decorators: [
+    createCommonDecorator({
+      auth: { withWorkspaces: true },
+      apollo: true,
+      router: { query: { workspaceSlug: WORKSPACE_DATA.slug } },
+    }),
+  ],
 };
 
 export const Open = () => <UpdatePlanBanner />;
