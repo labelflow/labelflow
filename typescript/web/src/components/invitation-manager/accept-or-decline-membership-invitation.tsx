18e443759a55292d1f4dd4d69ac69c51fd50c0c3
Jordan Le Lay <43350109+Jordanlelay@users.noreply.github.com>
2022-02-25 19:16:54 +0100

    Fix dark mode color problem in invitation modal and user menu (#874)

--- typescript/web/src/components/invitation-manager/accept-or-decline-membership-invitation.tsx
+++ typescript/web/src/components/invitation-manager/accept-or-decline-membership-invitation.tsx
@@ -1,4 +1,5 @@
-import { Button, Center, chakra } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { InvitationContainer } from "./invitation-container";
 
 export const AcceptOrDeclineMembershipInvitation = ({
   accept,
@@ -16,63 +17,27 @@ export const AcceptOrDeclineMembershipInvitation = ({
   disabled: boolean;
 }) => {
   return (
-    <>
-      <Center h="full">
-        <chakra.div
-          role="dialog"
-          bg="white"
-          borderRadius="md"
-          boxShadow="lg"
-          color="inherit"
-          display="flex"
-          flexDirection="column"
-          maxW="2xl"
-          my="3.75rem"
-          outline={0}
-          position="relative"
-          width="100%"
-        >
-          <chakra.header
-            fontSize="xl"
-            fontWeight="semibold"
-            px={6}
-            py={4}
-            flex={0}
-          >
-            Join {workspaceName} as {currentUserIdentifier}?
-          </chakra.header>
-          <chakra.div flex={1} px={6} py={2}>
-            This invitation was sent to {invitationEmailAddress}, by accepting
-            it you will have access to every datasets in the workspace.
-          </chakra.div>
-
-          <chakra.footer
-            display="flex"
-            alignItems="center"
-            justifyContent="flex-end"
-            px={6}
-            py={4}
-          >
-            <Button
-              aria-label="Decline invitation"
-              disabled={disabled}
-              onClick={decline}
-            >
-              Decline
-            </Button>
-            <Button
-              aria-label="Accept invitation"
-              autoFocus
-              disabled={disabled}
-              colorScheme="brand"
-              ml={3}
-              onClick={accept}
-            >
-              Accept
-            </Button>
-          </chakra.footer>
-        </chakra.div>
-      </Center>
-    </>
+    <InvitationContainer
+      header={`Join ${workspaceName} as ${currentUserIdentifier}?`}
+      details={`This invitation was sent to ${invitationEmailAddress}, by accepting it you will have access to every datasets in the workspace.`}
+    >
+      <Button
+        aria-label="Decline invitation"
+        disabled={disabled}
+        onClick={decline}
+      >
+        Decline
+      </Button>
+      <Button
+        aria-label="Accept invitation"
+        autoFocus
+        disabled={disabled}
+        colorScheme="brand"
+        ml={3}
+        onClick={accept}
+      >
+        Accept
+      </Button>
+    </InvitationContainer>
   );
 };
