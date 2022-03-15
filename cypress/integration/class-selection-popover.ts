f0584dfaed16571b86d85df4edf6879dd58aa8ba
Baptist BENOIST <baptist.benoist@labelflow.ai>
2022-03-15 12:26:33 +0100

    Use data-testid to retrieve ImageCard in class-selection-popover Cypress test

--- cypress/integration/class-selection-popover.ts
+++ cypress/integration/class-selection-popover.ts
@@ -295,7 +295,7 @@ describe("Class selection popover", () => {
     cy.get('[aria-label="Confirm delete label class"]').click();
     cy.contains("A new class").should("not.exist");
     cy.contains(/^images$/).click();
-    cy.get('[aria-label="Open image"]').click();
+    cy.get('[data-testid="image-card"]').click();
 
     cy.wait(420);
     cy.get('[aria-label="loading indicator"]').should("not.exist");
