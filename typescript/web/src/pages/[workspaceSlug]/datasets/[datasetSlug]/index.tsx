fe8b90cc8e67c5827dba2b98e1db5470be8310a9
Baptist BENOIST <bbenoist@users.noreply.github.com>
2022-03-22 18:43:58 +0100

    Factorize datasets index page redirection

--- typescript/web/src/pages/[workspaceSlug]/datasets/[datasetSlug]/index.tsx
+++ typescript/web/src/pages/[workspaceSlug]/datasets/[datasetSlug]/index.tsx
@@ -1,8 +1,9 @@
-import { useQuery } from "@apollo/client";
+import { ApolloError, useQuery } from "@apollo/client";
 import { BreadcrumbLink, Skeleton, Text } from "@chakra-ui/react";
+import { isEmpty, isNil } from "lodash/fp";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
-import React, { useEffect } from "react";
+import { useCallback } from "react";
 import { useErrorHandler } from "react-error-boundary";
 import { Authenticated } from "../../../../components/auth";
 import { CookieBanner } from "../../../../components/cookie-banner";
@@ -15,52 +16,82 @@ import { NavLogo } from "../../../../components/logo/nav-logo";
 import { Meta } from "../../../../components/meta";
 import { LayoutSpinner } from "../../../../components/spinner";
 import { WorkspaceSwitcher } from "../../../../components/workspace-switcher";
-import { getApolloErrorMessage } from "../../../../utils/get-apollo-error-message";
-import { Error404Content } from "../../../404";
+import {
+  GetDatasetBySlugQuery,
+  GetDatasetBySlugQueryVariables,
+} from "../../../../graphql-types";
+import { useRouterQueryString } from "../../../../hooks";
+import { getApolloErrorMessage } from "../../../../utils";
+
+const useApolloErrorHandler = () => {
+  const router = useRouter();
+  const handleError = useErrorHandler();
+  return useCallback(
+    (error: ApolloError) => {
+      if (!router.isReady) return;
+      const errorMsg = getApolloErrorMessage(error);
+      if (errorMsg === "User not authorized to access dataset") {
+        router.push("/404");
+      } else {
+        handleError(error);
+      }
+    },
+    [handleError, router]
+  );
+};
+
+const useRedirectToDefaultTab = () => {
+  const router = useRouter();
+  return useCallback(() => {
+    if (!router.isReady) return;
+    const { datasetSlug, workspaceSlug, ...queryRest } = router.query;
+    router.replace({
+      pathname: `/${workspaceSlug}/datasets/${datasetSlug}/images`,
+      query: queryRest,
+    });
+  }, [router]);
+};
+
+const useRedirect = (
+  loading: boolean,
+  error: ApolloError | undefined
+): void => {
+  const handleError = useApolloErrorHandler();
+  const redirectToDefaultTab = useRedirectToDefaultTab();
+  const router = useRouter();
+  if (!loading && router.isReady) {
+    if (isNil(error)) {
+      redirectToDefaultTab();
+    } else {
+      handleError(error);
+    }
+  }
+};
 
 const Body = () => {
-  const router = useRouter();
-  const { datasetSlug, workspaceSlug, ...queryRest } = router.query;
+  const datasetSlug = useRouterQueryString("datasetSlug");
+  const workspaceSlug = useRouterQueryString("workspaceSlug");
 
   const {
     data: datasetResult,
     error,
     loading,
-  } = useQuery(GET_DATASET_BY_SLUG_QUERY, {
-    variables: { slug: datasetSlug, workspaceSlug },
-    skip: typeof datasetSlug !== "string" || typeof workspaceSlug !== "string",
-  });
-
-  if (
-    error &&
-    getApolloErrorMessage(error) === "User not authorized to access dataset"
-  ) {
-    router.push("/404");
-  }
+  } = useQuery<GetDatasetBySlugQuery, GetDatasetBySlugQueryVariables>(
+    GET_DATASET_BY_SLUG_QUERY,
+    {
+      variables: { slug: datasetSlug, workspaceSlug },
+      skip: isEmpty(workspaceSlug) || isEmpty(datasetSlug),
+    }
+  );
 
   const datasetName = datasetResult?.dataset.name;
 
-  useEffect(() => {
-    if (router.isReady && !error && !loading) {
-      router.replace({
-        pathname: `/${workspaceSlug}/datasets/${datasetSlug}/images`,
-        query: queryRest,
-      });
-    }
-  }, [error, loading, router.isReady]);
+  useRedirect(loading, error);
 
   const handleError = useErrorHandler();
   if (error && !loading) {
-    if (!error.message.match(/Couldn't find dataset corresponding to/)) {
-      handleError(error);
-    }
-    return (
-      <>
-        <Meta title="LabelFlow | Dataset not found" />
-        <CookieBanner />
-        <Error404Content />
-      </>
-    );
+    handleError(getApolloErrorMessage(error));
+    return <></>;
   }
 
   return (
