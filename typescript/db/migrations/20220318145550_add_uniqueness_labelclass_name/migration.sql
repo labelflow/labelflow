b2cc305fb523432e9c72af13eeaca54746cfa4ee
Jordan Le Lay <jordanlelay@protonmail.com>
2022-03-29 09:43:52 +0200

    Change index name using camel case

--- typescript/db/migrations/20220318145550_add_uniqueness_labelclass_name/migration.sql
+++ typescript/db/migrations/20220318145550_add_uniqueness_labelclass_name/migration.sql
@@ -2,4 +2,4 @@
 DELETE FROM "LabelClass" a WHERE EXISTS(SELECT * FROM "LabelClass" b WHERE "a"."datasetId" = "b"."datasetId" AND "a"."name" = "b"."name" AND "b"."id" < "a"."id");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "labelclass_name" ON "LabelClass"("name", "datasetId");
+CREATE UNIQUE INDEX "labelClass_name" ON "LabelClass"("name", "datasetId");
