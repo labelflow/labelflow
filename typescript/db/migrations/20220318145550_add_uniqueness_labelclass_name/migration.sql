4b88cfce2dc2b1b744f08d3e050206d32d3730b6
Baptist BENOIST <baptist.benoist@labelflow.ai>
2022-04-19 17:42:13 +0200

    Reassign labels to the remaining label-class ID

--- typescript/db/migrations/20220318145550_add_uniqueness_labelclass_name/migration.sql
+++ typescript/db/migrations/20220318145550_add_uniqueness_labelclass_name/migration.sql
@@ -1,5 +1,23 @@
--- Removes duplicates rows based on "datasetId" "name" combination. 
-DELETE FROM "LabelClass" a WHERE EXISTS(SELECT * FROM "LabelClass" b WHERE "a"."datasetId" = "b"."datasetId" AND "a"."name" = "b"."name" AND "b"."id" < "a"."id");
+-- Group label-classes by unique datasetId+name
+SELECT "b"."id" AS "oldId", "a"."id" AS "newId"
+INTO TEMP "#UniqueLabelClasses"
+FROM
+  (SELECT MIN(id::TEXT) AS "id", "datasetId", "name", COUNT(*) as "occurrences"
+   FROM "LabelClass" GROUP BY "datasetId","name") "a"
+INNER JOIN
+    "LabelClass" "b" ON "a"."datasetId" = "b"."datasetId" AND "a"."name" = "b"."name"
+WHERE ("occurrences" > 1) AND ("a"."id" <> "b"."id"::TEXT);
 
--- CreateIndex
+-- Update labels which are referencing the duplicates label-classes
+UPDATE "Label"
+    SET "labelClassId" = "#UniqueLabelClasses"."newId"::uuid
+FROM "#UniqueLabelClasses"
+WHERE "Label"."labelClassId" = "#UniqueLabelClasses"."oldId";
+
+-- Delete duplicate label-classes
+DELETE FROM "LabelClass"
+USING "#UniqueLabelClasses"
+WHERE "LabelClass"."id" = "#UniqueLabelClasses"."oldId";
+
+-- Add unique constraint on dasasetId+name
 CREATE UNIQUE INDEX "labelClass_name" ON "LabelClass"("name", "datasetId");
